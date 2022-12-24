40741438203db2bedcb5687ca3bb3a094454da81
atmelino <2985251+atmelino@users.noreply.github.com>
2022-12-23 18:36:19 -0600

    deno learn 3

---
+++ Examples/HTTP_Web_Server/run_webserver.sh
@@ -0,0 +1,11 @@
+
+deno --version
+
+whereis deno
+
+deno run --allow-net webserver.ts
+
+echo press enter
+
+read input
+
---
+++ first_steps/run_http_server.sh
@@ -0,0 +1,11 @@
+
+deno --version
+
+whereis deno
+
+deno run --allow-net --allow-write http_server.ts
+
+echo press enter
+
+read input
+
