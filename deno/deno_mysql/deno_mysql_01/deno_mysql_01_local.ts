c68f8bcefe24e3c0c9153ede0756a97c2e44ec34
atmelino <2985251+atmelino@users.noreply.github.com>
2023-01-02 19:40:02 -0600

    u

--- deno/deno_mysql/deno_mysql_01/deno_mysql_01_local.ts
+++ deno/deno_mysql/deno_mysql_01/deno_mysql_01_local.ts
@@ -1,7 +1,7 @@
 import { Client } from "https://deno.land/x/mysql/mod.ts";
 
 const client = await new Client().connect({
-  hostname: "45.79.28.148",
+  hostname: "127.0.0.1",
   username: "sammy",
   db: "sammydb",
   poolSize: 3, // connection limit
