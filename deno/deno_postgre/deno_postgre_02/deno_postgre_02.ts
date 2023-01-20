832613d7dc9410fb41a563f7dfb3d12364d85fa7
atmelino <2985251+atmelino@users.noreply.github.com>
2023-01-19 20:16:39 -0600

    u

--- deno/deno_postgre/deno_postgre_02/deno_postgre_02.ts
+++ deno/deno_postgre/deno_postgre_02/deno_postgre_02.ts
@@ -6,7 +6,7 @@ import { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 import * as postgres from "https://deno.land/x/postgres@v0.14.2/mod.ts";
 
 // Get the connection string from the environment variable "DATABASE_URL"
-const databaseUrl = Deno.env.get("postgres://postgres:deno1pass2word3@db.lwbjwhxdyxafkxjdldmn.supabase.co:6543/postgres")!;
+const databaseUrl = Deno.env.get("DATABASE_URL")!;
 
 // Create a database pool with three connections that are lazily established
 const pool = new postgres.Pool(databaseUrl, 3, true);
