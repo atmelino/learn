88414ff29ede10f8eff5ea8ef2b6f73487dbb4e8
atmelino <2985251+atmelino@users.noreply.github.com>
2023-02-14 20:54:46 -0600

    u

--- deno/deno_Fresh/deno_popup/components/Header.tsx
+++ deno/deno_Fresh/deno_popup/components/Header.tsx
@@ -10,13 +10,6 @@ export function Header() {
     >
       <div class="w-full h-full absolute" />
       <nav class="w-11/12 h-24 max-w-5xl mx-auto flex items-center justify-between relative">
-        <a href="/">
-          <img
-            src="/logo.svg"
-            alt="Deno Logo"
-            class="h-14 w-14"
-          />
-        </a>
         <h1>
           <img
             src="/text_logo.svg"
