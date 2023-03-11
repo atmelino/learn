bc2dd912d7777e8dea4bd48aa35c94e656991854
atmelino <2985251+atmelino@users.noreply.github.com>
2023-03-10 20:23:32 -0600

    u

---
+++ deno/deno_Fresh/noteKeeper_dev_02/routes/_middleware.ts
@@ -0,0 +1,49 @@
+import { remultFresh } from "remult/remult-fresh";
+import { Task } from "../model/task.ts";
+import { createPostgresConnection } from "https://deno.land/x/remult/postgres.ts";
+
+/* configuration sammy */
+export const remultServer = remultFresh({
+  entities: [Task],
+  dataProvider: async () => {
+    return createPostgresConnection({
+      configuration: {
+        user: "postgres",
+        password: "password",
+        database: "sammy",
+        hostname: "localhost",
+        port: 5432,
+        tls: {enabled:false}
+      },
+    });
+    return await undefined;
+  },
+}, Response);
+export const handler = remultServer.handle;
+
+/* connection string */
+// export const remultServer = remultFresh({
+//   entities: [Task],
+//   dataProvider: async () => {
+//     //const dbUrl = Deno.env.get("DATABASE_URL");
+//     const dbUrl = "postgres://postgres:password@localhost:5432/sammy";
+//     if (dbUrl) {
+//       return createPostgresConnection({ connectionString: dbUrl });
+//     }
+//     return await undefined;
+//   },
+// }, Response);
+//export const handler = remultServer.handle;
+
+/* original */
+// export const remultServer = remultFresh({
+//   entities: [Task],
+//   dataProvider: async () => {
+//     const dbUrl = Deno.env.get("DATABASE_URL");
+//     if (dbUrl) {
+//       return createPostgresConnection({ connectionString: dbUrl });
+//     }
+//     return await undefined;
+//   },
+// }, Response);
+// export const handler = remultServer.handle;
--- deno/deno_Fresh/noteKeeper_dev_02/routes/index.tsx
+++ deno/deno_Fresh/noteKeeper_dev_02/routes/index.tsx
@@ -4,6 +4,7 @@ import { Handlers, PageProps } from "$fresh/server.ts";
 import Todos from "../islands/todos.tsx";
 import { Task } from "../model/task.ts";
 import { remultServer } from "./_middleware.ts";
+import { Head } from "$fresh/runtime.ts";
 
 export const handler: Handlers<Task[]> = {
   async GET(req, ctx) {
@@ -15,6 +16,10 @@ export const handler: Handlers<Task[]> = {
 export default function Home({ data }: PageProps<Task[]>) {
   return (
     <div>
+      <Head>
+        <title>Remult with conf object</title>
+        <meta name="description" content="textarea test" />
+      </Head>
       <Todos data={data} />
     </div>
   );
