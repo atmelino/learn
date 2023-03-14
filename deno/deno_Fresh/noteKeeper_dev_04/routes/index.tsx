6c116a2a930453c428a1cbe154b7d8ff4f505b45
atmelino <2985251+atmelino@users.noreply.github.com>
2023-03-13 20:52:56 -0500

    u

---
+++ deno/deno_Fresh/noteKeeper_dev_04/routes/index.tsx
@@ -0,0 +1,36 @@
+import { Handlers, PageProps } from "$fresh/server.ts";
+import Todos from "../islands/todos.tsx";
+import { Task } from "../model/task.ts";
+import { remultServer } from "./_middleware.ts";
+import { Head } from "$fresh/runtime.ts";
+import Counter from "../islands/Counter.tsx";
+
+export const handler: Handlers<Task[]> = {
+  async GET(req, ctx) {
+    const remult = await remultServer.getRemult(req);
+    return ctx.render(await remult.repo(Task).find());
+  },
+};
+
+export default function Home({ data }: PageProps<Task[]>) {
+  return (
+    <>
+      <Head>
+        <title>Fresh App</title>
+      </Head>
+      <div class="p-4 mx-auto max-w-screen-md">
+        <img
+          src="/logo.svg"
+          class="w-32 h-32"
+          alt="the fresh logo: a sliced lemon dripping with juice"
+        />
+        <p class="my-6">
+          Welcome to `fresh`. Try updating this message in the
+          ./routes/index.tsx file, and refresh.
+        </p>
+        <Counter start={3} />
+      </div>
+      <Todos data={data} />
+    </>
+  );
+}
