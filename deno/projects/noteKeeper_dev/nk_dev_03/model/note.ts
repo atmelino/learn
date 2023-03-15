f68f3c4998463c9c19ca0f7cb8addd405b00d8a3
atmelino <2985251+atmelino@users.noreply.github.com>
2023-03-15 18:23:59 -0500

    u

---
+++ deno/projects/noteKeeper_dev/nk_dev_03/model/note.ts
@@ -0,0 +1,27 @@
+import { Entity, Fields } from "remult";
+
+@Entity("notes", {
+    allowApiCrud: true
+})
+export class Task {
+    @Fields.uuid()
+    id!: string;
+
+    @Fields.date()
+    timestamp='';
+
+    //@Fields.updatedAt()
+    //timestamp = new Date()
+
+    //@Fields.date()
+    //timestamp = new Date()
+
+    @Fields.string()
+    note = '';
+}
+
+// id SERIAL PRIMARY KEY,
+// timestamp TIMESTAMP NOT NULL,
+// noteId TEXT NOT NULL,
+// note TEXT NOT NULL
+
---
+++ deno/projects/noteKeeper_dev/nk_dev_03/run_dbtable_create.sh
@@ -0,0 +1,9 @@
+
+echo run
+
+deno run --watch --allow-net --allow-env --allow-read dbtable_create.ts
+
+echo press enter
+
+read input
+
