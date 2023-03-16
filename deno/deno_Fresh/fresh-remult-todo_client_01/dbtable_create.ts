043928379c8dde1ba531cf97912bec73080c9554
atmelino <2985251+atmelino@users.noreply.github.com>
2023-03-15 20:19:14 -0500

    u

---
+++ deno/deno_Fresh/fresh-remult-todo_client_01/dbtable_create.ts
@@ -0,0 +1,33 @@
+import { Client } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
+import settings from "../../../../settings_nk.js";
+
+async function createTable() {
+  try {
+    // Create the table
+    const result = await client.queryArray`
+  CREATE TABLE IF NOT EXISTS tasks (
+    id UUID PRIMARY KEY,
+    title TEXT NOT NULL,
+    completed BOOLEAN NOT NULL
+  )
+  `;
+    console.log(result);
+  } finally {
+    // Release the connection back into the pool
+    await client.end();
+  }
+}
+
+// Connect to the database
+const client = new Client({
+  user: settings.dbsettings.user,
+  password: settings.dbsettings.password,
+  database: settings.dbsettings.database,
+  hostname: settings.dbsettings.hostname,
+  port: settings.dbsettings.port,
+});
+await client.connect();
+
+createTable();
+
+
