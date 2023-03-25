76d4a66e396b72cd783a9f0941def70a2e4afd4a
atmelino <2985251+atmelino@users.noreply.github.com>
2023-03-24 21:07:42 -0500

    u

---
+++ nodejs/chartjs/chartjs_03/run.sh
@@ -0,0 +1,16 @@
+
+echo run
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+cd /media/data/github/learn/nodejs/chartjs/chartjs_03
+yarn dev
+
+#~/.nvm/versions/node/v19.2.0/bin/npm run dev
+#yarn dev
+
+echo press enter
+
+read input
+
---
+++ nodejs/chartjs/dynamic_01/README.md
@@ -0,0 +1,4 @@
+# Chart.js - Dynamically Update Chart Via Ajax Requests
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/jordanwillis/pen/bqaGRR](https://codepen.io/jordanwillis/pen/bqaGRR).
+
