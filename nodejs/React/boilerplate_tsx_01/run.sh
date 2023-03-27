abf5f06e6e3be4335e86dc57d5b46a27e15c27e8
atmelino <2985251+atmelino@users.noreply.github.com>
2023-03-26 20:49:22 -0500

    u

---
+++ nodejs/React/boilerplate_tsx_01/run.sh
@@ -0,0 +1,13 @@
+
+echo run
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+cd /media/data/github/learn/nodejs/React/boilerplate_tsx_01
+npm start
+
+echo press enter
+`
+read input
+
---
+++ nodejs/React/chartjs_logrocket_01/src/App_logrocket.tsx
@@ -0,0 +1,21 @@
+// App.js
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import { useState } from "react";
+import { Data } from "../utils/Data";
+import PieChart from "./components/PieChart";
+import "./styles.css";
+
+Chart.register(CategoryScale);
+
+export default function App() {
+  const [chartData, setChartData] = useState({
+    // ...chart data
+  });
+ 
+  return (
+    <div className="App">
+      <PieChart chartData={chartData} />
+    </div>
+  );
+}
