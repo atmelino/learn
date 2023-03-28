3fe5f5f4cf96a6719a3cd84ca9bcd2849c0df7e7
atmelino <2985251+atmelino@users.noreply.github.com>
2023-03-27 20:13:06 -0500

    u

---
+++ nodejs/React/timer_03/src/App.tsx
@@ -0,0 +1,22 @@
+// https://blog.greenroots.info/how-to-create-a-countdown-timer-using-react-hooks
+
+import React from 'react';
+import CountdownTimer from './CountdownTimer.js';
+import './App.css';
+
+export default function App() {
+  const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
+  const SEVEN_DAYS_IN_MS = 7 * 24 * 60 * 60 * 1000;
+  const NOW_IN_MS = new Date().getTime();
+
+  const dateTimeAfterSevenDays = NOW_IN_MS + SEVEN_DAYS_IN_MS;
+
+  return (
+    <div>
+      <h1>Periodic Timer</h1>
+
+      <h2>Expires after 7 days!!!</h2>
+      <CountdownTimer targetDate={dateTimeAfterSevenDays} />
+    </div>
+  );
+}
