28f43f7b19ca169ea676e1a7e0f9197ce55ae910
atmelino <2985251+atmelino@users.noreply.github.com>
2023-03-28 18:57:14 -0500

    u

--- nodejs/React/timer_04/src/App.tsx
+++ nodejs/React/timer_04/src/App.tsx
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+//import { Helmet } from 'react-helmet';
+import Helmet from 'react-helmet';
 
 import Stopwatch from "./components/Stopwatch";
 import Countdown from "./components/Countdown";
@@ -6,7 +8,11 @@ import Countdown from "./components/Countdown";
 class App extends Component {
   render() {
     return (
+      
       <div className="App">
+        <Helmet>
+          <title>timer_04</title>
+        </Helmet>
         <div className="App-title">Timers Demo</div>
         <div className="Timers">
           <Stopwatch />
---
+++ nodejs/React/timer_05/src/components/PeriodicTask.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class PeriodicTask extends Component {
+
+    render() {
+        return (
+          <div>
+            hello
+          </div>
+        );
+      }
+    
+}
+
+export default PeriodicTask;
