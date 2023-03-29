08623a806b5cdaf064167d1dc4f1f071852ddb7f
atmelino <you@example.com>
2023-03-29 00:13:18 -0500

    u

--- nodejs/React/timer_05/src/components/PeriodicTask.js
+++ nodejs/React/timer_05/src/components/PeriodicTask.js
@@ -6,25 +6,31 @@ import { useEffect, useState } from 'react';
 const PeriodicTask = ({ targetDate }) => {
   const [days, hours, minutes, seconds] = useSeconds(targetDate);
   const [counter, setCounter] = useState(0);
+  //const [myTimer, setmyTimer] = useState(null);
 
 
   function startTask() {
     console.log("startTask called")
-    setInterval(myTask, 3000);
-    setCounter(counter + 1);
+    setInterval(myTask, 2000);
+    //setCounter(counter => counter + 1);
   }
 
   function myTask() {
     console.log("myTask called")
-    setCounter(counter + 1);
+    console.log("counter=" + counter)
+    //let tmpvar = counter + 1;
+    setCounter(15);
   }
+
+
+
   return (
     <div>
       seconds = {seconds}
-   
+
       <button onClick={() => startTask()}>Start</button>
       {counter}
-   
+
     </div>
   );
 
