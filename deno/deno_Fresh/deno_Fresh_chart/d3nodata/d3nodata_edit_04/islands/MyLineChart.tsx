f8defa9cc10e476efd42b30c75abe6089bc2024c
atmelino <2985251+atmelino@users.noreply.github.com>
2023-04-10 20:54:15 -0500

    u

--- deno/deno_Fresh/deno_Fresh_chart/d3nodata/d3nodata_edit_04/islands/MyLineChart.tsx
+++ deno/deno_Fresh/deno_Fresh_chart/d3nodata/d3nodata_edit_04/islands/MyLineChart.tsx
@@ -4,7 +4,27 @@ import { LineChartDateMod } from "../library/charts.ts";
 
 export default function MyLineChart() {
   let timems = Date.now();
-  const datasets1 = [
+  let datasets2 = [
+    {
+      label: "test",
+      color: "green",
+      data: [
+        {
+          x: new Date(10000),
+          y: 2,
+        },
+        {
+          x: new Date(20000),
+          y: 4,
+        },
+        {
+          x: new Date(30000),
+          y: 3,
+        },
+      ],
+    },
+  ];
+  let datasets1 = [
     {
       label: "test",
       color: "green",
@@ -14,16 +34,12 @@ export default function MyLineChart() {
           y: 2,
         },
         {
-          x: new Date(timems + 1000),
+          x: new Date(timems + 10000),
           y: 4,
         },
         {
-          x: new Date(timems + 2000),
+          x: new Date(timems + 20000),
           y: 3,
-        },
-        {
-          x: new Date(timems + 3000),
-          y: 1,
         },
       ],
     },
@@ -33,41 +49,37 @@ export default function MyLineChart() {
   const [datasets, setData] = useState(datasets1);
   const [update, setUpdate] = useState("initial");
 
-  let titleOnOff = true;
-
   function addData() {
     console.log("addData called");
 
-    timems += 1000;
-    const value = Math.floor((Math.random() * 100) + 1);
-    datasets1[0].data.splice(0, 1);
+    timems += 100000;
+    const value = Math.floor((Math.random() * 10) + 1);
+    // datasets1[0].data.splice(0, 1);
     // console.log(datasets1[0].data);
-    console.log("datasets " + JSON.stringify(datasets, null, 4));
-    datasets1[0].data.push({
-      x: new Date(timems + 3000),
+    datasets[0].data.push({
+      x: new Date(timems + 30000),
       y: value,
     });
-    setData(datasets1);
+    console.log("datasets1 " + JSON.stringify(datasets1, null, 4));
+
+    setData(datasets);
+
     setUpdate("changed");
+
+    console.log("datasets " + JSON.stringify(datasets, null, 4));
   }
 
   const changeData = () => {
     console.log("changeData called");
-    // console.log("datasets before" + JSON.stringify(datasets, null, 4));
-    // datasets1[0].data[0] = {
-    //   x: new Date(timems),
-    //   y: 8,
-    // };
 
     timems += 1000;
     datasets1[0].data.push({
-      x: new Date(timems + 3000),
+      x: new Date(timems + 30000),
       y: 2,
     });
-    setData(datasets1);
+    // setData(datasets1);
     // console.log("datasets after" + JSON.stringify(datasets, null, 4));
     setUpdate("changed");
-    titleOnOff = false;
   };
 
   return (
@@ -81,7 +93,6 @@ export default function MyLineChart() {
       <LineChartDateMod
         datasets={datasets}
         data={data1}
-        addTitle={titleOnOff}
         requestUpdate={update}
       >
       </LineChartDateMod>
