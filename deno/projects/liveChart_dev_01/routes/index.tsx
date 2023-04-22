921b573e6f680c333381e3d2ea1eeb93727376d7
atmelino <2985251+atmelino@users.noreply.github.com>
2023-04-21 21:41:48 -0500

    u

--- deno/projects/liveChart_dev_01/routes/index.tsx
+++ deno/projects/liveChart_dev_01/routes/index.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>live Chart dev 02</title>
+        <title>live Chart dev 01</title>
       </Head>
       <Body />
     </>
