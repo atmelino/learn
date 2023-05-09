8d4ba0da2ffc811086e9db3bdd0cb8374c6fabcf
atmelino <2985251+atmelino@users.noreply.github.com>
2023-05-09 18:55:43 -0500

    u

--- deno/deno_Fresh/datatable_01/islands/Body.tsx
+++ deno/deno_Fresh/datatable_01/islands/Body.tsx
@@ -5,6 +5,9 @@ export default function Body() {
   const [inputValue, setInputValue] = useState("");
   const count = useRef(0);
   const noteRef = useRef<HTMLInputElement | null>(null);
+  const rows = 0;
+  const cols = 0;
+
 
   useEffect(() => {
     count.current = count.current + 1;
@@ -16,41 +19,51 @@ export default function Body() {
 
 
   function createTableFromObjects(data: {}[]) {
-    let mystring=""
+    let mystring = ""
     const tablestart = ('<table>');
-    const tableend = ('</table>');
-    const headerRow = ('<tr>');
-    const headerEnd = ('<tr>');
+    const tableEnd = ('</table>');
+    const rowStart = ('<tr>');
+    const rowEnd = ('</tr>');
 
 
-    mystring+=tablestart;
+    mystring += tablestart;
 
 
-    for (var cell of data) {
-      console.log(product.product_desc)
- }
+    for (const cell of data) {
+      console.log(cell);
 
+      mystring += rowStart;
+      const keys = Object.keys(data[0]);
 
-
-    // Create table header row
-    const keys = Object.keys(data[0]);
-    for (const key of keys) {
-      const headerCell = document.createElement('th');
-      headerCell.textContent = key;
-      headerRow.appendChild(headerCell);
-    }
-    table.appendChild(headerRow);
-
-    // Create table data rows
-    for (const obj of data) {
-      const dataRow = document.createElement('tr');
       for (const key of keys) {
-        const dataCell = document.createElement('td');
-        dataCell.textContent = obj[key];
-        dataRow.appendChild(dataCell);
+        console.log(key);
+
       }
-      table.appendChild(dataRow);
+      mystring += rowEnd;
     }
+
+    mystring += tableEnd;
+
+
+    // // Create table header row
+    // const keys = Object.keys(data[0]);
+    // for (const key of keys) {
+    //   const headerCell = document.createElement('th');
+    //   headerCell.textContent = key;
+    //   headerRow.appendChild(headerCell);
+    // }
+    // table.appendChild(headerRow);
+
+    // // Create table data rows
+    // for (const obj of data) {
+    //   const dataRow = document.createElement('tr');
+    //   for (const key of keys) {
+    //     const dataCell = document.createElement('td');
+    //     dataCell.textContent = obj[key];
+    //     dataRow.appendChild(dataCell);
+    //   }
+    //   table.appendChild(dataRow);
+    // }
 
     return mystring;
   }
@@ -71,6 +84,26 @@ export default function Body() {
         {table}
       </div>
 
+      <div class="flex gap-2 w-full">
+      <table class="border-collapse border border-slate-400 ...">
+
+          {
+            [1, 2, 3, 4, 5, 6].map((value, index) => {
+              return <div key={index}>{value}</div>
+            })
+          }
+
+          {
+            [1, 2, 3, 4, 5, 6].map((value, index) => {
+              return <tr key={index}>{value}
+                <td>Alfreds Futterkiste</td>
+                <td>{value}</td>
+              </tr>
+            })
+          }
+
+        </table>
+      </div>
 
       <div>
         value={inputValue}
