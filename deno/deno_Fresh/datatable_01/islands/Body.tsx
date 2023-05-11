a41203e4d55f658bfb999ffd0986c4f8a3cdd1ed
atmelino <you@example.com>
2023-05-11 11:51:09 -0500

    u

--- deno/deno_Fresh/datatable_01/islands/Body.tsx
+++ deno/deno_Fresh/datatable_01/islands/Body.tsx
@@ -7,9 +7,10 @@ export default function Body() {
   const rows = 0;
   const cols = 0;
   const dataArray = [
-    { id: 1, name: 'John', age: 30 ,rand: 0},
-    { id: 2, name: 'Jane', age: 28 ,rand: 3},
-    { id: 3, name: 'Doe', age: 45 ,rand: 4},
+    { id: 1, name: "John", age: 30, rand: 0 },
+    { id: 2, name: "Jane", age: 28, rand: 3 },
+    { id: 3, name: "Doe", age: 45, rand: 4 },
+    { id: 4, name: "Cindy", age: 35, rand: 1 },
   ];
 
   // console.log(JSON.stringify(dataArray));
@@ -17,9 +18,10 @@ export default function Body() {
   for (const data of dataArray) {
     const keys = Object.keys(data);
     console.log("The use of map is as follows=");
-    keys.map(k => { console.log(data[k as keyof typeof data]) })
+    keys.map((k) => {
+      console.log(data[k as keyof typeof data]);
+    });
   }
-
 
   for (const data of dataArray) {
     // console.log(data);
@@ -27,7 +29,7 @@ export default function Body() {
     const someObj = data;
     const keys = Object.keys(data);
     for (const key of keys) {
-      const temp = someObj[key as keyof typeof someObj]
+      const temp = someObj[key as keyof typeof someObj];
       // console.log(key + " : " + temp);
     }
   }
@@ -36,67 +38,36 @@ export default function Body() {
     count.current = count.current + 1;
   });
 
-
-
+  const keys = Object.keys(dataArray[0]);
 
   return (
     <div class="flex flex-col w-full pt-5">
-
       <div class="flex gap-2 w-full">
         <table class="border-2">
-          {
-            dataArray.map((data, index) => {
-              // console.log(value);
-              const keys = Object.keys(data);
+          <tr class="border-2">
+            {keys.map((data, index) => {
+              // console.log(keys[index]);
+              return <th class="border-2">{keys[index]}</th>;
+            })}
+          </tr>
 
-              return <tr class="border-2" key={index}>{data}
-                {
-                  keys.map(k => {
-
-                    // <td>{data[k as keyof typeof data]}</td>
-
-                    console.log(data[k as keyof typeof data])
-
-                    return <td  class="border-2">{data[k as keyof typeof data]}</td>
-
-
-                  })
-                }
+          {dataArray.map((data, index) => {
+            // console.log(data);
+            // const keys = Object.keys(data);
+            return (
+              <tr class="border-2" key={index}>
+                {keys.map((k) => {
+                  console.log(data[k as keyof typeof data]);
+                  return (
+                    <td class="border-2">{data[k as keyof typeof data]}</td>
+                  );
+                })}
               </tr>
-            })
-          }
+            );
+          })}
+
         </table>
       </div>
-
-
-
-      <div class="flex gap-2 w-full">
-        <table class="border-2">
-          {
-            dataArray.map((value, index) => {
-              // console.log(value);
-              return <tr class="border-2" key={index}>{value}
-                <td>cell content</td>
-                <td>{value}</td>
-                <td class="border-2" >{JSON.stringify(value)}</td>
-              </tr>
-            })
-          }
-        </table>
-      </div>
-
-      <div class="flex gap-2 w-full">
-        <table class=" border-2">
-          {
-            [1, 2, 3, 4, 5, 6].map((value, index) => {
-              return <div key={index}>{value}</div>
-            })
-          }
-        </table>
-      </div>
-
     </div>
   );
 }
-
-
