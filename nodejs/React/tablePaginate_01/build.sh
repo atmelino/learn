f3cce84afddd80014b6d6bc52f139fd017e0530f
atmelino <you@example.com>
2023-05-16 23:03:53 -0500

    u

--- nodejs/React/tablePaginate_01/build.sh
+++ nodejs/React/tablePaginate_01/build.sh
@@ -1,12 +1,12 @@
 
 echo build
-echo boilerplate_tsx_01
+echo tablePaginate_01
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-cd /media/data/github/learn/nodejs/React/boilerplate_tsx_01
+cd /media/data/github/learn/nodejs/React/tablePaginate_01
 
 npm install
 
--- nodejs/React/tablePaginate_01/src/App.tsx
+++ nodejs/React/tablePaginate_01/src/App.tsx
@@ -1,51 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { myData } from "./myData.jsx"
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { myData } from "./myData.jsx";
 
 function App() {
-
   const [features, setFeatures] = useState([
     {
       "userId": 1,
       "id": 1,
       "title": "delectus aut autem",
-      "completed": false
+      "completed": false,
     },
-  ]
-  )
+  ]);
   const [html1, sethtml1] = useState("");
 
   // console.log(myData);
 
+  // for (const x of myData) {
+  // console.log(x);
+  // setFeatures(features.push(x))
 
-  for (const x of myData) {
-    console.log(x);
-    // setFeatures(features.push(x))
-
-  }
+  // }
 
   const data = myData;
-  const postsPerPage = 10;
+  const postsPerPage = 15;
   const currentPage = 1;
 
   function showData() {
     const indexOfLastPage = currentPage * postsPerPage;
     const indexOfFirstPage = indexOfLastPage - postsPerPage;
-    const currentPosts = data.slice(indexOfFirstPage, indexOfLastPage)
+    const currentPosts = data.slice(indexOfFirstPage, indexOfLastPage);
     // try {
-    return currentPosts.map((item: { userId: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; title: string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; completed: { toString: () => string | number | boolean | React.ReactElement<any, string | React.JSXElementConstructor<any>> | React.ReactFragment | React.ReactPortal | null | undefined; }; }, index: number) => {
-      return (
-        <tbody>
-          <tr>
-            <td>{postsPerPage * (currentPage - 1) + index + 1}</td>
-            <td>{item.userId}</td>
-            <td>{item.title}</td>
-            <td>{item.completed.toString()}</td>
-          </tr>
-        </tbody>
-      )
-    })
+    return currentPosts.map(
+      (
+        item: {
+          userId: number;
+          title: string;
+          completed: boolean;
+        },
+        index: number,
+      ) => {
+        return (
+          <tbody key={index}>
+            <tr>
+              <td>{postsPerPage * (currentPage - 1) + index + 1}</td>
+              <td>{item.userId}</td>
+              <td>{item.title}</td>
+              <td>{item.completed.toString()}</td>
+            </tr>
+          </tbody>
+        );
+      },
+    );
     // } catch (e) {
     //   alert(e.message)
     // }
@@ -57,16 +62,14 @@ function App() {
     //    setFeatures(myData);
 
     // sethtml1(showData());
-
-
-
-  }, [])
+  }, []);
 
   return (
     <div className="App">
-      {/* <header className="App-header">
-      </header> */}
-
+      {
+        /* <header className="App-header">
+      </header> */
+      }
 
       <div className="container">
         <table className="table align-items-center justify-content-center mb-0">
@@ -78,16 +81,13 @@ function App() {
               <th>Completed</th>
             </tr>
           </thead>
-          {html1}
+          {showData()}
         </table>
 
-        <div style={{ float: 'right' }}>
+        <div style={{ float: "right" }}>
           {/* {this.showPagination()} */}
         </div>
-
       </div>
-
-
     </div>
   );
 }
