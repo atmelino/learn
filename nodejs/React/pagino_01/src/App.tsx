b369dc8f960ee3ad3d4189be039d9f4d1bf12f83
atmelino <2985251+atmelino@users.noreply.github.com>
2023-05-21 20:07:56 -0500

    u

--- nodejs/React/pagino_01/src/App.tsx
+++ nodejs/React/pagino_01/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useMemo, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Pagino from "pagino";
@@ -6,13 +6,55 @@ import Pagino from "pagino";
 function App() {
   const [pages, setPages] = useState([]);
 
+  const pagino = useMemo(() => {
+    const _ = new Pagino({
+      onChange: (page, count) => setPages(_.getPages())
+    });
+
+    _.setCount(10);
+
+    return _;
+  }, []);
+
+  const hanglePaginoNavigation = (type) => {
+    if (typeof type === "string") {
+      pagino[type]?.();
+      return;
+    }
+
+    pagino.setPage(type);
+  };
+
+  const renderElement = (page) => {
+    if (page === "start-ellipsis" || page === "end-ellipsis") {
+      return <button key={page}>...</button>;
+    }
+
+    return (
+      <button
+        style={{
+          backgroundColor: page === pagino.page ? "#0971f1" : ""
+        }}
+        key={page}
+        onClick={() => hanglePaginoNavigation(page)}
+      >
+        {page}
+      </button>
+    );
+  };
+
 
 
   return (
     <div className="App">
       Learn React
+      <div>
+        <h1>Page: {pagino.page}</h1>
+        <ul>{pages.map(renderElement)}</ul>
+      </div>
 
     </div>
+
   );
 }
 
