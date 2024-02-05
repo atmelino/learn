62b4305a28dae04c9d93954d851c5cf9726f0cd0
atmelino <atmelino@users.noreply.github.com>
2024-02-04 18:46:24 -0600

    update

--- ai/tensorflow/tutorial/tf_02.py
+++ ai/tensorflow/tutorial/tf_02.py
@@ -10,7 +10,42 @@ pp = pprint.PrettyPrinter(width=41, compact=True)
 mnist = tf.keras.datasets.mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+print('Shape of X_train: ', x_train.shape)
+print('Shape of y_train: ', y_train.shape)
+print('Shape of X_test: ', x_test.shape)
+print('Shape of y_test: ', y_test.shape)
+
+pp.pprint(x_test)
+
+pp.pprint(x_test[0][0])
+pp.pprint(x_test[0][0][0])
+mystring=pprint.pformat(x_test[0][0], indent=1, width=200, depth=None)
+print(mystring)
+
+
+# import sys
+# import pprint as PP
+# PP.pprint(x_test[0][0],width=sys.maxsize,compact=True)
+
+new=x_test[0][0]
+pp.pprint(new)
+
+
+
+pp.pprint(y_train)
+mystring=pprint.pformat(y_train, indent=1, width=200, depth=None)
+print(mystring)
+
+
+
+
+
 x_train, x_test = x_train / 255.0, x_test / 255.0
+
+
+
+
 
 #print(x_train)
 #print(y_train)
@@ -25,13 +60,4 @@ x_train, x_test = x_train / 255.0, x_test / 255.0
 
 # pp.pprint(x_test[0])
 
-
-
-
-pp.pprint(x_test[0][0])
-
-pp.pprint(x_test[0][0][0])
-
-mystring=pprint.pformat(x_test[0][0], indent=1, width=200, depth=None)
-print(mystring)
 
