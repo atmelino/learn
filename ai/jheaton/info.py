e056eb21a2b4d59709d0e36a6c5dbfbaae03cc1e
atmelino <tmbscience@protonmail.com>
2024-02-14 00:45:12 -0600

    update

--- ai/jheaton/info.py
+++ ai/jheaton/info.py
@@ -7,8 +7,7 @@ import sklearn as sk
 import tensorflow as tf
 
 print(f"Tensor Flow Version: {tf.__version__}")
-print(f"Keras Version: {tensorflow.keras.__version__}")
-print()
+#print(f"Keras Version: {tensorflow.keras.__version__}")
 print(f"Python {sys.version}")
 print(f"Pandas {pd.__version__}")
 print(f"Scikit-Learn {sk.__version__}")
