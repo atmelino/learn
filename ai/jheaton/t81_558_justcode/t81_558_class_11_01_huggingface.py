5f983b7cf787e81218acb0582564125ca3d03875
atmelino <atmelino@users.noreply.github.com>
2024-02-19 18:12:30 -0600

    u

--- ai/jheaton/t81_558_justcode/t81_558_class_11_01_huggingface.py
+++ ai/jheaton/t81_558_justcode/t81_558_class_11_01_huggingface.py
@@ -22,18 +22,14 @@ import pandas as pd
 from transformers import pipeline
 
 classifier = pipeline("text-classification")
-
 outputs = classifier(text)
 df=pd.DataFrame(outputs)
 print(df)
 
 
-
 # Entity Tagging
 text2 = "Abraham Lincoln was a president who lived in the United States."
-
 tagger = pipeline("ner", aggregation_strategy="simple")
-
 outputs = tagger(text2)
 df=pd.DataFrame(outputs)
 print(df)
@@ -41,10 +37,34 @@ print(df)
 
 reader = pipeline("question-answering")
 question = "What now shall fade?"
-
 outputs = reader(question=question, context=text)
 df=pd.DataFrame([outputs])
 print(df)
 
 
+translator = pipeline("translation_en_to_de",
+                      model="Helsinki-NLP/opus-mt-en-de")
+outputs = translator(text, clean_up_tokenization_spaces=True,
+                     min_length=100)
+print(outputs[0]['translation_text'])
 
+
+text2 = """
+An apple is an edible fruit produced by an apple tree (Malus domestica). 
+Apple trees are cultivated worldwide and are the most widely grown species 
+in the genus Malus. The tree originated in Central Asia, where its wild 
+ancestor, Malus sieversii, is still found today. Apples have been grown 
+for thousands of years in Asia and Europe and were brought to North America 
+by European colonists. Apples have religious and mythological significance 
+in many cultures, including Norse, Greek, and European Christian tradition.
+"""
+summarizer = pipeline("summarization")
+outputs = summarizer(text2, max_length=45,
+                     clean_up_tokenization_spaces=True)
+print(outputs[0]['summary_text'])
+
+
+from urllib.request import urlopen
+generator = pipeline("text-generation")
+outputs = generator(text, max_length=400)
+print(outputs[0]['generated_text'])
