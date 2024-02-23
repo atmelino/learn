b99156fb56dfdd052e7bb0c2a4bf1f1fe0f79039
atmelino <atmelino@users.noreply.github.com>
2024-02-22 18:56:07 -0600

    update

--- ai/jheaton/t81_558_justcode/t81_558_class_02_1_python_pandas.py
+++ ai/jheaton/t81_558_justcode/t81_558_class_02_1_python_pandas.py
@@ -2,13 +2,17 @@
 import os
 import pandas as pd
 
-pd.set_option('display.max_columns', 7)
+pd.set_option('display.max_columns', 8)
 df = pd.read_csv(
     "https://data.heatonresearch.com/data/t81-558/auto-mpg.csv")
 print(df[0:5])
 
 
 
-pd.set_option('display.max_columns', 8)
-pd.set_option('display.max_rows', 5)
-print(df)
+# pd.set_option('display.max_columns', 8)
+# print(df)
+
+
+# pd.set_option('display.max_rows', 5)
+# print(df)
+
