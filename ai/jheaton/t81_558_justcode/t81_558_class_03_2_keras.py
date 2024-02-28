ad57aafca9772f42fe3a92d55639b6b3ff753d5e
atmelino <atmelino@users.noreply.github.com>
2024-02-27 19:35:39 -0600

    u

--- ai/jheaton/t81_558_justcode/t81_558_class_03_2_keras.py
+++ ai/jheaton/t81_558_justcode/t81_558_class_03_2_keras.py
@@ -39,63 +39,63 @@ print(sub.numpy())
 print()
 
 
-# import tensorflow as tf
-# import numpy as np
+import tensorflow as tf
+import numpy as np
+import PIL.Image
+from io import BytesIO
+from IPython.display import Image, display
 
-# import PIL.Image
-# from io import BytesIO
-# from IPython.display import Image, display
+def render(a):
+  a_cyclic = (a*0.3).reshape(list(a.shape)+[1])
+  img = np.concatenate([10+20*np.cos(a_cyclic),
+                        30+50*np.sin(a_cyclic),
+                        155-80*np.cos(a_cyclic)], 2)
+  img[a==a.max()] = 0
+  a = img
+  a = np.uint8(np.clip(a, 0, 255))
+  f = BytesIO()
+  return PIL.Image.fromarray(a)
 
-# def render(a):
-#   a_cyclic = (a*0.3).reshape(list(a.shape)+[1])
-#   img = np.concatenate([10+20*np.cos(a_cyclic),
-#                         30+50*np.sin(a_cyclic),
-#                         155-80*np.cos(a_cyclic)], 2)
-#   img[a==a.max()] = 0
-#   a = img
-#   a = np.uint8(np.clip(a, 0, 255))
-#   f = BytesIO()
-#   return PIL.Image.fromarray(a)
-
-# #@tf.function
-# def mandelbrot_helper(grid_c, current_values, counts,cycles):
+#@tf.function
+def mandelbrot_helper(grid_c, current_values, counts,cycles):
   
-#   for i in range(cycles):
-#     temp = current_values*current_values + grid_c
-#     not_diverged = tf.abs(temp) < 4
-#     current_values.assign(temp),
-#     counts.assign_add(tf.cast(not_diverged, tf.float32))
+  for i in range(cycles):
+    temp = current_values*current_values + grid_c
+    not_diverged = tf.abs(temp) < 4
+    current_values.assign(temp),
+    counts.assign_add(tf.cast(not_diverged, tf.float32))
 
-# def mandelbrot(render_size,center,zoom,cycles):
-#   f = zoom/render_size[0]
-#   real_start = center[0]-(render_size[0]/2)*f
-#   real_end = real_start + render_size[0]*f 
-#   imag_start = center[1]-(render_size[1]/2)*f
-#   imag_end = imag_start + render_size[1]*f 
+def mandelbrot(render_size,center,zoom,cycles):
+  f = zoom/render_size[0]
+  real_start = center[0]-(render_size[0]/2)*f
+  real_end = real_start + render_size[0]*f 
+  imag_start = center[1]-(render_size[1]/2)*f
+  imag_end = imag_start + render_size[1]*f 
 
-#   real_range = tf.range(real_start,real_end,f,dtype=tf.float64)
-#   imag_range = tf.range(imag_start,imag_end,f,dtype=tf.float64)
-#   real, imag = tf.meshgrid(real_range,imag_range)
-#   grid_c = tf.constant(tf.complex(real, imag))
-#   current_values = tf.Variable(grid_c)
-#   counts = tf.Variable(tf.zeros_like(grid_c, tf.float32))
+  real_range = tf.range(real_start,real_end,f,dtype=tf.float64)
+  imag_range = tf.range(imag_start,imag_end,f,dtype=tf.float64)
+  real, imag = tf.meshgrid(real_range,imag_range)
+  grid_c = tf.constant(tf.complex(real, imag))
+  current_values = tf.Variable(grid_c)
+  counts = tf.Variable(tf.zeros_like(grid_c, tf.float32))
 
-#   mandelbrot_helper(grid_c, current_values,counts,cycles)
-#   return counts.numpy()
+  mandelbrot_helper(grid_c, current_values,counts,cycles)
+  return counts.numpy()
 
-# counts = mandelbrot(
-#     #render_size=(3840,2160), # 4K
-#     #render_size=(1920,1080), # HD
-#     render_size=(640,480),
-#     center=(-0.5,0),
-#     zoom=4,
-#     cycles=200
-# )  
-# img = render(counts)
-# print(img.size)
-# img
+counts = mandelbrot(
+    #render_size=(3840,2160), # 4K
+    #render_size=(1920,1080), # HD
+    render_size=(640,480),
+    center=(-0.5,0),
+    zoom=4,
+    cycles=200
+)  
+img = render(counts)
+print(img.size)
+img
 
-# img.save("test.png")
+img.save("jheaton/t81_558_justcode/images/mandel.png")
+# img.save('jheaton/t81_558_justcode/large_csv/madel01.png')
 
 
 
