c961aa6f62f819f38cc5cabbcb1cdab3d67265a8
atmelino <atmelino@users.noreply.github.com>
2024-02-27 19:54:55 -0600

    u

--- ai/jheaton/t81_558_justcode/t81_558_class_03_2_keras.py
+++ ai/jheaton/t81_558_justcode/t81_558_class_03_2_keras.py
@@ -99,39 +99,40 @@ img.save("jheaton/t81_558_justcode/images/mandel.png")
 
 
 
+print("Simple TensorFlow Regression: MPG")
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+import pandas as pd
+import io
+import os
+import requests
+import numpy as np
+from sklearn import metrics
 
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras.layers import Dense, Activation
-# import pandas as pd
-# import io
-# import os
-# import requests
-# import numpy as np
-# from sklearn import metrics
+df = pd.read_csv(
+    "https://data.heatonresearch.com/data/t81-558/auto-mpg.csv", 
+    na_values=['NA', '?'])
+print(df)
 
-# df = pd.read_csv(
-#     "https://data.heatonresearch.com/data/t81-558/auto-mpg.csv", 
-#     na_values=['NA', '?'])
-# print(df)
+cars = df['name']
 
-# cars = df['name']
+# Handle missing value
+df['horsepower'] = df['horsepower'].fillna(df['horsepower'].median())
 
-# # Handle missing value
-# df['horsepower'] = df['horsepower'].fillna(df['horsepower'].median())
+# Pandas to Numpy
+x = df[['cylinders', 'displacement', 'horsepower', 'weight',
+       'acceleration', 'year', 'origin']].values
+y = df['mpg'].values # regression
 
-# # Pandas to Numpy
-# x = df[['cylinders', 'displacement', 'horsepower', 'weight',
-#        'acceleration', 'year', 'origin']].values
-# y = df['mpg'].values # regression
-
-# # Build the neural network
-# model = Sequential()
-# model.add(Dense(25, input_dim=x.shape[1], activation='relu')) # Hidden 1
-# model.add(Dense(10, activation='relu')) # Hidden 2
-# model.add(Dense(1)) # Output
-# model.compile(loss='mean_squared_error', optimizer='adam')
-# model.fit(x,y,verbose=2,epochs=100)
-
+# Build the neural network
+model = Sequential()
+model.add(Dense(25, input_dim=x.shape[1], activation='relu')) # Hidden 1
+model.add(Dense(10, activation='relu')) # Hidden 2
+model.add(Dense(1)) # Output
+model.compile(loss='mean_squared_error', optimizer='adam')
+model.fit(x,y,verbose=2,epochs=100)
+print("model.fit")
+print()
 
 # pred = model.predict(x)
 # print(f"Shape: {pred.shape}")
