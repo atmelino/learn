db9d9352d956e5ebb4490557837524cd2d0dfbfa
atmelino <atmelino@users.noreply.github.com>
2024-02-29 18:32:39 -0600

    u

---
+++ ai/jheaton/practice/path.py
@@ -0,0 +1,14 @@
+import os
+import os.path
+
+# path="."
+# basename = os.path.basename(path)
+# print("basename="+basename)
+# dirname=os.path.dirname(path)
+# print("dirname="+dirname)
+print("path of current file="+__file__)
+# print(__file__) 
+
+
+
+
