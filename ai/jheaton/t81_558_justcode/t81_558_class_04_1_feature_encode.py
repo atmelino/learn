fc6be747e675ba19aaa04be48b306b9ecb2349bf
atmelino <atmelino@users.noreply.github.com>
2024-03-08 18:35:13 -0600

    u

---
+++ ai/jheaton/t81_558_justcode/t81_558_class_04_1_feature_encode.py
@@ -0,0 +1,67 @@
+import pandas as pd
+
+pd.set_option('display.expand_frame_repr', False)
+print_number=4
+
+pd.set_option('display.max_columns', 7) 
+pd.set_option('display.max_rows', 5)
+
+df = pd.read_csv(
+    "https://data.heatonresearch.com/data/t81-558/jh-simple-dataset.csv",
+    na_values=['NA','?'])
+
+pd.set_option('display.max_columns', 9)
+pd.set_option('display.max_rows', 5)
+
+if print_number==1:
+    # display(df)
+    print(df)
+
+
+
+pd.set_option('display.max_columns', 7) 
+pd.set_option('display.max_rows', 5)
+
+dummies = pd.get_dummies(df['job'],prefix="job")
+print(dummies.shape)
+
+pd.set_option('display.max_columns', 9)
+pd.set_option('display.max_rows', 10)
+
+if print_number==2:
+    # display(dummies)
+    print(dummies)
+
+
+pd.set_option('display.max_columns', 7) 
+pd.set_option('display.max_rows', 5)
+
+df = pd.concat([df,dummies],axis=1)
+df.drop('job', axis=1, inplace=True)
+
+pd.set_option('display.max_columns', 9)
+pd.set_option('display.max_rows', 10)
+
+if print_number==3:
+    # display(df)
+    print(df)
+
+pd.set_option('display.max_columns', 7) 
+pd.set_option('display.max_rows', 5)
+
+df = pd.concat([df,pd.get_dummies(df['area'],prefix="area")],axis=1)
+df.drop('area', axis=1, inplace=True)
+
+pd.set_option('display.max_columns', 9)
+pd.set_option('display.max_rows', 10)
+
+if print_number==4:
+    # display(df)
+    print(df)
+
+
+
+
+
+
+
