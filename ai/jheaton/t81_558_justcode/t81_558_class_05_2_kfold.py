14b237544ee0b5387091b0a36ef4a411dbf7f0f9
atmelino <tmbscience@protonmail.com>
2024-09-19 20:37:52 -0500

    u

--- ai/jheaton/t81_558_justcode/t81_558_class_05_2_kfold.py
+++ ai/jheaton/t81_558_justcode/t81_558_class_05_2_kfold.py
@@ -33,8 +33,67 @@ df['aspect'] = zscore(df['aspect'])
 df['save_rate'] = zscore(df['save_rate'])
 df['subscriptions'] = zscore(df['subscriptions'])
 
+print(df.head())
+
+
 # Convert to numpy - Classification
 x_columns = df.columns.drop('age').drop('id')
 x = df[x_columns].values
 y = df['age'].values
 
+
+EPOCHS=500
+
+import pandas as pd
+import os
+import numpy as np
+from sklearn import metrics
+from scipy.stats import zscore
+from sklearn.model_selection import KFold
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+
+# Cross-Validate
+kf = KFold(5, shuffle=True, random_state=42) # Use for KFold classification
+oos_y = []
+oos_pred = []
+
+fold = 0
+for train, test in kf.split(x):
+    fold+=1
+    print(f"Fold #{fold}")
+        
+    x_train = x[train]
+    y_train = y[train]
+    x_test = x[test]
+    y_test = y[test]
+    
+    model = Sequential()
+    model.add(Dense(20, input_dim=x.shape[1], activation='relu'))
+    model.add(Dense(10, activation='relu'))
+    model.add(Dense(1))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    
+    model.fit(x_train,y_train,validation_data=(x_test,y_test),verbose=0,
+              epochs=EPOCHS)
+    
+    pred = model.predict(x_test)
+    
+    oos_y.append(y_test)
+    oos_pred.append(pred)    
+
+    # Measure this fold's RMSE
+    score = np.sqrt(metrics.mean_squared_error(pred,y_test))
+    print(f"Fold score (RMSE): {score}")
+
+# Build the oos prediction list and calculate the error.
+oos_y = np.concatenate(oos_y)
+oos_pred = np.concatenate(oos_pred)
+score = np.sqrt(metrics.mean_squared_error(oos_pred,oos_y))
+print(f"Final, out of sample score (RMSE): {score}")    
+    
+# Write the cross-validated prediction
+oos_y = pd.DataFrame(oos_y)
+oos_pred = pd.DataFrame(oos_pred)
+oosDF = pd.concat( [df, oos_y, oos_pred],axis=1 )
+#oosDF.to_csv(filename_write,index=False)
