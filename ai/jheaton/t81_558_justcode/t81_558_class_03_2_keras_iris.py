510a3b29d52735d7e8ee9146a1cf262a3583791b
atmelino <tmbscience@protonmail.com>
2024-09-29 18:51:32 -0500

    u

---
+++ ai/jheaton/t81_558_justcode/t81_558_class_03_2_keras_iris.py
@@ -0,0 +1,89 @@
+import os.path
+import pandas as pd
+import io
+import requests
+import numpy as np
+from sklearn import metrics
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras.callbacks import EarlyStopping
+
+# df = pd.read_csv(
+#     "https://data.heatonresearch.com/data/t81-558/iris.csv",
+#     na_values=['NA', '?'])
+df = pd.read_csv("./input/iris.csv", na_values=["NA", "?"])
+
+# Convert to numpy - Classification
+x = df[["sepal_l", "sepal_w", "petal_l", "petal_w"]].values
+dummies = pd.get_dummies(df["species"])  # Classification
+species = dummies.columns
+y = dummies.values
+
+
+# Build neural network
+model = Sequential()
+model.add(Dense(50, input_dim=x.shape[1], activation="relu"))  # Hidden 1
+model.add(Dense(25, activation="relu"))  # Hidden 2
+model.add(Dense(y.shape[1], activation="softmax"))  # Output
+
+model.compile(loss="categorical_crossentropy", optimizer="adam")
+model.fit(x, y, verbose=2, epochs=100)
+
+# Print out number of species found:
+
+print(species)
+
+pred = model.predict(x)
+print(f"Shape: {pred.shape}")
+print(pred[0:10])
+
+np.set_printoptions(suppress=True)
+print(y[0:10])
+
+predict_classes = np.argmax(pred, axis=1)
+expected_classes = np.argmax(y, axis=1)
+diff = predict_classes - expected_classes
+print(f"Predictions: {predict_classes}")
+print(f"Expected: {expected_classes}")
+print(f"diff: {diff}")
+
+print(species[predict_classes[1:70]])
+
+from sklearn.metrics import accuracy_score
+
+correct = accuracy_score(expected_classes, predict_classes)
+print(f"Accuracy: {correct}")
+
+sample_flower = np.array([[5.0, 3.0, 4.0, 2.0]], dtype=float)
+pred = model.predict(sample_flower)
+print(pred)
+pred = np.argmax(pred)
+print(f"Predict that {sample_flower} is: {species[pred]}")
+
+sample_flower = np.array([[5.0, 3.0, 4.0, 2.0], [5.2, 3.5, 1.5, 0.8]], dtype=float)
+pred = model.predict(sample_flower)
+print(pred)
+pred = np.argmax(pred, axis=1)
+print(f"Predict that these two flowers {sample_flower} ")
+print(f"are: {species[pred]}")
+
+
+# print(x.shape)
+# print(y.shape)
+
+# print("model.fit")
+# print()
+
+# pred = model.predict(x)
+# print(f"Shape: {pred.shape}")
+# print(pred[0:10])
+
+# # Measure RMSE error.  RMSE is common for regression.
+# score = np.sqrt(metrics.mean_squared_error(pred,y))
+# print(f"Final score (RMSE): {score}")
+
+
+# # Sample predictions
+# for i in range(10):
+#     print(f"{i+1}. Car name: {cars[i]}, MPG: {y[i]}, "
+#           + f"predicted MPG: {pred[i]}")
--- ai/practice/practice_t81_558_class_05_2_kfold_A.py
+++ ai/practice/practice_t81_558_class_05_2_kfold_A.py
@@ -34,94 +34,99 @@ start = 10
 stop = 50
 step = 10
 
-for length in range(start, stop, step):
-    df=df_original.iloc[0:length]
+start = 20
+stop = 100
+step = 20
 
-    print("original data set\n", df)
-    print(length)
+for length in range(start, stop, step):
+    df = df_original.iloc[0:length]
+
+    # print("original data set\n", df)
+    # print(length)
+
+    # Generate dummies for job
+    df = pd.concat([df, pd.get_dummies(df["job"], prefix="job")], axis=1)
+    df.drop("job", axis=1, inplace=True)
+
+    # Generate dummies for area
+    df = pd.concat([df, pd.get_dummies(df["area"], prefix="area")], axis=1)
+    df.drop("area", axis=1, inplace=True)
+
+    # Generate dummies for product
+    df = pd.concat([df, pd.get_dummies(df["product"], prefix="product")], axis=1)
+    df.drop("product", axis=1, inplace=True)
+
+    # Missing values for income
+    med = df["income"].median()
+    df["income"] = df["income"].fillna(med)
+
+    # Standardize ranges
+    df["income"] = zscore(df["income"])
+    df["aspect"] = zscore(df["aspect"])
+    df["save_rate"] = zscore(df["save_rate"])
+    df["subscriptions"] = zscore(df["subscriptions"])
+
+    if print_debug == True:
+        print("Table after feature vector encoding\n", df)
+    # print("Table after feature vector encoding\n", df.head())
+    # print("dataset size after feature vector encoding:\n", df.shape)
+
+    # Convert to numpy - Classification
+    x_columns = df.columns.drop("age").drop("id")
+    x = df[x_columns].values
+    y = df["age"].values
+
+    EPOCHS = 500
+    # EPOCHS = 100
+    number_of_folds = 2
+
+    # Cross-Validate
+    # kf = KFold(
+    #     number_of_folds, shuffle=True, random_state=42
+    # )
+
+    kf = KFold(number_of_folds)  # Use for KFold classification
+    print(kf)
+    oos_y = []
+    oos_pred = []
+
+    fold = 0
+    for train, test in kf.split(x):
+        fold += 1
+        print_debug = True
+        if print_debug == True:
+            print(f"Fold #{fold}")
+            print(f"  Train: index={train}")
+            print(f"  Test:  index={test}")
+        print_debug = False
+
+        x_train = np.asarray(x[train]).astype(np.float32)
+        y_train = np.asarray(y[train]).astype(np.float32)
+        x_test = np.asarray(x[test]).astype(np.float32)
+        y_test = np.asarray(y[test]).astype(np.float32)
+
+        model = Sequential()
+        model.add(Dense(20, input_dim=x.shape[1], activation="relu"))
+        model.add(Dense(10, activation="relu"))
+        model.add(Dense(1))
+        model.compile(loss="mean_squared_error", optimizer="adam")
+
+        model.fit(
+            x_train, y_train, validation_data=(x_test, y_test), verbose=0, epochs=EPOCHS
+        )
+
+        pred = model.predict(x_test)
+
+        oos_y.append(y_test)
+        oos_pred.append(pred)
+
+        # Measure this fold's RMSE
+        score = np.sqrt(metrics.mean_squared_error(pred, y_test))
+        print(f"Fold score (RMSE): {score}")
+
 
 exit()
 
-# Generate dummies for job
-df = pd.concat([df, pd.get_dummies(df["job"], prefix="job")], axis=1)
-df.drop("job", axis=1, inplace=True)
-
-# Generate dummies for area
-df = pd.concat([df, pd.get_dummies(df["area"], prefix="area")], axis=1)
-df.drop("area", axis=1, inplace=True)
-
-# Generate dummies for product
-df = pd.concat([df, pd.get_dummies(df["product"], prefix="product")], axis=1)
-df.drop("product", axis=1, inplace=True)
-
-# Missing values for income
-med = df["income"].median()
-df["income"] = df["income"].fillna(med)
-
-# Standardize ranges
-df["income"] = zscore(df["income"])
-df["aspect"] = zscore(df["aspect"])
-df["save_rate"] = zscore(df["save_rate"])
-df["subscriptions"] = zscore(df["subscriptions"])
-
-# print("Table after feature vector encoding\n", df.head())
-print("dataset size after feature vector encoding:\n", df.shape)
-
-# Convert to numpy - Classification
-x_columns = df.columns.drop("age").drop("id")
-x = df[x_columns].values
-y = df["age"].values
-
-
-
-
-EPOCHS = 500
-# EPOCHS = 100
-
-# Cross-Validate
-if original == True:
-    kf = KFold(5, shuffle=True, random_state=42)  # Use for KFold classification
-else:
-    kf = KFold(2)  # Use for KFold classification
-print(kf)
-oos_y = []
-oos_pred = []
-
-fold = 0
-for train, test in kf.split(x):
-    fold += 1
-    if print_debug == True:
-        print(f"Fold #{fold}")
-        print(f"  Train: index={train}")
-        print(f"  Test:  index={test}")
-    # x_train = x[train]
-    # y_train = y[train]
-    # x_test = x[test]
-    # y_test = y[test]
-
-    x_train = np.asarray(x[train]).astype(np.float32)
-    y_train = np.asarray(y[train]).astype(np.float32)
-    x_test = np.asarray(x[test]).astype(np.float32)
-    y_test = np.asarray(y[test]).astype(np.float32)
-
-    model = Sequential()
-    model.add(Dense(20, input_dim=x.shape[1], activation="relu"))
-    model.add(Dense(10, activation="relu"))
-    model.add(Dense(1))
-    model.compile(loss="mean_squared_error", optimizer="adam")
-
-    model.fit(
-        x_train, y_train, validation_data=(x_test, y_test), verbose=0, epochs=EPOCHS
-    )
-
-    pred = model.predict(x_test)
-
-    oos_y.append(y_test)
-    oos_pred.append(pred)
-
-    # Measure this fold's RMSE
-    score = np.sqrt(metrics.mean_squared_error(pred, y_test))
-    print(f"Fold score (RMSE): {score}")
 
 # Build the oos prediction list and calculate the error.
 oos_y = np.concatenate(oos_y)
