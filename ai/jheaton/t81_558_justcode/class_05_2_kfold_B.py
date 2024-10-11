297fa5aa7a124882bcb4177389d23a953867b0e4
atmelino <tmbscience@protonmail.com>
2024-10-11 18:55:32 -0500

    u

--- ai/jheaton/t81_558_justcode/class_05_2_kfold_B.py
+++ ai/jheaton/t81_558_justcode/class_05_2_kfold_B.py
@@ -8,6 +8,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Activation
 from sklearn.model_selection import KFold
 from tensorflow.keras.models import Sequential
+
 pd.set_option("display.max_rows", None)
 
 print("class_05_2_kfold_B")
@@ -15,10 +16,10 @@ print("class_05_2_kfold_B")
 # Options for this run
 shuffle = False
 print_fold = True
-length=2000
-folds=5
-length=100
-folds=3
+length = 2000
+folds = 5
+length = 100
+folds = 3
 
 # Read the data set
 df_original = pd.read_csv(
@@ -32,41 +33,49 @@ print("dataset original size:\n", df_original.shape)
 df = df_original.iloc[0:length]
 
 # Generate dummies for job
-df = pd.concat([df,pd.get_dummies(df['job'],prefix="job")],axis=1)
-df.drop('job', axis=1, inplace=True)
+df = pd.concat([df, pd.get_dummies(df["job"], prefix="job")], axis=1)
+df.drop("job", axis=1, inplace=True)
 
 # Generate dummies for area
-df = pd.concat([df,pd.get_dummies(df['area'],prefix="area")],axis=1)
-df.drop('area', axis=1, inplace=True)
+df = pd.concat([df, pd.get_dummies(df["area"], prefix="area")], axis=1)
+df.drop("area", axis=1, inplace=True)
 
 # Missing values for income
-med = df['income'].median()
-df['income'] = df['income'].fillna(med)
+med = df["income"].median()
+df["income"] = df["income"].fillna(med)
 
 # Standardize ranges
-df['income'] = zscore(df['income'])
-df['aspect'] = zscore(df['aspect'])
-df['save_rate'] = zscore(df['save_rate'])
-df['age'] = zscore(df['age'])
-df['subscriptions'] = zscore(df['subscriptions'])
+df["income"] = zscore(df["income"])
+df["aspect"] = zscore(df["aspect"])
+df["save_rate"] = zscore(df["save_rate"])
+df["age"] = zscore(df["age"])
+df["subscriptions"] = zscore(df["subscriptions"])
 df.to_csv("./output/class_05_2_B_jh-simple-dataset_06.csv", sep=",")
 
 # print("Table after feature vector encoding\n", df.head())
 print("dataset size after feature vector encoding:\n", df.shape)
 
 # Convert to numpy - Classification
-x_columns = df.columns.drop('product').drop('id')
+x_columns = df.columns.drop("product").drop("id")
 x = df[x_columns].values
-dummies = pd.get_dummies(df['product']) # Classification
+# print(x)
+dummies = pd.get_dummies(df["product"])  # Classification
 products = dummies.columns
 y = dummies.values
-print("shape of y=",y.shape)
-print("y=",y)
-
-EPOCHS=500
+print("shape of y=", y.shape)
+# print("y=",y)
 
 
-print(df['product'])
+col1 = pd.DataFrame(df, columns=["id"])
+col2 = pd.DataFrame(df, columns=["product"])
+col3 = pd.DataFrame(y)
+compare = pd.concat([col1, col2, col3], axis=1)
+print(compare)
+
+# print(df['product'])
+
+
+EPOCHS = 500
 
 
 # np.argmax(pred,axis=1)
@@ -78,12 +87,29 @@ oos_pred = []
 fold = 0
 
 # Must specify y StratifiedKFold for
-for train, test in kf.split(x,df['product']):
-    fold+=1
+for train, test in kf.split(x, df["product"]):
+    fold += 1
     print(f"Fold #{fold}")
     if print_fold == True:
-        print(f"  Train: index={train}")
-        print(f"  Test:  index={test}")
+        # print(f"  Train: index={train}")
+        # print(f"  Test:  index={test}")
+        print(f"  Train: index={train} size={train.shape}")
+        print(f"  Test:  index={test} size={test.shape}")
+
+    myarr=[]
+    for i in test:
+        print(i,df["product"][i])
+        myarr.append(i)
+    colp = pd.DataFrame(myarr)
+    print(colp)
+
+    test_product=df["product"][7]
+    test_product=df["product"][7]
+    test_product=df["product"][7]
+ 
+    print(test_product)
+    # print(test_product[test])
+    # print(df[test])
 
     # x_train = x[train]
     # y_train = y[train]
@@ -94,23 +120,24 @@ for train, test in kf.split(x,df['product']):
     y_train = np.asarray(y[train]).astype(np.float32)
     x_test = np.asarray(x[test]).astype(np.float32)
     y_test = np.asarray(y[test]).astype(np.float32)
-    print("y_test=",y_test)
+    print("y_test=", y_test)
 
     model = Sequential()
     # Hidden 1
-    model.add(Dense(50, input_dim=x.shape[1], activation='relu'))
-    model.add(Dense(25, activation='relu')) # Hidden 2
-    model.add(Dense(y.shape[1],activation='softmax')) # Output
-    model.compile(loss='categorical_crossentropy', optimizer='adam')
-    model.fit(x_train,y_train,validation_data=(x_test,y_test),
-    verbose=0, epochs=EPOCHS)
+    model.add(Dense(50, input_dim=x.shape[1], activation="relu"))
+    model.add(Dense(25, activation="relu"))  # Hidden 2
+    model.add(Dense(y.shape[1], activation="softmax"))  # Output
+    model.compile(loss="categorical_crossentropy", optimizer="adam")
+    model.fit(
+        x_train, y_train, validation_data=(x_test, y_test), verbose=0, epochs=EPOCHS
+    )
     pred = model.predict(x_test)
     oos_y.append(y_test)
     # raw probabilities to chosen class (highest probability)
-    pred = np.argmax(pred,axis=1)
+    pred = np.argmax(pred, axis=1)
     oos_pred.append(pred)
     # Measure this fold's accuracy
-    y_compare = np.argmax(y_test,axis=1) # For accuracy calculation
+    y_compare = np.argmax(y_test, axis=1)  # For accuracy calculation
     score = metrics.accuracy_score(y_compare, pred)
     print(f"Fold score (accuracy): {score}")
 
