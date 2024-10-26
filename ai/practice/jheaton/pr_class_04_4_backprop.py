193ac8aa7ee18083c9fb9c37d71061e1357c1bc2
atmelino <tmbscience@protonmail.com>
2024-10-26 18:55:57 -0500

    u

--- ai/practice/jheaton/pr_class_04_4_backprop.py
+++ ai/practice/jheaton/pr_class_04_4_backprop.py
@@ -40,70 +40,65 @@ for length in range(start, stop, step):
 
     # print(df)
 
-    if True:
+    # Generate dummies for job
+    df = pd.concat([df, pd.get_dummies(df["job"], prefix="job")], axis=1)
+    df.drop("job", axis=1, inplace=True)
+    df.to_csv("./output/jh-simple-dataset_02.csv", sep=",")
 
-        # Generate dummies for job
-        df = pd.concat([df, pd.get_dummies(df["job"], prefix="job")], axis=1)
-        df.drop("job", axis=1, inplace=True)
-        df.to_csv("./output/jh-simple-dataset_02.csv", sep=",")
+    # Generate dummies for area
+    df = pd.concat([df, pd.get_dummies(df["area"], prefix="area")], axis=1)
+    df.drop("area", axis=1, inplace=True)
+    df.to_csv("./output/jh-simple-dataset_03.csv", sep=",")
 
-        # Generate dummies for area
-        df = pd.concat([df, pd.get_dummies(df["area"], prefix="area")], axis=1)
-        df.drop("area", axis=1, inplace=True)
-        df.to_csv("./output/jh-simple-dataset_03.csv", sep=",")
+    # Generate dummies for product
+    df = pd.concat([df, pd.get_dummies(df["product"], prefix="product")], axis=1)
+    df.drop("product", axis=1, inplace=True)
+    df.to_csv("./output/jh-simple-dataset_04.csv", sep=",")
 
-        # Generate dummies for product
-        df = pd.concat([df, pd.get_dummies(df["product"], prefix="product")], axis=1)
-        df.drop("product", axis=1, inplace=True)
-        df.to_csv("./output/jh-simple-dataset_04.csv", sep=",")
+    # Missing values for income
+    med = df["income"].median()
+    df["income"] = df["income"].fillna(med)
+    df.to_csv("./output/jh-simple-dataset_05.csv", sep=",")
 
-        # Missing values for income
-        med = df["income"].median()
-        df["income"] = df["income"].fillna(med)
-        df.to_csv("./output/jh-simple-dataset_05.csv", sep=",")
+    # Standardize ranges
+    df["income"] = zscore(df["income"])
+    df["aspect"] = zscore(df["aspect"])
+    df["save_rate"] = zscore(df["save_rate"])
+    df["subscriptions"] = zscore(df["subscriptions"])
+    df.to_csv("./output/jh-simple-dataset_06.csv", sep=",")
 
-        # Standardize ranges
-        df["income"] = zscore(df["income"])
-        df["aspect"] = zscore(df["aspect"])
-        df["save_rate"] = zscore(df["save_rate"])
-        df["subscriptions"] = zscore(df["subscriptions"])
-        df.to_csv("./output/jh-simple-dataset_06.csv", sep=",")
+    # Convert to numpy - Classification
+    x_columns = df.columns.drop("age").drop("id")
+    x = df[x_columns].values
+    y = df["age"].values
 
-        # Convert to numpy - Classification
-        x_columns = df.columns.drop("age").drop("id")
-        x = df[x_columns].values
-        y = df["age"].values
+    x = x.astype("float32")
+    y = y.astype("float32")
 
-        x = x.astype("float32")
-        y = y.astype("float32")
+    # Create train/test
+    x_train, x_test, y_train, y_test = train_test_split(
+        x, y, test_size=0.25, random_state=42
+    )
 
-        # Create train/test
-        x_train, x_test, y_train, y_test = train_test_split(
-            x, y, test_size=0.25, random_state=42
-        )
+    EPOCHS = 500
 
-        EPOCHS = 500
+    # Build the neural network
+    model = Sequential()
+    model.add(Dense(25, input_dim=x.shape[1], activation="relu"))  # Hidden 1
+    model.add(Dense(10, activation="relu"))  # Hidden 2
+    model.add(Dense(1))  # Output
+    model.compile(loss="mean_squared_error", optimizer="adam")  # Modify here
+    model.fit(
+        x_train, y_train, validation_data=(x_test, y_test), verbose=0, epochs=EPOCHS
+    )
 
-        # Build the neural network
-        model = Sequential()
-        model.add(Dense(25, input_dim=x.shape[1], activation="relu"))  # Hidden 1
-        model.add(Dense(10, activation="relu"))  # Hidden 2
-        model.add(Dense(1))  # Output
-        model.compile(loss="mean_squared_error", optimizer="adam")  # Modify here
-        model.fit(
-            x_train, y_train, validation_data=(x_test, y_test), verbose=0, epochs=EPOCHS
-        )
-
-        pred = model.predict(x_test)
-        # print(y_test.shape)
-        # print(pred.shape)
-        # Measure this set's RMSE
-        score = np.sqrt(metrics.mean_squared_error(pred, y_test))
-        # print(f"Set score (RMSE): {score}")
-        print("Rows used=", length, ",   Set score (RMSE)=", score)
-
-exit()
-
+    pred = model.predict(x_test)
+    # print(y_test.shape)
+    # print(pred.shape)
+    # Measure this set's RMSE
+    score = np.sqrt(metrics.mean_squared_error(pred, y_test))
+    # print(f"Set score (RMSE): {score}")
+    print("Rows used=", length, ",   Set score (RMSE)=", score)
 
 col1 = pd.DataFrame(y_test, columns=["y_test"])
 col2 = pd.DataFrame(pred, columns=["pred"])
