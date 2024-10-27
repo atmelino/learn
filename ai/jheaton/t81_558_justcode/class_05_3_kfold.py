5f89acdd474a34edd41a4d8b90292978e9a586b8
atmelino <tmbscience@protonmail.com>
2024-10-27 18:50:21 -0500

    u

---
+++ ai/jheaton/t81_558_justcode/class_05_3_kfold.py
@@ -0,0 +1,105 @@
+import pandas as pd
+from scipy.stats import zscore
+import os
+import numpy as np
+from sklearn import metrics
+from sklearn.model_selection import KFold
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation
+from tensorflow.keras import regularizers
+
+print("class_05_3_kfold")
+
+# Options for this run
+length = 100
+folds = 2
+length = 2000
+folds = 5
+
+# Read the data set
+df_original = pd.read_csv(
+    "./input/jh-simple-dataset.csv",
+    na_values=["NA", "?"],
+)
+df = df_original.iloc[0:length]
+
+# Generate dummies for job
+df = pd.concat([df,pd.get_dummies(df['job'],prefix="job")],axis=1)
+df.drop('job', axis=1, inplace=True)
+# Generate dummies for area
+df = pd.concat([df,pd.get_dummies(df['area'],prefix="area")],axis=1)
+df.drop('area', axis=1, inplace=True)
+# Missing values for income
+med = df['income'].median()
+df['income'] = df['income'].fillna(med)
+# Standardize ranges
+df['income'] = zscore(df['income'])
+df['aspect'] = zscore(df['aspect'])
+df['save_rate'] = zscore(df['save_rate'])
+df['age'] = zscore(df['age'])
+df['subscriptions'] = zscore(df['subscriptions'])
+# Convert to numpy - Classification
+x_columns = df.columns.drop('product').drop('id')
+x = df[x_columns].values
+dummies = pd.get_dummies(df['product']) # Classification
+products = dummies.columns
+y = dummies.values
+
+# Cross-validate
+kf = KFold(5, shuffle=True, random_state=42)
+oos_y = []
+oos_pred = []
+fold = 0
+
+for train, test in kf.split(x):
+    fold+=1
+    print(f"Fold #{fold}")
+    # x_train = x[train]
+    # y_train = y[train]
+    # x_test = x[test]
+    # y_test = y[test]
+    x_train = np.asarray(x[train]).astype(np.float32)
+    y_train = np.asarray(y[train]).astype(np.float32)
+    x_test = np.asarray(x[test]).astype(np.float32)
+    y_test = np.asarray(y[test]).astype(np.float32)
+
+
+
+    #kernel_regularizer=regularizers.l2(0.01),
+    model = Sequential()
+    # Hidden 1
+    model.add(Dense(50, input_dim=x.shape[1],
+    activation='relu',
+    activity_regularizer=regularizers.l1(1e-4)))
+    # Hidden 2
+    model.add(Dense(25, activation='relu',
+    activity_regularizer=regularizers.l1(1e-4)))
+    # Output
+    model.add(Dense(y.shape[1],activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam')
+    model.fit(x_train,y_train,validation_data=(x_test,y_test),
+    verbose=0,epochs=500)
+    pred = model.predict(x_test)
+    oos_y.append(y_test)
+    # raw probabilities to chosen class (highest probability)
+    pred = np.argmax(pred,axis=1)
+    oos_pred.append(pred)
+    # Measure this fold's accuracy
+    y_compare = np.argmax(y_test,axis=1) # For accuracy calculation
+    score = metrics.accuracy_score(y_compare, pred)
+    print(f"Fold score (accuracy): {score}")
+    # Build the oos prediction list and calculate the error.
+    oos_y = np.concatenate(oos_y)
+    oos_pred = np.concatenate(oos_pred)
+    oos_y_compare = np.argmax(oos_y,axis=1) # For accuracy calculation
+    score = metrics.accuracy_score(oos_y_compare, oos_pred)
+    print(f"Final score (accuracy): {score}")
+    # Write the cross-validated prediction
+    oos_y = pd.DataFrame(oos_y)
+    oos_pred = pd.DataFrame(oos_pred)
+    oosDF = pd.concat( [df, oos_y, oos_pred],axis=1 )
+    #oosDF.to_csv(filename_write,index=False)
+
+
+
+
