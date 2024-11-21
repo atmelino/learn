9de3427c4a5534ef61180969127ff907b1822a7e
atmelino <tmbscience@protonmail.com>
2024-11-20 18:37:03 -0600

    u

---
+++ ai/jheaton/t81_558_justcode/t81_558_class_05_5_bootstrap_A.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from scipy.stats import zscore
+from sklearn.model_selection import train_test_split
+
+
+
+# Nicely formatted time string
+def hms_string(sec_elapsed):
+    h = int(sec_elapsed / (60 * 60))
+    m = int((sec_elapsed % (60 * 60)) / 60)
+    s = sec_elapsed % 60
+    return "{}:{:>02}:{:>05.2f}".format(h, m, s)
+
+# Read the data set
+df = pd.read_csv(
+"https://data.heatonresearch.com/data/t81-558/jh-simple-dataset.csv",
+na_values=['NA','?'])
+# Generate dummies for job
+df = pd.concat([df,pd.get_dummies(df['job'],prefix="job")],axis=1)
+df.drop('job', axis=1, inplace=True)
+# Generate dummies for area
+df = pd.concat([df,pd.get_dummies(df['area'],prefix="area")],axis=1)
+df.drop('area', axis=1, inplace=True)
+# Generate dummies for product
+df = pd.concat([df,pd.get_dummies(df['product'],prefix="product")],axis=1)
+df.drop('product', axis=1, inplace=True)
+# Missing values for income
+med = df['income'].median()
+df['income'] = df['income'].fillna(med)
+# Standardize ranges
+df['income'] = zscore(df['income'])
+df['aspect'] = zscore(df['aspect'])
+df['save_rate'] = zscore(df['save_rate'])
+df['subscriptions'] = zscore(df['subscriptions'])
+# Convert to numpy - Classification
+x_columns = df.columns.drop('age').drop('id')
+x = df[x_columns].values
+y = df['age'].values
+
+
+
+
+
+
+
+
