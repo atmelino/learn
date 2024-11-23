f852d4a765cd76116ad52a270c8e55b56140e008
atmelino <tmbscience@protonmail.com>
2024-11-22 20:41:18 -0600

    u

--- ai/jheaton/t81_558_justcode/class_05_5_bootstrap_A.py
+++ ai/jheaton/t81_558_justcode/class_05_5_bootstrap_A.py
@@ -15,12 +15,14 @@ from sklearn.model_selection import ShuffleSplit
 
 print("class_05_5_bootstrap_A")
 
+
 # Nicely formatted time string
 def hms_string(sec_elapsed):
     h = int(sec_elapsed / (60 * 60))
     m = int((sec_elapsed % (60 * 60)) / 60)
     s = sec_elapsed % 60
     return "{}:{:>02}:{:>05.2f}".format(h, m, s)
+
 
 # Options for this run
 length = 100
@@ -37,26 +39,26 @@ print("dataset original size:\n", df_original.shape)
 df = df_original.iloc[0:length]
 
 # Generate dummies for job
-df = pd.concat([df,pd.get_dummies(df['job'],prefix="job")],axis=1)
-df.drop('job', axis=1, inplace=True)
+df = pd.concat([df, pd.get_dummies(df["job"], prefix="job")], axis=1)
+df.drop("job", axis=1, inplace=True)
 # Generate dummies for area
-df = pd.concat([df,pd.get_dummies(df['area'],prefix="area")],axis=1)
-df.drop('area', axis=1, inplace=True)
+df = pd.concat([df, pd.get_dummies(df["area"], prefix="area")], axis=1)
+df.drop("area", axis=1, inplace=True)
 # Generate dummies for product
-df = pd.concat([df,pd.get_dummies(df['product'],prefix="product")],axis=1)
-df.drop('product', axis=1, inplace=True)
+df = pd.concat([df, pd.get_dummies(df["product"], prefix="product")], axis=1)
+df.drop("product", axis=1, inplace=True)
 # Missing values for income
-med = df['income'].median()
-df['income'] = df['income'].fillna(med)
+med = df["income"].median()
+df["income"] = df["income"].fillna(med)
 # Standardize ranges
-df['income'] = zscore(df['income'])
-df['aspect'] = zscore(df['aspect'])
-df['save_rate'] = zscore(df['save_rate'])
-df['subscriptions'] = zscore(df['subscriptions'])
+df["income"] = zscore(df["income"])
+df["aspect"] = zscore(df["aspect"])
+df["save_rate"] = zscore(df["save_rate"])
+df["subscriptions"] = zscore(df["subscriptions"])
 # Convert to numpy - Classification
-x_columns = df.columns.drop('age').drop('id')
+x_columns = df.columns.drop("age").drop("id")
 x = df[x_columns].values
-y = df['age'].values
+y = df["age"].values
 
 
 SPLITS = 50
@@ -69,8 +71,8 @@ num = 0
 # Loop through samples
 for train, test in boot.split(x):
     start_time = time.time()
-    num+=1
-    
+    num += 1
+
     # Split train and test
     # x_train = x[train]
     # y_train = y[train]
@@ -82,24 +84,35 @@ for train, test in boot.split(x):
     x_test = np.asarray(x[test]).astype(np.float32)
     y_test = np.asarray(y[test]).astype(np.float32)
 
-
     # Construct neural network
     model = Sequential()
-    model.add(Dense(20, input_dim=x_train.shape[1], activation='relu'))
-    model.add(Dense(10, activation='relu'))
+    model.add(Dense(20, input_dim=x_train.shape[1], activation="relu"))
+    model.add(Dense(10, activation="relu"))
     model.add(Dense(1))
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    monitor = EarlyStopping(monitor='val_loss', min_delta=1e-3,
-    patience=5, verbose=0, mode='auto', restore_best_weights=True)
+    model.compile(loss="mean_squared_error", optimizer="adam")
+    monitor = EarlyStopping(
+        monitor="val_loss",
+        min_delta=1e-3,
+        patience=5,
+        verbose=0,
+        mode="auto",
+        restore_best_weights=True,
+    )
     # Train on the bootstrap sample
-    model.fit(x_train,y_train,validation_data=(x_test,y_test),
-    callbacks=[monitor],verbose=0,epochs=1000)
+    model.fit(
+        x_train,
+        y_train,
+        validation_data=(x_test, y_test),
+        callbacks=[monitor],
+        verbose=0,
+        epochs=1000,
+    )
     epochs = monitor.stopped_epoch
     epochs_needed.append(epochs)
     # Predict on the out of boot (validation)
     pred = model.predict(x_test)
     # Measure this bootstrap's log loss
-    score = np.sqrt(metrics.mean_squared_error(pred,y_test))
+    score = np.sqrt(metrics.mean_squared_error(pred, y_test))
 
     mean_benchmark.append(score)
     m1 = statistics.mean(mean_benchmark)
@@ -107,10 +120,8 @@ for train, test in boot.split(x):
     mdev = statistics.pstdev(mean_benchmark)
     # Record this iteration
     time_took = time.time() - start_time
-    print(f"#{num}: score={score:.6f}, mean score={m1:.6f},"
-    f" stdev={mdev:.6f}",
-    f" epochs={epochs}, mean epochs={int(m2)}",
-    f" time={hms_string(time_took)}")
-
-
-
+    print(
+        f"#{num}: score={score:.6f}, mean score={m1:.6f}," f" stdev={mdev:.6f}",
+        f" epochs={epochs}, mean epochs={int(m2)}",
+        f" time={hms_string(time_took)}",
+    )
