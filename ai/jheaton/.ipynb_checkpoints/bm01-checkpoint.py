daa66d2c00340c287b5cc88045e1d781a7d2b46c
atmelino <tmbscience@protonmail.com>
2024-12-06 21:00:05 -0600

    u

--- ai/jheaton/.ipynb_checkpoints/bm01-checkpoint.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-from ai_benchmark import AIBenchmark
-# benchmark = AIBenchmark(verbose_level=3)
-# results = benchmark.run()
-print(f"Python {sys.version}")
-
-results = AIBenchmark().run()
-
