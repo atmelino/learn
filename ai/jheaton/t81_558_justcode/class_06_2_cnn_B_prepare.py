7fc4239bee443bf7d334aec2dcc939f36cb1d7bc
atmelino <tmbscience@protonmail.com>
2024-12-19 18:54:22 -0600

    u

--- ai/jheaton/t81_558_justcode/class_06_2_cnn_B_prepare.py
+++ ai/jheaton/t81_558_justcode/class_06_2_cnn_B_prepare.py
@@ -4,6 +4,7 @@ import os
 
 do_download=False
 do_mkdir=False
+do_unzip=True
 
 
 URL = "https://github.com/jeffheaton/data-mirror/releases/"
@@ -21,7 +22,7 @@ if do_download==True:
     # cmd = "wget --directory-prefix=./input "+DOWNLOAD_SOURCE
     cmd = "wget --directory-prefix="+PATH+" "+DOWNLOAD_SOURCE
     print(cmd)
-    # result: wget --directory-prefix=./not_on_github https://github.com/jeffheaton/data-mirror/releases/download/v1/paperclips.zip
+    # result: wget --directory-prefix=./not_on_github https://github.com/jeffheaton/data-mirror/releases/download/v1/iris-image.zip
     # os.system(cmd)
 
 
@@ -31,4 +32,11 @@ if do_mkdir==True:
     print(cmd1)
 
     os.system(cmd1)
+
+if do_unzip==True:
+    cmd="unzip -o -j -d "+SOURCE+" " +ZIPFILE+" >/dev/null "
+    print(cmd)
+    # unzip -o -j -d ./not_on_github/iris ./not_on_github/iris-image.zip >/dev/null 
+
+
 
