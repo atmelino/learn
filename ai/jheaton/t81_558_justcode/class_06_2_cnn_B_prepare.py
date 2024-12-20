ab10d35b42bbf4f84c4fd5d3ab4835c6b914233d
atmelino <tmbscience@protonmail.com>
2024-12-20 00:42:59 +0000

    update

--- ai/jheaton/t81_558_justcode/class_06_2_cnn_B_prepare.py
+++ ai/jheaton/t81_558_justcode/class_06_2_cnn_B_prepare.py
@@ -7,7 +7,7 @@ do_download=False
 
 
 URL = "https://github.com/jeffheaton/data-mirror/releases/"
-DOWNLOAD_SOURCE = URL+"/download/v1/iris-image.zip"
+DOWNLOAD_SOURCE = URL+"download/v1/iris-image.zip"
 print(DOWNLOAD_SOURCE)
 DOWNLOAD_NAME = DOWNLOAD_SOURCE[DOWNLOAD_SOURCE.rfind('/')+1:]
 
