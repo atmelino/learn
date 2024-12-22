25ed3c0e6d6a62da19e1ada1a766c8112e65a550
atmelino <tmbscience@protonmail.com>
2024-12-21 18:18:11 -0600

    u

--- ai/practice/keras/image/catdog.py
+++ ai/practice/keras/image/catdog.py
@@ -6,4 +6,38 @@ from tensorflow import data as tf_data
 import matplotlib.pyplot as plt
 
 
+imagedir="../not_on_github/PetImages"
+
+num_skipped = 0
+for folder_name in ("Cat", "Dog"):
+    folder_path = os.path.join(imagedir, folder_name)
+    for fname in os.listdir(folder_path):
+        fpath = os.path.join(folder_path, fname)
+        try:
+            fobj = open(fpath, "rb")
+            is_jfif = b"JFIF" in fobj.peek(10)
+        finally:
+            fobj.close()
+
+        if not is_jfif:
+            num_skipped += 1
+            # Delete corrupted image
+            os.remove(fpath)
+
+print(f"Deleted {num_skipped} images.")
+
+image_size = (180, 180)
+batch_size = 128
+
+train_ds, val_ds = keras.utils.image_dataset_from_directory(
+    imagedir,
+    validation_split=0.2,
+    subset="both",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+
+
+
 
