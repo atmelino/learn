df8617868ba9c41ae5dadeaf9b0a38bd5e6506d0
atmelino <tmbscience@protonmail.com>
2024-12-22 14:38:27 -0600

    u

--- ai/practice/keras/image/catdog.py
+++ ai/practice/keras/image/catdog.py
@@ -173,7 +173,7 @@ if plot==True:
 
 img_array = keras.utils.img_to_array(img)
 # img_array = keras.ops.expand_dims(img_array, 0)  # Create batch axis
-img_array = tf.ops.expand_dims(img_array, 0)  # Create batch axis
+img_array = tf.expand_dims(img_array, 0)  # Create batch axis
 
 predictions = model.predict(img_array)
 print(predictions)
