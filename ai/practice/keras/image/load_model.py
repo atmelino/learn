d5bcfc1198bc7a639d86ddd4b9f919a0893a05d9
atmelino <tmbscience@protonmail.com>
2024-12-24 17:08:52 -0600

    u

--- ai/practice/keras/image/load_model.py
+++ ai/practice/keras/image/load_model.py
@@ -1,21 +1,47 @@
 import os
+import pandas
 import keras
 from tensorflow.keras.models import load_model
 import tensorflow as tf
 
-imagedir="../not_on_github/PetImages"
+# Options for this run
+plot = False
+
+imagedir = "../not_on_github/PetImages"
 image_size = (180, 180)
 
-
 load_path = "../not_on_github/catdogsavemodel"
-model = load_model(os.path.join(load_path,"catdog01.h5"))
+model = load_model(os.path.join(load_path, "catdog01.h5"))
 
+test_images = [
+    "/Cat/22.jpg",
+    "/Cat/24.jpg",
+    "/Cat/25.jpg",
+    "/Cat/648.jpg",
+    "/Dog/0.jpg",
+    "/Cat/6779.jpg",
+    "/Dog/189.jpg",
+]
 
-img = keras.utils.load_img(imagedir+"/Cat/6779.jpg", target_size=image_size)
+result_table = []
 
-img_array = keras.utils.img_to_array(img)
-# img_array = keras.ops.expand_dims(img_array, 0)  # Create batch axis
-img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+for filename in test_images:
+    img = keras.utils.load_img(imagedir + filename, target_size=image_size)
 
-predictions = model.predict(img_array)
-print(predictions)
+    if plot == True:
+        print("show cat")
+        print(imagedir + filename)
+        plt.imshow(img)
+        plt.show()
+
+    img_array = keras.utils.img_to_array(img)
+    img_array = keras.ops.expand_dims(img_array, 0)  # Create batch axis
+
+    predictions = model.predict(img_array)
+    score = float(keras.ops.sigmoid(predictions[0][0]))
+
+    print(predictions)
+    print(imagedir + filename)
+    print(f"This image is {100 * (1 - score):.2f}% cat and {100 * score:.2f}% dog.")
+
+    result_table.append(imagedir + filename, predictions, score)
