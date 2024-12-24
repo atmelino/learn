47e4e61f990baaea27c18d2fa7c430d4247903e4
atmelino <tmbscience@protonmail.com>
2024-12-23 21:01:22 -0600

    u

--- ai/practice/keras/image/load_model.py
+++ ai/practice/keras/image/load_model.py
@@ -1,10 +1,21 @@
 import os
-
+import keras
 from tensorflow.keras.models import load_model
+import tensorflow as tf
 
+imagedir="../not_on_github/PetImages"
+image_size = (180, 180)
 
 
 load_path = "../not_on_github/catdogsavemodel"
-model2 = load_model(os.path.join(load_path,"catdog01.h5"))
+model = load_model(os.path.join(load_path,"catdog01.h5"))
 
 
+img = keras.utils.load_img(imagedir+"/Cat/6779.jpg", target_size=image_size)
+
+img_array = keras.utils.img_to_array(img)
+# img_array = keras.ops.expand_dims(img_array, 0)  # Create batch axis
+img_array = tf.expand_dims(img_array, 0)  # Create batch axis
+
+predictions = model.predict(img_array)
+print(predictions)
