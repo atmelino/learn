d08c08f7f3f22c19577b9353bc55c800f6bf4369
atmelino <tmbscience@protonmail.com>
2024-12-27 19:52:34 -0600

    u

--- ai/jheaton/t81_558_justcode/class_06_3_resnet.py
+++ ai/jheaton/t81_558_justcode/class_06_3_resnet.py
@@ -5,4 +5,12 @@
 print("class_06_3_resnet")
 # this program requires the file structure to exist, that is created by running class_06_2_cnn_A_prepare.py
 
+PATH = "./not_on_github"
+EXTRACT_TARGET = os.path.join(PATH, "clips")
+SOURCE = os.path.join(PATH, "clips/paperclips")
+TARGET = os.path.join(PATH, "clips-processed")
+
+df_train = pd.read_csv(os.path.join(SOURCE, "train.csv"))
+df_train['filename'] = "clips-" + df_train.id.astype(str) + ".jpg"
+print(df_train)
 
