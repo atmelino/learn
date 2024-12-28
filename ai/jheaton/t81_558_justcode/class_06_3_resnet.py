0c637a616c704eb22196380c51725f8723977b98
atmelino <tmbscience@protonmail.com>
2024-12-28 14:34:09 -0600

    u

--- ai/jheaton/t81_558_justcode/class_06_3_resnet.py
+++ ai/jheaton/t81_558_justcode/class_06_3_resnet.py
@@ -16,7 +16,6 @@ print("class_06_3_resnet")
 # this program requires the file structure to exist, that is created by running class_06_2_cnn_A_prepare.py
 
 PATH = "./not_on_github"
-EXTRACT_TARGET = os.path.join(PATH, "clips")
 SOURCE = os.path.join(PATH, "clips/paperclips")
 TARGET = os.path.join(PATH, "clips-processed")
 
