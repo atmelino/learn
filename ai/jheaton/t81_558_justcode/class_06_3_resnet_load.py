cec6e558f026ff600ef6e351d7e5af039a08f2b0
atmelino <tmbscience@protonmail.com>
2024-12-29 18:37:21 -0600

    u

--- ai/jheaton/t81_558_justcode/class_06_3_resnet_load.py
+++ ai/jheaton/t81_558_justcode/class_06_3_resnet_load.py
@@ -4,10 +4,8 @@ from tensorflow.keras.models import load_model
 
 print("class_06_3_resnet_load")
 
-load_path = "../models"
+load_path = "./models"
 model = load_model(os.path.join(load_path, "class_06_3_resnet.h5"))
-
-
 
 exit()
 
