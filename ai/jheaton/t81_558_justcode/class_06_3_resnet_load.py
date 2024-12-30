51299c5da3296cee5aee5eaa724ef1e5f44d854e
atmelino <tmbscience@protonmail.com>
2024-12-29 18:44:44 -0600

    u

--- ai/jheaton/t81_558_justcode/class_06_3_resnet_load.py
+++ ai/jheaton/t81_558_justcode/class_06_3_resnet_load.py
@@ -16,6 +16,16 @@ filename="clips-30002.jpg"
 
 img = keras.utils.load_img(imagedir + filename, target_size=image_size)
 
+img_array = keras.utils.img_to_array(img)
+img_array = keras.ops.expand_dims(img_array, 0)  # Create batch axis
+
+prediction = model.predict(img_array)
+score = float(keras.ops.sigmoid(prediction[0][0]))
+
+print(prediction)
+
+
+
 
 exit()
 
