fde5e661c244cf27cb95d92fbb6a9bdd5057eba5
atmelino <tmbscience@protonmail.com>
2024-12-30 19:53:07 -0600

    u

--- ai/jheaton/t81_558_justcode/class_06_2_cnn_A_load.py
+++ ai/jheaton/t81_558_justcode/class_06_2_cnn_A_load.py
@@ -5,20 +5,13 @@ from tensorflow.keras.models import load_model
 
 print("class_06_2_cnn_A_load")
 
-
 PATH = "./not_on_github"
 SOURCE = os.path.join(PATH, "clips/paperclips")
-
-
-
 
 validation_datagen = ImageDataGenerator(rescale=1.0 / 255)
 
 load_path = "./models"
 model = load_model(os.path.join(load_path, "class_06_2_cnn_A.h5"))
-
-
-
 
 df_test = pd.read_csv(
     os.path.join(SOURCE,"test.csv"),
@@ -44,7 +37,4 @@ df_submit = pd.DataFrame({'id':df_test['id'],'clip_count':pred.flatten()})
 df_submit.to_csv(os.path.join(PATH,"class_06_2_cnn_A.csv"),index=False)
 
 print(df_submit)
-
-
-
 
--- ai/jheaton/t81_558_justcode/tmp/class_06_3_resnet_load.py
+++ ai/jheaton/t81_558_justcode/tmp/class_06_3_resnet_load.py
@@ -1,19 +1,23 @@
 # requires keras 2 meaning tensorflow version <= 2.15 
 
-import pandas as pd
 import os
-from keras_preprocessing.image import ImageDataGenerator
+import keras
 from tensorflow.keras.models import load_model
+from keras_preprocessing.image import ImageDataGenerator
+
 
 print("class_06_3_resnet_load")
 
 PATH = "./not_on_github"
 SOURCE = os.path.join(PATH, "clips/paperclips")
 
-validation_datagen = ImageDataGenerator(rescale=1.0 / 255)
+imagedir = "./not_on_github/clips/paperclips/"
+image_size = (256, 256)
 
 load_path = "./models"
 model = load_model(os.path.join(load_path, "class_06_3_resnet.h5"))
+
+filename="clips-30002.jpg"
 
 df_test = pd.read_csv(
     os.path.join(SOURCE,"test.csv"),
@@ -21,7 +25,9 @@ df_test = pd.read_csv(
 
 df_test['filename']="clips-"+df_test["id"].astype(str)+".jpg"
 
+
 test_datagen = ImageDataGenerator(rescale = 1./255)
+
 
 test_generator = validation_datagen.flow_from_dataframe(
     dataframe=df_test,
@@ -36,7 +42,31 @@ test_generator.reset()
 pred = model.predict(test_generator,steps=len(df_test))
 
 df_submit = pd.DataFrame({'id':df_test['id'],'clip_count':pred.flatten()})
-df_submit.to_csv(os.path.join(PATH,"class_06_3_resnet.csv"),index=False)
+df_submit.to_csv(os.path.join(PATH,"class_06_3_resnet_load.csv"),index=False)
 
 print(df_submit)
+
+
+
+
+
+
+
+
+
+# for keras 3 meaning tensorflow version > 2.15
+# filename="clips-30002.jpg"
+# img = keras.utils.load_img(imagedir + filename, target_size=image_size)
+# img_array = keras.utils.img_to_array(img)
+# img_array = keras.ops.expand_dims(img_array, 0)  # Create batch axis
+# prediction = model.predict(img_array)
+# score = float(keras.ops.sigmoid(prediction[0][0]))
+# print(prediction)
+
+
+
+
+exit()
+
+
 
