125807bb4d84f1431830a95847066aa8b61f28a0
atmelino <tmbscience@protonmail.com>
2024-12-30 18:40:53 -0600

    u

--- ai/jheaton/t81_558_justcode/class_06_3_resnet_load.py
+++ ai/jheaton/t81_558_justcode/class_06_3_resnet_load.py
@@ -14,15 +14,26 @@ model = load_model(os.path.join(load_path, "class_06_3_resnet.h5"))
 filename="clips-30002.jpg"
 
 
-img = keras.utils.load_img(imagedir + filename, target_size=image_size)
 
-img_array = keras.utils.img_to_array(img)
-img_array = keras.ops.expand_dims(img_array, 0)  # Create batch axis
+test_datagen = ImageDataGenerator(rescale = 1./255)
 
-prediction = model.predict(img_array)
-score = float(keras.ops.sigmoid(prediction[0][0]))
 
-print(prediction)
+
+
+
+
+
+
+
+
+# for keras 3 meaning tensorflow version > 2.15
+# filename="clips-30002.jpg"
+# img = keras.utils.load_img(imagedir + filename, target_size=image_size)
+# img_array = keras.utils.img_to_array(img)
+# img_array = keras.ops.expand_dims(img_array, 0)  # Create batch axis
+# prediction = model.predict(img_array)
+# score = float(keras.ops.sigmoid(prediction[0][0]))
+# print(prediction)
 
 
 
