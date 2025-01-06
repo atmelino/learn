67384e76033066c3c3641015febcf4c4cd94603d
atmelino <tmbscience@protonmail.com>
2025-01-05 18:19:05 -0600

    u

--- ai/practice/keras/image/resnet_02.py
+++ ai/practice/keras/image/resnet_02.py
@@ -12,86 +12,110 @@ import tensorflow_datasets as tfds
 
 print("Tensorflow version " + tf.__version__)
 
-BATCH_SIZE = 32 
-classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+BATCH_SIZE = 32
+classes = [
+    "airplane",
+    "automobile",
+    "bird",
+    "cat",
+    "deer",
+    "dog",
+    "frog",
+    "horse",
+    "ship",
+    "truck",
+]
 
-#Matplotlib config
-plt.rc('image', cmap='gray')
-plt.rc('grid', linewidth=0)
-plt.rc('xtick', top=False, bottom=False, labelsize='large')
-plt.rc('ytick', left=False, right=False, labelsize='large')
-plt.rc('axes', facecolor='F8F8F8', titlesize="large", edgecolor='white')
-plt.rc('text', color='a8151a')
-plt.rc('figure', facecolor='F0F0F0')# Matplotlib fonts
+# Matplotlib config
+plt.rc("image", cmap="gray")
+plt.rc("grid", linewidth=0)
+plt.rc("xtick", top=False, bottom=False, labelsize="large")
+plt.rc("ytick", left=False, right=False, labelsize="large")
+plt.rc("axes", facecolor="F8F8F8", titlesize="large", edgecolor="white")
+plt.rc("text", color="a8151a")
+plt.rc("figure", facecolor="F0F0F0")  # Matplotlib fonts
 MATPLOTLIB_FONT_DIR = os.path.join(os.path.dirname(plt.__file__), "mpl-data/fonts/ttf")
+
 
 # utility to display a row of digits with their predictions
 def display_images(digits, predictions, labels, title):
 
-  n = 10
+    n = 10
 
-  indexes = np.random.choice(len(predictions), size=n)
-  n_digits = digits[indexes]
-  n_predictions = predictions[indexes]
-  n_predictions = n_predictions.reshape((n,))
-  n_labels = labels[indexes]
- 
-  fig = plt.figure(figsize=(20, 4))
-  plt.title(title)
-  plt.yticks([])
-  plt.xticks([])
+    indexes = np.random.choice(len(predictions), size=n)
+    n_digits = digits[indexes]
+    n_predictions = predictions[indexes]
+    n_predictions = n_predictions.reshape((n,))
+    n_labels = labels[indexes]
 
-  for i in range(10):
-    ax = fig.add_subplot(1, 10, i+1)
-    class_index = n_predictions[i]
-    
-    plt.xlabel(classes[class_index])
-    plt.xticks([])
+    fig = plt.figure(figsize=(20, 4))
+    plt.title(title)
     plt.yticks([])
-    plt.imshow(n_digits[i])
+    plt.xticks([])
 
-  plt.show()
-  fig.savefig('../output/resnet02.png')
-  # fig.savefig('resnet02.png')
+    for i in range(10):
+        ax = fig.add_subplot(1, 10, i + 1)
+        class_index = n_predictions[i]
+
+        plt.xlabel(classes[class_index])
+        plt.xticks([])
+        plt.yticks([])
+        plt.imshow(n_digits[i])
+
+    plt.show()
+    fig.savefig("../output/resnet02.png")
+    # fig.savefig('resnet02.png')
+
 
 # utility to display training and validation curves
 def plot_metrics(metric_name, title, ylim=5):
-  plt.title(title)
-  plt.ylim(0,ylim)
-  plt.plot(history.history[metric_name],color='blue',label=metric_name)
-  plt.plot(history.history['val_' + metric_name],color='green',label='val_' + metric_name)
+    plt.title(title)
+    plt.ylim(0, ylim)
+    plt.plot(history.history[metric_name], color="blue", label=metric_name)
+    plt.plot(
+        history.history["val_" + metric_name], color="green", label="val_" + metric_name
+    )
 
 
-(training_images, training_labels) , (validation_images, validation_labels) = tf.keras.datasets.cifar10.load_data()
+(training_images, training_labels), (validation_images, validation_labels) = (
+    tf.keras.datasets.cifar10.load_data()
+)
 
-display_images(training_images, training_labels, training_labels, "Training Data" )
-display_images(validation_images, validation_labels, validation_labels, "Validation Data" )
+display_images(training_images, training_labels, training_labels, "Training Data")
+display_images(
+    validation_images, validation_labels, validation_labels, "Validation Data"
+)
 
 
 def preprocess_image_input(input_images):
-  input_images = input_images.astype('float32')
-  output_ims = tf.keras.applications.resnet50.preprocess_input(input_images)
-  return output_ims
+    input_images = input_images.astype("float32")
+    output_ims = tf.keras.applications.resnet50.preprocess_input(input_images)
+    return output_ims
+
 
 train_X = preprocess_image_input(training_images)
 valid_X = preprocess_image_input(validation_images)
 
 
-'''
+"""
 Feature Extraction is performed by ResNet50 pretrained on imagenet weights. 
 Input size is 224 x 224.
-'''
+"""
+
+
 def feature_extractor(inputs):
 
-  feature_extractor = tf.keras.applications.resnet.ResNet50(input_shape=(224, 224, 3),
-                                               include_top=False,
-                                               weights='imagenet')(inputs)
-  return feature_extractor
+    feature_extractor = tf.keras.applications.resnet.ResNet50(
+        input_shape=(224, 224, 3), include_top=False, weights="imagenet"
+    )(inputs)
+    return feature_extractor
 
 
-'''
+"""
 Defines final dense layers and subsequent softmax layer for classification.
-'''
+"""
+
+
 def classifier(inputs):
     x = tf.keras.layers.GlobalAveragePooling2D()(inputs)
     x = tf.keras.layers.Flatten()(x)
@@ -100,46 +124,52 @@ def classifier(inputs):
     x = tf.keras.layers.Dense(10, activation="softmax", name="classification")(x)
     return x
 
-'''
+
+"""
 Since input image size is (32 x 32), first upsample the image by factor of (7x7) to transform it to (224 x 224)
 Connect the feature extraction and "classifier" layers to build the model.
-'''
+"""
+
+
 def final_model(inputs):
 
-    resize = tf.keras.layers.UpSampling2D(size=(7,7))(inputs)
+    resize = tf.keras.layers.UpSampling2D(size=(7, 7))(inputs)
 
     resnet_feature_extractor = feature_extractor(resize)
     classification_output = classifier(resnet_feature_extractor)
 
     return classification_output
 
-'''
+
+"""
 Define the model and compile it. 
 Use Stochastic Gradient Descent as the optimizer.
 Use Sparse Categorical CrossEntropy as the loss function.
-'''
+"""
+
+
 def define_compile_model():
-  inputs = tf.keras.layers.Input(shape=(32,32,3))
-  
-  classification_output = final_model(inputs) 
-  model = tf.keras.Model(inputs=inputs, outputs = classification_output)
- 
-  model.compile(optimizer='SGD', 
-                loss='sparse_categorical_crossentropy',
-                metrics = ['accuracy'])
-  
-  return model
+    inputs = tf.keras.layers.Input(shape=(32, 32, 3))
+
+    classification_output = final_model(inputs)
+    model = tf.keras.Model(inputs=inputs, outputs=classification_output)
+
+    model.compile(
+        optimizer="SGD", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
+    )
+
+    return model
 
 
 model = define_compile_model()
 
 model.summary()
 
-
-
-
-
-
-
-
-
+EPOCHS = 3
+history = model.fit(
+    train_X,
+    training_labels,
+    epochs=EPOCHS,
+    validation_data=(valid_X, validation_labels),
+    batch_size=64,
+)
