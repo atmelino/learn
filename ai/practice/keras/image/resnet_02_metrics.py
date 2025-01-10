88cff72fe1b349eebc4f1ece44aa2cd075f755d9
atmelino <tmbscience@protonmail.com>
2025-01-09 18:47:14 -0600

    u

--- ai/practice/keras/image/resnet_02_metrics.py
+++ ai/practice/keras/image/resnet_02_metrics.py
@@ -5,6 +5,7 @@
 import os
 import tensorflow as tf
 from tensorflow.keras.models import load_model
+import pandas as pd
 
 
 
@@ -29,6 +30,12 @@ load_path = "../not_on_github/models/resnet_02"
 model = load_model(os.path.join(load_path, "resnet_02.h5"))
 
 model.summary()
+
+probabilities = model.predict(valid_X, batch_size=64)
+
+
+df = pd.DataFrame(probabilities)
+print(df)
 
 
 
