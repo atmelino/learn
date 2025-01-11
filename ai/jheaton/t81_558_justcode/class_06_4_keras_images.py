78a946d7de4005f2ff3065698c340c0c4e815c93
atmelino <tmbscience@protonmail.com>
2025-01-11 14:18:13 -0600

    u

---
+++ ai/jheaton/t81_558_justcode/class_06_4_keras_images.py
@@ -0,0 +1,61 @@
+import urllib.request
+import shutil
+from IPython.display import Image
+import matplotlib.pyplot as plt
+import keras
+from numpy import expand_dims
+from keras.preprocessing.image import load_img
+from keras.preprocessing.image import img_to_array
+from keras.preprocessing.image import ImageDataGenerator
+from matplotlib import pyplot
+import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib
+
+URL = "https://github.com/jeffheaton/t81_558_deep_learning/blob/master/photos/landscape.jpg?raw=true"
+LOCAL_IMG_FILE = "./not_on_github/images/landscape.jpg"
+with urllib.request.urlopen(URL) as response, open(LOCAL_IMG_FILE, "wb") as out_file:
+    shutil.copyfileobj(response, out_file)
+
+# Image(filename=LOCAL_IMG_FILE)
+
+img = keras.utils.load_img(LOCAL_IMG_FILE)
+plt.imshow(img)
+plt.show()
+
+
+def visualize_generator(img_file, gen):
+    # Load the requested image
+    img = load_img(img_file)
+    data = img_to_array(img)
+    samples = expand_dims(data, 0)
+
+    # Generate augumentations from the generator
+    it = gen.flow(samples, batch_size=1)
+    images = []
+    for i in range(4):
+        batch = it.next()
+        image = batch[0].astype("uint8")
+        images.append(image)
+
+    images = np.array(images)
+
+    # Create a grid of 4 images from the generator
+    index, height, width, channels = images.shape
+    nrows = index // 2
+    grid = (
+        images.reshape(nrows, 2, height, width, channels)
+        .swapaxes(1, 2)
+        .reshape(height * nrows, width * 2, 3)
+    )
+    fig = plt.figure(figsize=(15.0, 15.0))
+    plt.axis("off")
+    plt.imshow(grid)
+    plt.show()
+    fig.savefig('./output/class_06_4_keras_images_01.png')
+
+
+
+visualize_generator(
+    LOCAL_IMG_FILE, ImageDataGenerator(horizontal_flip=True, vertical_flip=True)
+)
