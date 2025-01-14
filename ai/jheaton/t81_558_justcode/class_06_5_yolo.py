7fa7521d53bdabeb969db461d6ff93a5feb00f7a
atmelino <tmbscience@protonmail.com>
2025-01-13 18:54:35 -0600

    u

---
+++ ai/jheaton/t81_558_justcode/class_06_5_yolo.py
@@ -0,0 +1,9 @@
+# https://github.com/ultralytics/yolov5
+
+
+
+URL = "https://github.com/jeffheaton/t81_558_deep_learning/raw/master/photos/jeff_cook.jpg"
+LOCAL_IMG_FILE = "./not_on_github/images/jeff_cook.jpg"
+
+
+cmd="python3 /not_on_github/yolov5/detect.py --weights yolov5s.pt --img 1024 --conf 0.25 --source /not_on_github/images/"
