c187944867db06133a33a923e380de7d10a7b98a
atmelino <tmbscience@protonmail.com>
2025-01-14 18:22:26 -0600

    u

--- ai/jheaton/t81_558_justcode/class_06_5_yolo.py
+++ ai/jheaton/t81_558_justcode/class_06_5_yolo.py
@@ -7,3 +7,29 @@ LOCAL_IMG_FILE = "./not_on_github/images/jeff_cook.jpg"
 
 
 cmd="python3 /not_on_github/yolov5/detect.py --weights yolov5s.pt --img 1024 --conf 0.25 --source /not_on_github/images/"
+
+
+import torch
+import pandas as pd
+
+pd.set_option('display.width', 1000)
+
+
+# Model
+yolo_model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
+# or yolov5n - yolov
+# Inference
+results = yolo_model(LOCAL_IMG_FILE)
+# Results
+df = results.pandas().xyxy[0]
+print(df)
+
+
+df2 = df[['name','class']].groupby(by=["name"]).count().reset_index()
+df2.columns = ['name','count']
+df2['image'] = 1
+df2.pivot(index=['image'],columns='name',values='count').reset_index().fillna(0)
+print(df2)
+
+
+
