9c4021dd70d142fd6c0e64b205591bbe9edd6cd3
atmelino <tmbscience@protonmail.com>
2025-01-18 21:48:10 -0600

    u

--- ai/jheaton/t81_558_justcode/class_07_2_train_gan.sh
+++ ai/jheaton/t81_558_justcode/class_07_2_train_gan.sh
@@ -1,4 +1,17 @@
-python3 ./not_on_github/stylegan2-ada-pytorch/dataset_tool.py "\
-"--source /content/drive/MyDrive/data/gan/images/circuit "\
-"--dest /content/drive/MyDrive/data/gan/dataset/circuit"
+
+mkdir ./not_on_github/class_07_2_train_gan
+mkdir ./not_on_github/class_07_2_train_gan/data
+mkdir ./not_on_github/class_07_2_train_gan/data/gan
+mkdir ./not_on_github/class_07_2_train_gan/data/gan/images
+mkdir ./not_on_github/class_07_2_train_gan/data/gan/dataset
+mkdir ./not_on_github/class_07_2_train_gan/data/gan/images/circuit
+mkdir ./not_on_github/class_07_2_train_gan/data/gan/dataset/circuit
+
+
+
+
+
+python3 not_on_github/stylegan2-ada-pytorch/dataset_tool.py \
+--source /content/drive/MyDrive/data/gan/images/circuit \
+--dest /content/drive/MyDrive/data/gan/dataset/circuit
 
