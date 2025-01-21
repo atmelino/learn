e8ad8086c1aeb4e84567674363a5c2451fb34a8b
atmelino <tmbscience@protonmail.com>
2025-01-20 20:21:58 -0600

    u

---
+++ ai/jheaton/t81_558_justcode/class_07_2_train_gan_train.sh
@@ -0,0 +1,10 @@
+# Modify these to suit your needs
+EXPERIMENTS = "/content/drive/MyDrive/data/gan/experiments"
+DATA = "/content/drive/MyDrive/data/gan/dataset/circuit"
+SNAP = 10
+
+
+# Build the command and run it
+python3 not_on_github/stylegan2-ada-pytorch/train.py --snap {SNAP} --outdir {EXPERIMENTS} --data {DATA}
+
+
