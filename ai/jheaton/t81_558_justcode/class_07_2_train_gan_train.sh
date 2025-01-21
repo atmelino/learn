9daed3d9d758c85076acbe2c433261291c3bc398
atmelino <tmbscience@protonmail.com>
2025-01-20 20:37:29 -0600

    u

--- ai/jheaton/t81_558_justcode/class_07_2_train_gan_train.sh
+++ ai/jheaton/t81_558_justcode/class_07_2_train_gan_train.sh
@@ -1,12 +1,12 @@
 # Modify these to suit your needs
-$EXPERIMENTS = "not_on_github/drive/MyDrive/data/gan/experiments"
-$DATA = "/content/drive/MyDrive/data/gan/dataset/circuit"
-$SNAP = '10'
+EXPERIMENTS="not_on_github/stylegan2-ada-pytorch/data/gan/experiments"
+DATA="not_on_github/stylegan2-ada-pytorch/data/gan/dataset/circuit"
+SNAP='10'
 
 
 # Build the command and run it
 # python3 not_on_github/stylegan2-ada-pytorch/train.py --snap={SNAP} --outdir {EXPERIMENTS} --data {DATA}
 
 
-python3 not_on_github/stylegan2-ada-pytorch/train.py --snap=10 --outdir=not_on_github/class_07_2_train_gan/data/gan/output --data=not_on_github/class_07_2_train_gan/data/gan/dataset/circuit
+python3 not_on_github/stylegan2-ada-pytorch/train.py --snap 10 --outdir not_on_github/class_07_2_train_gan/data/gan/output --data not_on_github/class_07_2_train_gan/data/gan/dataset/circuit
 
