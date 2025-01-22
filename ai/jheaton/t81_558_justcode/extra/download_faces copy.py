868a5aad8cd9878a22560fb1f3ec8b674b32767b
atmelino <tmbscience@protonmail.com>
2025-01-21 20:26:04 -0600

    u

---
+++ ai/jheaton/t81_558_justcode/extra/download_faces copy.py
@@ -0,0 +1,13 @@
+import gdown
+
+# a file
+# url = "https://drive.usercontent.google.com/download?id=16N0RV4fHI6joBuKbQAoG34V_cQk7vxSA"
+# output = "fcn8s_from_caffe.npz"
+# gdown.download(url, output)
+
+
+
+url = "https://drive.google.com/uc?id=1l_5RK28JRL19wpT22B-DY9We3TVXnnQQ"
+output = "ffhq-dataset-v2.json"
+gdown.download(url, output)
+
