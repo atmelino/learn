89c48097b3aaa083847f40a91f61a130b858f7a9
atmelino <tmbscience@protonmail.com>
2025-01-21 19:41:31 -0600

    u

--- ai/jheaton/t81_558_justcode/class_07_2_train_gan_train.sh
+++ ai/jheaton/t81_558_justcode/class_07_2_train_gan_train.sh
@@ -1,5 +1,5 @@
 # Modify these to suit your needs
-EXPERIMENTS="not_on_github/stylegan2-ada-pytorch/data/gan/experiments"
+OUTPUT="not_on_github/stylegan2-ada-pytorch/data/gan/experiments"
 DATA="not_on_github/stylegan2-ada-pytorch/data/gan/dataset/circuit"
 SNAP='10'
 
@@ -8,5 +8,10 @@ SNAP='10'
 # python3 not_on_github/stylegan2-ada-pytorch/train.py --snap={SNAP} --outdir {EXPERIMENTS} --data {DATA}
 
 
-python3 not_on_github/stylegan2-ada-pytorch/train.py --snap 10 --outdir not_on_github/class_07_2_train_gan/data/gan/output --data not_on_github/class_07_2_train_gan/data/gan/dataset/circuit
+# python3 not_on_github/stylegan2-ada-pytorch/train.py --snap 10 --outdir not_on_github/class_07_2_train_gan/data/gan/output --data not_on_github/class_07_2_train_gan/data/gan/dataset/circuit
+# python3 not_on_github/stylegan3/train.py --cfg=stylegan2 --snap 10 --outdir not_on_github/class_07_2_train_gan/data/gan/output --data not_on_github/class_07_2_train_gan/data/gan/dataset/circuit
 
+
+
+
+python3 not_on_github/stylegan3/train.py  --gpus=2 --outdir=~/sg3/output/ --cfg=stylegan2 --data=~/sg3/data --gpus=2 --batch=32 --gamma=10 --mirror=1 --aug=noaug
