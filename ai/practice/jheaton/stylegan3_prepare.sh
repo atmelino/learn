849cf0741fc5ba3747522aec8121374f17de0938
atmelino <tmbscience@protonmail.com>
2025-01-22 20:03:11 -0600

    u

---
+++ ai/practice/jheaton/stylegan3_prepare.sh
@@ -0,0 +1,10 @@
+
+
+# Original 1024x1024 resolution.
+# python3  not_on_github/stylegan3/dataset_tool.py --source=/tmp/images1024x1024 --dest=~/datasets/ffhq-1024x1024.zip
+
+# Scaled down 256x256 resolution.
+python not_on_github/stylegan3/dataset_tool.py --source=../../../../../sg3/images1024x1024 --dest=../../../../../sg3/datasets/ffhq-256x256.zip  --resolution=256x256
+
+
+
