23396a0e7928f0e9e4ea925f2f002a08ebda760c
atmelino <tmbscience@protonmail.com>
2025-01-27 20:50:20 -0600

    u

--- ai/practice/jheaton/class_07_5_tabular_synthetic.py
+++ ai/practice/jheaton/class_07_5_tabular_synthetic.py
@@ -14,28 +14,44 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 
 df = pd.read_csv(
-"https://data.heatonresearch.com/data/t81-558/auto-mpg.csv",
-na_values=['NA', '?'])
-COLS_USED = ['cylinders', 'displacement', 'horsepower', 'weight',
-'acceleration', 'year', 'origin','mpg']
-COLS_TRAIN = ['cylinders', 'displacement', 'horsepower', 'weight',
-'acceleration', 'year', 'origin']
+    "https://data.heatonresearch.com/data/t81-558/auto-mpg.csv", na_values=["NA", "?"]
+)
+COLS_USED = [
+    "cylinders",
+    "displacement",
+    "horsepower",
+    "weight",
+    "acceleration",
+    "year",
+    "origin",
+    "mpg",
+]
+COLS_TRAIN = [
+    "cylinders",
+    "displacement",
+    "horsepower",
+    "weight",
+    "acceleration",
+    "year",
+    "origin",
+]
 df = df[COLS_USED]
 
 # Handle missing value
-df['horsepower'] = df['horsepower'].fillna(df['horsepower'].median())
+df["horsepower"] = df["horsepower"].fillna(df["horsepower"].median())
 # Split into training and test sets
 df_x_train, df_x_test, df_y_train, df_y_test = train_test_split(
     df.drop("mpg", axis=1),
     df["mpg"],
     test_size=0.20,
-    #shuffle=False,
+    # shuffle=False,
     random_state=42,
 )
 
 # Create dataframe versions for tabular GAN
-df_x_test, df_y_test = df_x_test.reset_index(drop=True), \
-df_y_test.reset_index(drop=True)
+df_x_test, df_y_test = df_x_test.reset_index(drop=True), df_y_test.reset_index(
+    drop=True
+)
 df_y_train = pd.DataFrame(df_y_train)
 df_y_test = pd.DataFrame(df_y_test)
 # Pandas to Numpy
@@ -46,39 +62,59 @@ y_test = df_y_test.values
 
 # Build the neural network
 model = Sequential()
-model.add(Dense(50, input_dim=x_train.shape[1], activation='relu')) # Hidden 1
-model.add(Dense(25, activation='relu')) # Hidden 2
-model.add(Dense(12, activation='relu')) # Hidden 2
-model.add(Dense(1)) # Output
-model.compile(loss='mean_squared_error', optimizer='adam')
-monitor = EarlyStopping(monitor='val_loss', min_delta=1e-3,
-patience=5, verbose=1, mode='auto',
-restore_best_weights=True)
-model.fit(x_train,y_train,validation_data=(x_test,y_test),
-callbacks=[monitor], verbose=2,epochs=1000)
+model.add(Dense(50, input_dim=x_train.shape[1], activation="relu"))  # Hidden 1
+model.add(Dense(25, activation="relu"))  # Hidden 2
+model.add(Dense(12, activation="relu"))  # Hidden 2
+model.add(Dense(1))  # Output
+model.compile(loss="mean_squared_error", optimizer="adam")
+monitor = EarlyStopping(
+    monitor="val_loss",
+    min_delta=1e-3,
+    patience=5,
+    verbose=1,
+    mode="auto",
+    restore_best_weights=True,
+)
+model.fit(
+    x_train,
+    y_train,
+    validation_data=(x_test, y_test),
+    callbacks=[monitor],
+    verbose=2,
+    epochs=1000,
+)
 
 pred = model.predict(x_test)
-score = np.sqrt(metrics.mean_squared_error(pred,y_test))
+score = np.sqrt(metrics.mean_squared_error(pred, y_test))
 print("Final score (RMSE): {}".format(score))
 
 
-
-gen_x, gen_y = GANGenerator(gen_x_times=1.1, cat_cols=None,
-bot_filter_quantile=0.001, top_filter_quantile=0.999, \
-is_post_process=True,
-adversarial_model_params={
-"metrics": "rmse", "max_depth": 2, "max_bin": 100,
-"learning_rate": 0.02, "random_state": \
-42, "n_estimators": 500,
-}, pregeneration_frac=2, only_generated_data=False,\
-gan_params = {"batch_size": 500, "patience": 25, \
-"epochs" : 500,}).generate_data_pipe(df_x_train, df_y_train,\
-df_x_test, deep_copy=True, only_adversarial=False, \
-use_adversarial=True)
-
-
-
-
-
-
-
+gen_x, gen_y = GANGenerator(
+    gen_x_times=1.1,
+    cat_cols=None,
+    bot_filter_quantile=0.001,
+    top_filter_quantile=0.999,
+    is_post_process=True,
+    adversarial_model_params={
+        "metrics": "rmse",
+        "max_depth": 2,
+        "max_bin": 100,
+        "learning_rate": 0.02,
+        "random_state": 42,
+        "n_estimators": 500,
+    },
+    pregeneration_frac=2,
+    only_generated_data=False,
+    gan_params={
+        "batch_size": 500,
+        "patience": 25,
+        "epochs": 500,
+    },
+).generate_data_pipe(
+    df_x_train,
+    df_y_train,
+    df_x_test,
+    deep_copy=True,
+    only_adversarial=False,
+    use_adversarial=True,
+)
