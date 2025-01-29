7568899b5aa8d380e3a794d2b9a6c994f965ccd9
atmelino <tmbscience@protonmail.com>
2025-01-28 18:44:17 -0600

    u

--- ai/practice/jheaton/gan_basic.py
+++ ai/practice/jheaton/gan_basic.py
@@ -1,9 +1,10 @@
 from numpy.random import rand
 from numpy import hstack
+from numpy import ones,zeros
+import numpy as np
 from matplotlib import pyplot
 from keras.models import Sequential
 from keras.layers import Dense
-# from keras.utils.vis_utils import plot_model
 from tensorflow.keras.utils import plot_model
 
 # options
@@ -59,3 +60,58 @@ model = define_discriminator()
 model.summary()
 # plot the model
 plot_model(model, to_file='discriminator_plot.png', show_shapes=True, show_layer_names=True)
+
+
+# generate n real samples with class labels
+def generate_real_samples(n):
+    # generate inputs in [-0.5, 0.5]
+    X1 = rand(n) - 0.5
+    # generate outputs X^2
+    X2 = X1 * X1
+    # stack arrays
+    X1 = X1.reshape(n, 1)
+    X2 = X2.reshape(n, 1)
+    X = hstack((X1, X2))
+    # generate class labels
+    y = ones((n, 1))
+    return X, y
+ 
+float_formatter = "{:.2f}".format    
+np.set_printoptions(formatter={'float_kind':float_formatter})
+print(generate_real_samples(10))
+
+# generate n fake samples with class labels
+def generate_fake_samples(n):
+    # generate inputs in [-1, 1]
+    X1 = -1 + rand(n) * 2
+    # generate outputs in [-1, 1]
+    X2 = -1 + rand(n) * 2
+    # stack arrays
+    X1 = X1.reshape(n, 1)
+    X2 = X2.reshape(n, 1)
+    X = hstack((X1, X2))
+    # generate class labels
+    y = zeros((n, 1))
+    return X, y
+
+print(generate_fake_samples(10))
+
+# train the discriminator model
+def train_discriminator(model, n_epochs=1000, n_batch=128):
+    half_batch = int(n_batch / 2)
+    # run epochs manually
+    for i in range(n_epochs):
+        # generate real examples
+        X_real, y_real = generate_real_samples(half_batch)
+        # update model
+        model.train_on_batch(X_real, y_real)
+        # generate fake examples
+        X_fake, y_fake = generate_fake_samples(half_batch)
+        # update model
+        model.train_on_batch(X_fake, y_fake)
+        # evaluate the model
+        _, acc_real = model.evaluate(X_real, y_real, verbose=0)
+        _, acc_fake = model.evaluate(X_fake, y_fake, verbose=0)
+        print(i, acc_real, acc_fake)
+
+
