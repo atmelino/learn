50cafb7857c8543959c97292a54ac2508e43b15e
atmelino <tmbscience@protonmail.com>
2025-01-30 20:20:39 -0600

    update

--- ai/practice/gan/4_generator.py
+++ ai/practice/gan/4_generator.py
@@ -2,11 +2,12 @@
 # https://machinelearningmastery.com/how-to-develop-a-generative-adversarial-network-for-a-1-dimensional-function-from-scratch-in-keras/
 
 
-
 # define the generator model
 from keras.models import Sequential
 from keras.layers import Dense
 from tensorflow.keras.utils import plot_model
+from numpy.random import randn
+from matplotlib import pyplot
 
 # define the standalone generator model
 def define_generator(latent_dim, n_outputs=2):
@@ -23,5 +24,37 @@ model.summary()
 
 # plot the model
 plot_model(model, to_file='generator_plot.png', show_shapes=True, show_layer_names=True)
+
+
+# generate points in latent space as input for the generator
+def generate_latent_points(latent_dim, n):
+    # generate points in the latent space
+    x_input = randn(latent_dim * n)
+    # reshape into a batch of inputs for the network
+    x_input = x_input.reshape(n, latent_dim)
+    return x_input
+
+
+
+
+# use the generator to generate n fake examples and plot the results
+def generate_fake_samples(generator, latent_dim, n):
+    # generate points in latent space
+    x_input = generate_latent_points(latent_dim, n)
+    # predict outputs
+    X = generator.predict(x_input)
+    # plot the results
+    pyplot.scatter(X[:, 0], X[:, 1])
+    pyplot.show()
+
+# size of the latent space
+latent_dim = 5
+# define the discriminator model
+model = define_generator(latent_dim)
+# generate and plot generated samples
+generate_fake_samples(model, latent_dim, 100)
+
+
+
 
 
