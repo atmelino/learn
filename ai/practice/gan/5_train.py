c6b1b3979cb6f936f13bde5b966c06c92b6ef985
atmelino <tmbscience@protonmail.com>
2025-01-30 20:52:53 -0600

    update

--- ai/practice/gan/5_train.py
+++ ai/practice/gan/5_train.py
@@ -5,6 +5,10 @@
 from keras.models import Sequential
 from keras.layers import Dense
 from tensorflow.keras.utils import plot_model
+from numpy.random import rand
+from numpy import hstack
+from numpy import ones,zeros
+from numpy.random import randn
 
 
 # define the standalone discriminator model
@@ -37,6 +41,76 @@ def define_gan(generator, discriminator):
     model.compile(loss='binary_crossentropy', optimizer='adam')
     return model
     
+def generate_real_samples(n):
+    # generate inputs in [-0.5, 0.5]
+    X1 = rand(n) - 0.5
+    # generate outputs X^2
+    X2 = X1 * X1
+    # stack arrays
+    X1 = X1.reshape(n, 1)
+    X2 = X2.reshape(n, 1)
+    X = hstack((X1, X2))
+    # generate class labels
+    y = ones((n, 1))
+    return X, y
+
+# use the generator to generate n fake examples, with class labels
+def generate_fake_samples(generator, latent_dim, n):
+    # generate points in latent space
+    x_input = generate_latent_points(latent_dim, n)
+    # predict outputs
+    X = generator.predict(x_input)
+    # create class labels
+    y = zeros((n, 1))
+    return X, y
+
+# generate points in latent space as input for the generator
+def generate_latent_points(latent_dim, n):
+    # generate points in the latent space
+    x_input = randn(latent_dim * n)
+    # reshape into a batch of inputs for the network
+    x_input = x_input.reshape(n, latent_dim)
+    return x_input    
+
+
+# # train the composite model
+# def train_gan(gan_model, latent_dim, n_epochs=10000, n_batch=128):
+#     # manually enumerate epochs
+#     for i in range(n_epochs):
+#         # prepare points in latent space as input for the generator
+#         x_gan = generate_latent_points(latent_dim, n_batch)
+#         # create inverted labels for the fake samples
+#         y_gan = ones((n_batch, 1))
+#         # update the generator via the discriminator's error
+#         gan_model.train_on_batch(x_gan, y_gan)
+
+
+
+# train the generator and discriminator
+def train(g_model, d_model, gan_model, latent_dim, n_epochs=10000, n_batch=128):
+    # determine half the size of one batch, for updating the discriminator
+    half_batch = int(n_batch / 2)
+    # manually enumerate epochs
+    for i in range(n_epochs):
+        # prepare real samples
+        x_real, y_real = generate_real_samples(half_batch)
+        # prepare fake examples
+        x_fake, y_fake = generate_fake_samples(g_model, latent_dim, half_batch)
+        # update discriminator
+        d_model.train_on_batch(x_real, y_real)
+        d_model.train_on_batch(x_fake, y_fake)
+        # prepare points in latent space as input for the generator
+        x_gan = generate_latent_points(latent_dim, n_batch)
+        # create inverted labels for the fake samples
+        y_gan = ones((n_batch, 1))
+        # update the generator via the discriminator's error
+        gan_model.train_on_batch(x_gan, y_gan)
+
+
+
+
+
+
 # size of the latent space
 latent_dim = 5
 # create the discriminator
@@ -48,8 +122,6 @@ gan_model = define_gan(generator, discriminator)
 # summarize gan model
 gan_model.summary()
 # plot gan model
-plot_model(gan_model, to_file='gan_plot.png', show_shapes=True, show_layer_names=True)
-
-
-
-
+# plot_model(gan_model, to_file='gan_plot.png', show_shapes=True, show_layer_names=True)
+# train model
+train(generator, discriminator, gan_model, latent_dim)
