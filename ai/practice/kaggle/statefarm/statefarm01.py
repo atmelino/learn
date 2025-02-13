e4b472eaa3ed34798f275a21d2d9accd6d9fb7ad
atmelino <tmbscience@protonmail.com>
2025-02-13 13:24:42 -0600

    u

---
+++ ai/practice/kaggle/statefarm/statefarm01.py
@@ -0,0 +1,16 @@
+# Sayed Gamal
+# https://www.kaggle.com/code/sayedgamal99/in-cabin-state-farm-distracted-driver-detection
+
+# conda activate kaggle
+
+import ultralytics
+from ultralytics import YOLO
+from IPython.display import Image
+from roboflow import Roboflow
+from kaggle_secrets import UserSecretsClient
+import os
+import splitfolders
+
+ultralytics.checks()
+
+
