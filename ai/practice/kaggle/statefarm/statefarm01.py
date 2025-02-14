6bb9b392e1c722847ac0fd5d1322c03a52b4ceb9
atmelino <tmbscience@protonmail.com>
2025-02-13 18:50:12 -0600

    u

--- ai/practice/kaggle/statefarm/statefarm01.py
+++ ai/practice/kaggle/statefarm/statefarm01.py
@@ -13,4 +13,13 @@ import splitfolders
 
 ultralytics.checks()
 
+datafoldername="../../../../local_data/kaggle/statefarm"
+
+trainfoldername=os.path.join(datafoldername, "imgs/train")
+datasetfoldername=os.path.join(datafoldername, "dataset/")
+# splitfolders.ratio("/kaggle/input/state-farm-distracted-driver-detection/imgs/train", output="dataset", seed=32, ratio=(0.7, 0.15, 0.15))
+splitfolders.ratio(trainfoldername, output=datasetfoldername, seed=32, ratio=(0.7, 0.15, 0.15))
+
+
+
 
