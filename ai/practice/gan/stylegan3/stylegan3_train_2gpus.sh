cf1cefe26c50d59247715899ed1ca2822dd0075c
atmelino <tmbscience@protonmail.com>
2025-02-20 18:54:41 -0600

    u

--- ai/practice/gan/stylegan3/stylegan3_train_2gpus.sh
+++ ai/practice/gan/stylegan3/stylegan3_train_2gpus.sh
@@ -19,7 +19,7 @@
 #     --gpus=2 --batch=32 --gamma=10 --mirror=1 --aug=noaug
 
 # Train StyleGAN2 for FFHQ at 256x256 resolution using 2 GPUs.
-python3 not_on_github/stylegan3/train.py --outdir=../../../../local_data/sg3/training-runs --cfg=stylegan2 --data=../../../../local_data/sg3/datasets/ffhq-256x256.zip \
+python3 not_on_github/stylegan3/train.py --outdir=../../../../../local_data/sg3/training-runs --cfg=stylegan2 --data=../../../../../local_data/sg3/datasets/ffhq-256x256.zip \
     --gpus=2 --batch=32 --gamma=10 --mirror=1 --aug=noaug
 
 
--- ai/practice/kaggle/dogsvscats/kidcoder/load_model.py
+++ ai/practice/kaggle/dogsvscats/kidcoder/load_model.py
@@ -1,12 +1,15 @@
-
-
 import torch
 from torch import nn
 from torchvision import datasets, models, transforms
+from torchvision.transforms import *
+from torch.utils.data import DataLoader
+
+
+
 
 print("load model dogs vs cats")
 
-num_classes = 3
+num_classes = 2
 pretrained=True
 basepath="../../../../../../local_data/kaggle/dogsvscats_short"
 modelpath='./output/dogsvscats_model_weights.pth'
@@ -22,9 +25,46 @@ model.load_state_dict(torch.load(modelpath, map_location=device))
 
 # Validation
 transform = transforms.Compose([Resize(224), ToTensor()])
-val_image_folder = datasets.ImageFolder(basepath+"/working/train/train", transform=transform)
+val_image_folder = datasets.ImageFolder(basepath+"/working/valid", transform=transform)
 val_loader = DataLoader(val_image_folder, batch_size=4, shuffle=False)
 
+def evaluate(model, val_loader):
+    model.eval()
+    correct = 0
+    total = 0
+
+    compare_data = []
+
+    with torch.no_grad():
+        for inputs, labels,img_path in val_loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            # print("labels",labels)
+            # print("predic",predicted)
+            # print("labels",labels.tolist())
+            # print("predic",predicted.tolist())
+
+            for i in range(len(labels)):
+                # label = 'dog' if predicted[i].item() == 1 else 'cat'
+                compare_data.append([labels[i].item(), predicted[i].item(),img_path[i]])
+
+    accuracy = 100 * correct / total
+    print(f"Validation Accuracy: {accuracy:.2f}%")
+    # print(compare_data)
+    return(compare_data)
+
+
+myresults=evaluate(model, val_loader)
+compare_df = pd.DataFrame(myresults, columns=['label', 'prediction','path'])
+
+# print(compare_df)
+
+print(compare_df.to_string())
+filename_write = "./output/compare.csv"
+compare_df.to_csv(filename_write, index=False)
 
 
 
