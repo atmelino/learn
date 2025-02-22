faad677d0d142cc228a17fea05ae5ba69ad406e0
atmelino <tmbscience@protonmail.com>
2025-02-21 18:49:04 -0600

    u

--- ai/practice/kaggle/dogsvscats/kidcoder/load_model.py
+++ ai/practice/kaggle/dogsvscats/kidcoder/load_model.py
@@ -57,101 +57,65 @@ class TrainCustomDataset(Dataset):
         return image, label, img_path
 
 
+def validate(val_loader):
+
+    filelist=[]
+
+    for inputs, labels,img_path in val_loader:
+        # print(img_path)
+        for i in range(len(img_path)):
+            filelist.append([img_path[i]])
+
+    filelist_df = pd.DataFrame(filelist, columns=['path'])
+    # print(filelist_df.sort_values('path').to_string())
+
+    model = models.resnet18(pretrained=pretrained)
+    model.fc = nn.Linear(model.fc.in_features, num_classes)
+    model = model.to(device)
+
+    model.load_state_dict(torch.load(modelpath, map_location=device))
+
+    def evaluate(model, val_loader):
+        model.eval()
+        correct = 0
+        total = 0
+
+        compare_data = []
+
+        with torch.no_grad():
+            for inputs, labels,img_path in val_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+                # print("labels",labels)
+                # print("predic",predicted)
+                # print("labels",labels.tolist())
+                # print("predic",predicted.tolist())
+
+                for i in range(len(labels)):
+                    # label = 'dog' if predicted[i].item() == 1 else 'cat'
+                    compare_data.append([labels[i].item(), predicted[i].item(),img_path[i]])
+
+        accuracy = 100 * correct / total
+        print(f"Validation Accuracy: {accuracy:.2f}%")
+        # print(compare_data)
+        return(compare_data)
+
+    myresults=evaluate(model, val_loader)
+    compare_df = pd.DataFrame(myresults, columns=['label', 'prediction','path'])
+
+    # print(compare_df)
+
+    print(compare_df.to_string())
+    filename_write = "./output/compare_load.csv"
+    compare_df.to_csv(filename_write, index=False)
+
+
+
 #Load Dataset
-
-dataset = TrainCustomDataset(root_dir=basepath+"/working/train/train", transform=data_transforms)
-
-#Split Dataset
-
-train_size = int(0.8 * len(dataset))
-val_size = len(dataset) - train_size
-train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
-
+val_dataset = TrainCustomDataset(root_dir=basepath+"/working/valid", transform=data_transforms)
 #Make Dataloader
-
-train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
-
-
-filelist=[]
-
-for inputs, labels,img_path in val_loader:
-    # print(img_path)
-    for i in range(len(img_path)):
-        filelist.append([img_path[i]])
-
-filelist_df = pd.DataFrame(filelist, columns=['path'])
-print(filelist_df.sort_values('path').to_string())
-exit()
-
-
-model = models.resnet18(pretrained=pretrained)
-model.fc = nn.Linear(model.fc.in_features, num_classes)
-model = model.to(device)
-
-model.load_state_dict(torch.load(modelpath, map_location=device))
-
-# Validation
-# transform = transforms.Compose([Resize(224), ToTensor()])
-# val_image_folder = datasets.ImageFolder(basepath+"/working/valid", transform=transform)
-# val_loader = DataLoader(val_image_folder, batch_size=4, shuffle=False)
-
-def evaluate(model, val_loader):
-    model.eval()
-    correct = 0
-    total = 0
-
-    compare_data = []
-
-    with torch.no_grad():
-        for inputs, labels,img_path in val_loader:
-            inputs, labels = inputs.to(device), labels.to(device)
-            outputs = model(inputs)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-            # print("labels",labels)
-            # print("predic",predicted)
-            # print("labels",labels.tolist())
-            # print("predic",predicted.tolist())
-
-            for i in range(len(labels)):
-                # label = 'dog' if predicted[i].item() == 1 else 'cat'
-                compare_data.append([labels[i].item(), predicted[i].item(),img_path[i]])
-
-    accuracy = 100 * correct / total
-    print(f"Validation Accuracy: {accuracy:.2f}%")
-    # print(compare_data)
-    return(compare_data)
-
-
-myresults=evaluate(model, val_loader)
-compare_df = pd.DataFrame(myresults, columns=['label', 'prediction','path'])
-
-# print(compare_df)
-
-print(compare_df.to_string())
-filename_write = "./output/compare_load.csv"
-compare_df.to_csv(filename_write, index=False)
-
-
-
-
-
-
-
-
-
-
-# num_classes = 3
-# model = models.resnet18(pretrained=True) #Or Any Other Model
-# filepath='./output/dogsvscats_model_weights.pth'
-# # model.load_state_dict(torch.load(filepath))
-# model = models.resnet18(pretrained=True)
-# model.fc = nn.Linear(model.fc.in_features, num_classes)
-# PATH='./output/dogsvscats_model_weights.pth'
-# model.load_state_dict(torch.load(PATH))
-
-
-
-
+validate(val_loader)
--- ai/practice/kaggle/dogsvscats/kidcoder/train.py
+++ ai/practice/kaggle/dogsvscats/kidcoder/train.py
@@ -40,6 +40,7 @@ else:
 
 LEARNING_RATE = 0.00001
 do_unzip=False
+fixed_validation_data=True
 
 #Set Our Train And Test Path
 
@@ -164,51 +165,75 @@ def train(model, train_loader, criterion, optimizer, epochs):
             running_loss += loss.item()
         print(f"Epoch : [{epoch+1} / {epochs}], Average Loss : {running_loss / len(train_loader):.4f}")
 
+# Validation
+
+def validate(val_loader):
+
+    filelist=[]
+
+    for inputs, labels,img_path in val_loader:
+        # print(img_path)
+        for i in range(len(img_path)):
+            filelist.append([img_path[i]])
+
+    filelist_df = pd.DataFrame(filelist, columns=['path'])
+    # print(filelist_df.sort_values('path').to_string())
+
+    def evaluate(model, val_loader):
+        model.eval()
+        correct = 0
+        total = 0
+
+        compare_data = []
+
+        with torch.no_grad():
+            for inputs, labels,img_path in val_loader:
+                inputs, labels = inputs.to(device), labels.to(device)
+                outputs = model(inputs)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+                # print("labels",labels)
+                # print("predic",predicted)
+                # print("labels",labels.tolist())
+                # print("predic",predicted.tolist())
+
+                for i in range(len(labels)):
+                    # label = 'dog' if predicted[i].item() == 1 else 'cat'
+                    compare_data.append([labels[i].item(), predicted[i].item(),img_path[i]])
+
+        accuracy = 100 * correct / total
+        print(f"Validation Accuracy: {accuracy:.2f}%")
+        # print(compare_data)
+        return(compare_data)
+
+    myresults=evaluate(model, val_loader)
+    compare_df = pd.DataFrame(myresults, columns=['label', 'prediction','path'])
+
+    # print(compare_df)
+
+    print(compare_df.to_string())
+    filename_write = "./output/compare_train.csv"
+    compare_df.to_csv(filename_write, index=False)
+
 
 #Train Model
 
 train(model, train_loader, criterion, optimizer, epochs=EPOCHS)
 
-#Test Function
-
-def evaluate(model, val_loader):
-    model.eval()
-    correct = 0
-    total = 0
-
-    compare_data = []
-
-    with torch.no_grad():
-        for inputs, labels,img_path in val_loader:
-            inputs, labels = inputs.to(device), labels.to(device)
-            outputs = model(inputs)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-            # print("labels",labels)
-            # print("predic",predicted)
-            # print("labels",labels.tolist())
-            # print("predic",predicted.tolist())
-
-            for i in range(len(labels)):
-                # label = 'dog' if predicted[i].item() == 1 else 'cat'
-                compare_data.append([labels[i].item(), predicted[i].item(),img_path[i]])
-
-    accuracy = 100 * correct / total
-    print(f"Validation Accuracy: {accuracy:.2f}%")
-    # print(compare_data)
-    return(compare_data)
-
-
-myresults=evaluate(model, val_loader)
-compare_df = pd.DataFrame(myresults, columns=['label', 'prediction','path'])
-
-# print(compare_df)
-
-print(compare_df.to_string())
-filename_write = "./output/compare_train.csv"
-compare_df.to_csv(filename_write, index=False)
+#Save Model
 
 PATH='./output/dogsvscats_model_weights.pth'
 torch.save(model.state_dict(), PATH)
+
+
+if(fixed_validation_data==True):
+    #Load Dataset
+    val_dataset = TrainCustomDataset(root_dir=basepath+"/working/valid", transform=data_transforms)
+    #Make Dataloader
+    val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False)
+
+
+validate(val_loader)
+
 
