63f6fe60da2052b321db4385850ddf237dc92981
atmelino <tmbscience@protonmail.com>
2025-04-13 21:27:45 -0500

    u

--- ai/practice/gan/stylegan3/stylegan3_train.sh
+++ ai/practice/gan/stylegan3/stylegan3_train.sh
@@ -12,5 +12,14 @@
     # --gpus=8 --batch=32 --gamma=10 --mirror=1 --aug=noaug
 
 # Train StyleGAN2 for FFHQ at 256x256 resolution using 2 GPUs.
+
+
+code_path="../../../../../local_data/stylegan3/train.py"
+
+
+python3 $code_path --outdir=../../../../../sg3/training-runs --cfg=stylegan2 --data=../../../../../local_data/sg3/datasets/ffhq-256x256.zip \
+    --gpus=1 --batch=32 --gamma=10 --mirror=1 --aug=noaug
+
+
 python3 not_on_github/stylegan3/train.py --outdir=../../../../../sg3/training-runs --cfg=stylegan2 --data=../../../../../local_data/sg3/datasets/ffhq-256x256.zip \
     --gpus=1 --batch=32 --gamma=10 --mirror=1 --aug=noaug
