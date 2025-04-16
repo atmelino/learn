8e91c79586c28b6df42c609ce4af19f96deda37f
atmelino <tmbscience@protonmail.com>
2025-04-15 20:45:57 -0500

    u

---
+++ ai/practice/gan/stylegan3/download.sh
@@ -0,0 +1,14 @@
+
+# download the flickrfaces dataset:
+#
+# 1. Open a browser at
+# https://drive.usercontent.google.com/download?id=16N0RV4fHI6joBuKbQAoG34V_cQk7vxSA&export=download
+# Download the file named ffhq-dataset-v2.json.
+#
+# 2. Place the file ffhq-dataset-v2.json and the script named download_ffhq.py in the same directory.
+#
+# 3. Run the script:
+
+code_path="../../../../../local_data/practice/test_sg3/download_ffhq.py"
+
+python3 $code_path --json --images
--- ai/practice/gan/stylegan3/prepare.sh
+++ ai/practice/gan/stylegan3/prepare.sh
@@ -4,6 +4,13 @@
 # python3  not_on_github/stylegan3/dataset_tool.py --source=/tmp/images1024x1024 --dest=~/datasets/ffhq-1024x1024.zip
 
 # Scaled down 256x256 resolution.
+
+
+# 
+
+code_path=
+
+
 python not_on_github/stylegan3/dataset_tool.py --source=../../../../../sg3/images1024x1024 --dest=../../../../../sg3/datasets/ffhq-256x256.zip  --resolution=256x256
 
 
