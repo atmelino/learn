150f546a6afb377e28b32dfd3293fed30d287fd9
atmelino <tmbscience@protonmail.com>
2025-04-15 21:14:07 -0500

    u

--- ai/practice/gan/stylegan3/download.sh
+++ ai/practice/gan/stylegan3/download.sh
@@ -3,9 +3,9 @@ echo
 echo 1. Create a directory for the data, for example:
 echo mkdir ../../../../../local_data/practice/test_sg3
 echo
-echo 2. Open a browser at
-echo https://drive.usercontent.google.com/download?id=16N0RV4fHI6joBuKbQAoG34V_cQk7vxSA&export=download
-echo Download the file named ffhq-dataset-v2.json into the abovec created directory.
+echo 2. Open a browser at the address in the script
+# https://drive.usercontent.google.com/download?id=16N0RV4fHI6joBuKbQAoG34V_cQk7vxSA&export=download
+echo and download the file named ffhq-dataset-v2.json into the above created directory.
 echo
 echo 3. Place the file ffhq-dataset-v2.json and the script named download_ffhq.py in the same directory.
 echo
@@ -13,3 +13,5 @@ echo 4. Change into the directory
 echo
 echo 5. Run the script:
 echo python3 download_ffhq.py --json --images
+echo
+
