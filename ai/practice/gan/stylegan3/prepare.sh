b020d924e3f1d127e54172550abbcfcb7d60acd8
atmelino <tmbscience@protonmail.com>
2025-04-15 21:08:36 -0500

    u

--- ai/practice/gan/stylegan3/prepare.sh
+++ ai/practice/gan/stylegan3/prepare.sh
@@ -1,5 +1,3 @@
-
-
 # Original 1024x1024 resolution.
 # python3  not_on_github/stylegan3/dataset_tool.py --source=/tmp/images1024x1024 --dest=~/datasets/ffhq-1024x1024.zip
 
@@ -7,11 +5,11 @@
 
 
 # 
+flickrfaces_path="../../../../../local_data/practice/test_sg3"
+code_path="../../../../../local_data/stylegan3/dataset_tool.py"
 
-code_path=
 
-
-python not_on_github/stylegan3/dataset_tool.py --source=../../../../../sg3/images1024x1024 --dest=../../../../../sg3/datasets/ffhq-256x256.zip  --resolution=256x256
+python $code_path --source=../../../../../sg3/images1024x1024 --dest=../../../../../sg3/datasets/ffhq-256x256.zip  --resolution=256x256
 
 
 
