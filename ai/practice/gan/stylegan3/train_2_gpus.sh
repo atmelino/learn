c25e1c13ad85f65e1bee723ea69eac24b49dd0b8
atmelino <tmbscience@protonmail.com>
2025-04-16 21:05:34 -0500

    u

--- ai/practice/gan/stylegan3/train_2_gpus.sh
+++ ai/practice/gan/stylegan3/train_2_gpus.sh
@@ -1,10 +1,14 @@
 # conda activate stylegan3
 
-# Train StyleGAN2 for FFHQ at 256x256 resolution using 2 GPUs.
+# Train StyleGAN2 for FFHQ at 256x256 resolution using 1 GPU.
 
+code_path="../../../../../local_data/stylegan3/train.py"
+# outdir_path="../../../../../local_data/practice/sg3/training-runs"
+outdir_path="../../../../../local_data/practice/test_sg3/training-runs"
+# data_path="../../../../../local_data/practice/sg3/datasets/ffhq-256x256.zip"
+data_path="../../../../../local_data/practice/test_sg3/datasets/ffhq-256x256.zip"
+options="--cfg=stylegan2 --gpus=2 --batch=32 --gamma=10 --mirror=1 --aug=noaug"
 
-python3 not_on_github/stylegan3/train.py --outdir=../../../../../local_data/sg3/training-runs --cfg=stylegan2 --data=../../../../../local_data/sg3/datasets/ffhq-256x256.zip \
-    --gpus=2 --batch=32 --gamma=10 --mirror=1 --aug=noaug
-
+python3 $code_path --outdir=$outdir_path --data=$data_path $options
 
 
