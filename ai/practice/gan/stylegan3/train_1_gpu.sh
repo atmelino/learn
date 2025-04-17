095b30932f34b3f4c0e775db384fd6f4f937f670
atmelino <tmbscience@protonmail.com>
2025-04-16 20:36:07 -0500

    u

--- ai/practice/gan/stylegan3/train_1_gpu.sh
+++ ai/practice/gan/stylegan3/train_1_gpu.sh
@@ -4,7 +4,8 @@
 
 code_path="../../../../../local_data/stylegan3/train.py"
 outdir_path="../../../../../local_data/practice/sg3/training-runs"
-data_path="../../../../../local_data/practice/sg3/datasets/ffhq-256x256.zip"
+# data_path="../../../../../local_data/practice/sg3/datasets/ffhq-256x256.zip"
+data_path="../../../../../local_data/practice/test_sg3/datasets/ffhq-256x256.zip"
 options="--cfg=stylegan2 --gpus=1 --batch=32 --gamma=10 --mirror=1 --aug=noaug"
 
 python3 $code_path --outdir=$outdir_path --data=$data_path $options
