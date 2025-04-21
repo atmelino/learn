4332113426d7cf4d93f5e29b7a286a6e4f1597e1
atmelino <tmbscience@protonmail.com>
2025-04-20 21:08:36 -0500

    update

--- ai/practice/gan/stylegan3_ffhq_small/train.sh
+++ ai/practice/gan/stylegan3_ffhq_small/train.sh
@@ -1,14 +1,34 @@
 # conda activate stylegan3
 
 # Train StyleGAN2 for FFHQ at 256x256 resolution using 1 GPU.
-# outdir_path="../../../../../local_data/practice/test_stylegan3_ffhq/training-runs"
-# data_path="../../../../../local_data/practice/test_stylegan3_ffhq/datasets/ffhq-256x256.zip"
 
-code_path="../../../../../local_data/stylegan3/train.py"
-outdir_path="../../../../../local_data/practice/stylegan3_ffhq_small/training-runs"
-data_path="../../../../../local_data/practice/stylegan3_ffhq_small/datasets/ffhq-256x256_small.zip"
-options="--cfg=stylegan2 --gpus=1 --batch=32 --gamma=10 --mirror=1 --aug=noaug"
-
-python3 $code_path --outdir=$outdir_path --data=$data_path $options
-
-
+# code_path="../../../../../local_data/stylegan3/train.py"                                                                                                              
+# outdir_path="../../../../../local_data/practice/stylegan3_ffhq_small/training-runs"                                                                                   
+# data_path="../../../../../local_data/practice/stylegan3_ffhq_small/datasets/ffhq-256x256_small.zip"                                                                   
+# options="--cfg=stylegan2 --gpus=1 --batch=32 --gamma=10 --mirror=1 --aug=noaug --kimg=200"                                                                            
+                                                                                                                                                                        
+# python3 $code_path --outdir=$outdir_path --data=$data_path $options                                                                                                   
+                                                                                                                                                                        
+                                                                                                                                                                        
+# Train StyleGAN2 for FFHQ at 128x128 resolution using 1 GPU.                                                                                                           
+                                                                                                                                                                        
+# code_path="../../../../../local_data/stylegan3/train.py"                                                                                                              
+# outdir_path="../../../../../local_data/practice/stylegan3_ffhq_small/training-runs"                                                                                   
+# data_path="../../../../../local_data/practice/stylegan3_ffhq_small/datasets/ffhq-128x128_small.zip"                                                                   
+# options="--cfg=stylegan2 --gpus=1 --batch=32 --gamma=10 --mirror=1 --aug=noaug --kimg=200"                                                                            
+                                                                                                                                                                        
+# python3 $code_path --outdir=$outdir_path --data=$data_path $options                                                                                                   
+                                                                                                                                                                        
+                                                                                                                                                                        
+# Train StyleGAN2 for FFHQ at 64x64 resolution using 2 GPU.                                                                                                             
+                                                                                                                                                                        
+                                                                                                                                                                        
+                                                                                                                                                                        
+# Train StyleGAN2 for FFHQ at 32x32 resolution using 2 GPU.                                                                                                             
+                                                                                                                                                                        
+code_path="../../../../../local_data/stylegan3/train.py"                                                                                                                
+outdir_path="../../../../../local_data/practice/stylegan3_ffhq_small/training-runs"                                                                                     
+data_path="../../../../../local_data/practice/stylegan3_ffhq_small/datasets/ffhq-32x32_small.zip"                                                                       
+options="--cfg=stylegan2 --gpus=2 --batch=8 --gamma=10 --mirror=1 --aug=noaug --kimg=200 --snap=1"                                                                      
+                                                                                                                                                                        
+python3 $code_path --outdir=$outdir_path --data=$data_path $options   
