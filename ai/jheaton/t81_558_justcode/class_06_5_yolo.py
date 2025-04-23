ca2b8b2748f5e7f544dd9a886b60985f3a21740d
atmelino <tmbscience@protonmail.com>
2025-04-23 13:08:51 -0500

    u

--- ai/jheaton/t81_558_justcode/class_06_5_yolo.py
+++ ai/jheaton/t81_558_justcode/class_06_5_yolo.py
@@ -1,16 +1,15 @@
 # https://github.com/ultralytics/yolov5
 
-URL = "https://github.com/jeffheaton/t81_558_deep_learning/raw/master/photos/jeff_cook.jpg"
-LOCAL_IMG_FILE = "./not_on_github/images/jeff_cook.jpg"
+# URL = "https://github.com/jeffheaton/t81_558_deep_learning/raw/master/photos/jeff_cook.jpg"
 
-cmd="python3 /not_on_github/yolov5/detect.py --weights yolov5s.pt --img 1024 --conf 0.25 --source /not_on_github/images/"
-
+BASE_PATH = "../../../../local_data/jheaton"
+DATA_PATH = os.path.join(BASE_PATH, "images")
+LOCAL_IMG_FILE = DATA_PATH+"/jeff_cook.jpg"
 
 import torch
 import pandas as pd
 
 pd.set_option('display.width', 1000)
-
 
 # Model
 yolo_model = torch.hub.load('ultralytics/yolov5', 'yolov5s')
--- ai/jheaton/t81_558_justcode/class_06_5_yolo.sh
+++ ai/jheaton/t81_558_justcode/class_06_5_yolo.sh
@@ -1,2 +1,5 @@
 
-python3 not_on_github/yolov5/detect.py --weights yolov5s.pt --img 1024 --conf 0.25 --source not_on_github/images
+code_path="../../../../../local_data/yolov5/detect.py"  
+
+
+python3 $code_path --weights yolov5s.pt --img 1024 --conf 0.25 --source not_on_github/images
