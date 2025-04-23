41ef5e2f5ce06abd6a541a2d31e4c4b80e86666d
atmelino <tmbscience@protonmail.com>
2025-04-22 21:20:26 -0500

    u

--- ai/jheaton/t81_558_justcode/class_06_3_resnet_load.py
+++ ai/jheaton/t81_558_justcode/class_06_3_resnet_load.py
@@ -7,16 +7,16 @@ from tensorflow.keras.models import load_model
 
 print("class_06_3_resnet_load")
 
-PATH = "../../../../local_data/jheaton"
-SOURCE = os.path.join(PATH, "clips/paperclips")
+BASE_PATH = "../../../../local_data/jheaton"
+DATA_PATH = os.path.join(BASE_PATH, "clips/paperclips")
+OUTPUT_PATH = os.path.join(BASE_PATH, "class_06_3_resnet")
 
 validation_datagen = ImageDataGenerator(rescale=1.0 / 255)
 
-load_path = "./models"
-model = load_model(os.path.join(load_path, "class_06_3_resnet.h5"))
+model = load_model(os.path.join(OUTPUT_PATH, "class_06_3_resnet.h5"))
 
 df_test = pd.read_csv(
-    os.path.join(SOURCE,"test.csv"),
+    os.path.join(DATA_PATH,"test.csv"),
     na_values=['NA', '?'])
 
 df_test['filename']="clips-"+df_test["id"].astype(str)+".jpg"
@@ -25,7 +25,7 @@ test_datagen = ImageDataGenerator(rescale = 1./255)
 
 test_generator = validation_datagen.flow_from_dataframe(
     dataframe=df_test,
-    directory=SOURCE,
+    directory=DATA_PATH,
     x_col="filename",
     batch_size=1,
     shuffle=False,
@@ -36,7 +36,7 @@ test_generator.reset()
 pred = model.predict(test_generator,steps=len(df_test))
 
 df_submit = pd.DataFrame({'id':df_test['id'],'clip_count':pred.flatten()})
-df_submit.to_csv(os.path.join(PATH,"class_06_3_resnet.csv"),index=False)
+df_submit.to_csv(os.path.join(OUTPUT_PATH,"class_06_3_resnet.csv"),index=False)
 
 print(df_submit)
 
