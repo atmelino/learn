bfc836cfed803fb7c36ec0c7dd6ed4948c216200
atmelino <tmbscience@protonmail.com>
2025-04-23 21:13:59 -0500

    u

--- ai/jheaton/t81_558_justcode/class_07_1_gan_intro.sh
+++ ai/jheaton/t81_558_justcode/class_07_1_gan_intro.sh
@@ -4,8 +4,8 @@ echo Requires Nvidia GPU
 
 my_URL="https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan3/versions/1/files/stylegan3-r-ffhq-1024x1024.pkl"
 
-code_path="../../../../../local_data/stylegan3/gen_images.py"
-outdir_path="../../../../../local_data/jheaton/class_07_1_gan_intro_sh"
+code_path="../../../../local_data/stylegan3/gen_images.py"
+outdir_path="../../../../local_data/jheaton/class_07_1_gan_intro_sh"
 
 mkdir $outdir_path
 
