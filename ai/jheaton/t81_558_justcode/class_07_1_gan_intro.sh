2324de0ce5c15c666d4c8f11326420dbbd4ea2bd
atmelino <tmbscience@protonmail.com>
2025-04-23 21:22:21 -0500

    u

--- ai/jheaton/t81_558_justcode/class_07_1_gan_intro.sh
+++ ai/jheaton/t81_558_justcode/class_07_1_gan_intro.sh
@@ -6,9 +6,10 @@ my_URL="https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan3/versions/
 
 code_path="../../../../local_data/stylegan3/gen_images.py"
 outdir_path="../../../../local_data/jheaton/class_07_1_gan_intro_sh"
+pkl_path="../../../../local_data/stylegan3/stylegan3-r-ffhq-1024x1024.pkl"
 
 mkdir $outdir_path
 
-# python3 $code_path --network=$my_URL --outdir=./not_on_github/results --seeds=6600-6625
+python3 $code_path --network=$pkl_path --outdir=$outdir_path --seeds=6600-6625
 
 
