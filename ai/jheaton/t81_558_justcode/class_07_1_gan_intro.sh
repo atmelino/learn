17bc36211712667a66a8d0d60ee47fc193dedb20
atmelino <tmbscience@protonmail.com>
2025-04-23 21:11:45 -0500

    u

--- ai/jheaton/t81_558_justcode/class_07_1_gan_intro.sh
+++ ai/jheaton/t81_558_justcode/class_07_1_gan_intro.sh
@@ -1,14 +1,14 @@
-
-# https://github.com/NVlabs/stylegan3
-
-# !git clone https://github.com/NVlabs/stylegan3.git
-# !pip install ninja
-
+echo conda environment for this program:
+echo conda activate stylegan3
 echo Requires Nvidia GPU
 
 my_URL="https://api.ngc.nvidia.com/v2/models/nvidia/research/stylegan3/versions/1/files/stylegan3-r-ffhq-1024x1024.pkl"
 
+code_path="../../../../../local_data/stylegan3/gen_images.py"
+outdir_path="../../../../../local_data/jheaton/class_07_1_gan_intro_sh"
 
-python3 ./not_on_github/stylegan3/gen_images.py --network=$my_URL --outdir=./not_on_github/results --seeds=6600-6625
+mkdir $outdir_path
+
+# python3 $code_path --network=$my_URL --outdir=./not_on_github/results --seeds=6600-6625
 
 
--- ai/practice/gan/stylegan3_ffhq_small/train.sh
+++ ai/practice/gan/stylegan3_ffhq_small/train.sh
@@ -24,11 +24,12 @@
                                                                                                                                                                         
                                                                                                                                                                         
                                                                                                                                                                         
-# Train StyleGAN2 for FFHQ at 32x32 resolution using 2 GPU.                                                                                                             
-                                                                                                                                                                        
-code_path="../../../../../local_data/stylegan3/train.py"                                                                                                                
-outdir_path="../../../../../local_data/practice/stylegan3_ffhq_small/training-runs"                                                                                     
-data_path="../../../../../local_data/practice/stylegan3_ffhq_small/datasets/ffhq-32x32_small.zip"                                                                       
-options="--cfg=stylegan2 --gpus=2 --batch=8 --gamma=10 --mirror=1 --aug=noaug --kimg=200 --snap=1"                                                                      
-                                                                                                                                                                        
-python3 $code_path --outdir=$outdir_path --data=$data_path $options   
+# Train StyleGAN2 for FFHQ at 32x32 resolution using 2 GPU.
+
+code_path="../../../../../local_data/stylegan3/train.py"
+outdir_path="../../../../../local_data/practice/stylegan3_ffhq_small/training-runs"
+data_path="../../../../../local_data/practice/stylegan3_ffhq_small/datasets/ffhq-32x32_small.zip"
+options="--cfg=stylegan2 --gpus=2 --batch=8 --gamma=10 --mirror=1 --aug=noaug --kimg=200 --snap=1"
+
+python3 $code_path --outdir=$outdir_path --data=$data_path $options
+
