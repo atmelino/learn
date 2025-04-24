8347babf9b7dc6c21ed95924ed0fd7a31edbc28a
atmelino <tmbscience@protonmail.com>
2025-04-23 19:57:00 -0500

    u

--- ai/jheaton/t81_558_justcode/class_06_5_yolo.sh
+++ ai/jheaton/t81_558_justcode/class_06_5_yolo.sh
@@ -1,6 +1,6 @@
 
-code_path="../../../../../local_data/yolov5/detect.py"  
-data_path="../../../../../local_data/practice/jheaton/images"
+code_path="../../../../local_data/yolov5/detect.py"  
+data_path="../../../../local_data/practice/jheaton/images"
 
 
 python3 $code_path --weights yolov5s.pt --img 1024 --conf 0.25 --source $data_path
