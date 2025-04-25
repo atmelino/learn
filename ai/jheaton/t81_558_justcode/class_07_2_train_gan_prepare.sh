162b7872bb588670891f349eb44124cc77ba416c
atmelino <tmbscience@protonmail.com>
2025-04-24 21:07:43 -0500

    u

--- ai/jheaton/t81_558_justcode/class_07_2_train_gan_prepare.sh
+++ ai/jheaton/t81_558_justcode/class_07_2_train_gan_prepare.sh
@@ -7,7 +7,7 @@ mkdir ../../../../local_data/jheaton/class_07_2_train_gan/images/dogs
 mkdir ../../../../local_data/jheaton/class_07_2_train_gan/datasets
 
 
-code_path="../../../../../local_data/stylegan3/dataset_tool.py"
+code_path="../../../../local_data/stylegan3/dataset_tool.py"
 source_path="../../../../local_data/jheaton/class_07_2_train_gan/images/dogs"
 destination_path="../../../../local_data/jheaton/class_07_2_train_gan/datasets/dogs-256x256.zip"
 
