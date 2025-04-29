c6df6440ec0c7d3f7a8c1c3ec0cdf13e7eca60fc
atmelino <tmbscience@protonmail.com>
2025-04-28 20:48:40 -0500

    u

---
+++ ai/practice/gan/stylegan3_dogs/prepare_32.sh
@@ -0,0 +1,16 @@
+# Using stylegan3 for this module because stylegan2 did not work on my systems
+echo conda environment for this program:
+echo conda activate stylegan3
+
+
+mkdir ../../../../../local_data/practice/stylegan3_dogs
+mkdir ../../../../../local_data/practice/stylegan3_dogs/images
+mkdir ../../../../../local_data/practice/stylegan3_dogs/datasets
+mkdir ../../../../../local_data/practice/stylegan3_dogs/output
+
+
+code_path="../../../../../local_data/stylegan3/dataset_tool.py"
+source_path="../../../../../local_data/practice/stylegan3_dogs/images"
+destination_path="../../../../../local_data/practice/stylegan3_dogs/datasets/dogs-32x32.zip"
+
+python $code_path --source=$source_path --dest=$destination_path  --resolution=32x32
