85bf609a49519fcc65946f223bcdabdc803822f9
atmelino <tmbscience@protonmail.com>
2025-04-28 20:46:48 -0500

    u

--- ai/practice/gan/stylegan3_dogs/prepare.sh
+++ ai/practice/gan/stylegan3_dogs/prepare.sh
@@ -11,6 +11,6 @@ mkdir ../../../../../local_data/practice/stylegan3_dogs/output
 
 code_path="../../../../../local_data/stylegan3/dataset_tool.py"
 source_path="../../../../../local_data/practice/stylegan3_dogs/images"
-destination_path="../../../../local_data/practice/stylegan3_dogs/datasets/dogs-256x256.zip"
+destination_path="../../../../../local_data/practice/stylegan3_dogs/datasets/dogs-256x256.zip"
 
 python $code_path --source=$source_path --dest=$destination_path  --resolution=256x256
