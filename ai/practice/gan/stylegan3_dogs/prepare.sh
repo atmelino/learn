d73118a6c385eab16e87b03166b968f18e93bfd9
atmelino <tmbscience@protonmail.com>
2025-04-28 20:36:20 -0500

    u

--- ai/practice/gan/stylegan3_dogs/prepare.sh
+++ ai/practice/gan/stylegan3_dogs/prepare.sh
@@ -8,13 +8,13 @@ echo conda environment for this program:
 echo conda activate stylegan3
 
 
-mkdir ../../../../local_data/jheaton/class_07_2_train_gan/images/dogs
-mkdir ../../../../local_data/jheaton/class_07_2_train_gan/datasets
-mkdir ../../../../local_data/jheaton/class_07_2_train_gan/output
+mkdir ../../../../../local_data/practice/stylegan3_dogs/images
+mkdir ../../../../../local_data/practice/stylegan3_dogs/datasets
+mkdir ../../../../../local_data/practice/stylegan3_dogs/output
 
 
 code_path="../../../../local_data/stylegan3/dataset_tool.py"
-source_path="../../../../local_data/jheaton/class_07_2_train_gan/images/dogs"
-destination_path="../../../../local_data/jheaton/class_07_2_train_gan/datasets/dogs-256x256.zip"
+source_path="../../../../../local_data/practice/stylegan3_dogs/images"
+destination_path="../../../../local_data/practice/stylegan3_dogs/datasets/dogs-256x256.zip"
 
 python $code_path --source=$source_path --dest=$destination_path  --resolution=256x256
