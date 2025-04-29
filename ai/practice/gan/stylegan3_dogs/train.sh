7ed58d431446af28a3f8b927c776eacee1fdb81b
atmelino <tmbscience@protonmail.com>
2025-04-28 20:51:37 -0500

    u

--- ai/practice/gan/stylegan3_dogs/train.sh
+++ ai/practice/gan/stylegan3_dogs/train.sh
@@ -4,9 +4,9 @@ echo conda activate stylegan3
 
 
 # Modify these to suit your needs
-code_path="../../../../local_data/stylegan3/train.py"
-data_path="../../../../local_data/jheaton/class_07_2_train_gan/datasets/dogs-256x256.zip"
-outdir_path="../../../../local_data/jheaton/class_07_2_train_gan/output"
+code_path="../../../../../local_data/stylegan3/train.py"
+data_path="../../../../../local_data/practice/stylegan3_dogs/datasets/dogs-256x256.zip"
+outdir_path="../../../../../local_data/practice/stylegan3_dogs/output"
 options="--cfg=stylegan2 --gpus=2 --batch=8 --gamma=10 --mirror=1 --aug=noaug --kimg=200 --snap=1"
 
 python3 $code_path --outdir=$outdir_path --data=$data_path $options
