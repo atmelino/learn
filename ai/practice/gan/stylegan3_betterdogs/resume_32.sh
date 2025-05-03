a79d6d12eedcf188cd7b0a983e384e715a9cc5c4
atmelino <tmbscience@protonmail.com>
2025-05-02 20:14:08 -0500

    u

--- ai/practice/gan/stylegan3_betterdogs/resume_32.sh
+++ ai/practice/gan/stylegan3_betterdogs/resume_32.sh
@@ -5,7 +5,7 @@ echo conda activate stylegan3
                                                                                                                                                                         
 # Resume training
 code_path="../../../../../local_data/stylegan3/train.py"
-data_path="../../../../../local_data/practice/stylegan3_betterdogs/datasets/dogs-256x256.zip"
+data_path="../../../../../local_data/practice/stylegan3_betterdogs/datasets/dogs-32x32.zip"
 outdir_path="../../../../../local_data/practice/stylegan3_betterdogs/output"
 options1="--cfg=stylegan2 --gpus=2 --batch=8 --gamma=10 --mirror=1 --aug=noaug --kimg=200 --snap=1"
 options2=" --resume=../../../../../local_data/practice/stylegan3_betterdogs/output/00005-stylegan2-dogs-32x32-gpus2-batch8-gamma10/network-snapshot-000096.pkl"
