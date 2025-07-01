88f64ef9ed009ca45e1554a8fef821c92c1ac827
atmelino <tmbscience@protonmail.com>
2025-06-30 21:06:20 -0500

    u

--- ai/practice/tfds/pr_class_09_2_fchollet.py
+++ ai/practice/tfds/pr_class_09_2_fchollet.py
@@ -13,9 +13,9 @@ import logging, os
 logging.basicConfig(level=logging.INFO)
 # logging.basicConfig(level=logging.DEBUG)
 
-BASE_PATH = "../../../../local_data/jheaton/"
+BASE_PATH = "../../../../local_data/practice/tfds/"
 DATA_PATH = "../../../../local_data/tfds/"
-OUTPUT_PATH = BASE_PATH+"class_09_2_keras_xfer_cv/"
+OUTPUT_PATH = BASE_PATH+"pr_class_09_2_fchollet/"
 os.system("mkdir -p " + OUTPUT_PATH)
 
 tfds.disable_progress_bar()
@@ -31,6 +31,131 @@ train_ds, validation_ds, test_ds = tfds.load(
 print(f"Number of training samples: {train_ds.cardinality()}")
 print(f"Number of validation samples: {validation_ds.cardinality()}")
 print(f"Number of test samples: {test_ds.cardinality()}")
+
+
+fig=plt.figure(figsize=(10, 10))
+for i, (image, label) in enumerate(train_ds.take(9)):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(image)
+    plt.title(int(label))
+    plt.axis("off")
+filename=OUTPUT_PATH+"/pic01.png"
+fig.savefig(filename)
+
+resize_fn = keras.layers.Resizing(150, 150)
+
+train_ds = train_ds.map(lambda x, y: (resize_fn(x), y))
+validation_ds = validation_ds.map(lambda x, y: (resize_fn(x), y))
+test_ds = test_ds.map(lambda x, y: (resize_fn(x), y))
+
+augmentation_layers = [
+    layers.RandomFlip("horizontal"),
+    layers.RandomRotation(0.1),
+]
+
+def data_augmentation(x):
+    for layer in augmentation_layers:
+        x = layer(x)
+    return x
+
+train_ds = train_ds.map(lambda x, y: (data_augmentation(x), y))
+
+from tensorflow import data as tf_data
+
+batch_size = 64
+
+train_ds = train_ds.batch(batch_size).prefetch(tf_data.AUTOTUNE).cache()
+validation_ds = validation_ds.batch(batch_size).prefetch(tf_data.AUTOTUNE).cache()
+test_ds = test_ds.batch(batch_size).prefetch(tf_data.AUTOTUNE).cache()
+
+for images, labels in train_ds.take(1):
+    fig=plt.figure(figsize=(10, 10))
+    first_image = images[0]
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        augmented_image = data_augmentation(np.expand_dims(first_image, 0))
+        plt.imshow(np.array(augmented_image[0]).astype("int32"))
+        plt.title(int(labels[0]))
+        plt.axis("off")
+
+filename=OUTPUT_PATH+"/pic02.png"
+fig.savefig(filename)
+
+base_model = keras.applications.Xception(
+    weights="imagenet",  # Load weights pre-trained on ImageNet.
+    input_shape=(150, 150, 3),
+    include_top=False,
+)  # Do not include the ImageNet classifier at the top.
+
+# Freeze the base_model
+base_model.trainable = False
+
+# Create new model on top
+inputs = keras.Input(shape=(150, 150, 3))
+
+# Pre-trained Xception weights requires that input be scaled
+# from (0, 255) to a range of (-1., +1.), the rescaling layer
+# outputs: `(inputs * scale) + offset`
+scale_layer = keras.layers.Rescaling(scale=1 / 127.5, offset=-1)
+x = scale_layer(inputs)
+
+# The base model contains batchnorm layers. We want to keep them in inference mode
+# when we unfreeze the base model for fine-tuning, so we make sure that the
+# base_model is running in inference mode here.
+x = base_model(x, training=False)
+x = keras.layers.GlobalAveragePooling2D()(x)
+x = keras.layers.Dropout(0.2)(x)  # Regularize with dropout
+outputs = keras.layers.Dense(1)(x)
+model = keras.Model(inputs, outputs)
+
+model.summary(show_trainable=True)
+
+model.compile(
+    optimizer=keras.optimizers.Adam(),
+    loss=keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=[keras.metrics.BinaryAccuracy()],
+)
+
+epochs = 2
+print("Fitting the top layer of the model")
+model.fit(train_ds, epochs=epochs, validation_data=validation_ds)
+
+# Unfreeze the base_model. Note that it keeps running in inference mode
+# since we passed `training=False` when calling it. This means that
+# the batchnorm layers will not update their batch statistics.
+# This prevents the batchnorm layers from undoing all the training
+# we've done so far.
+base_model.trainable = True
+model.summary(show_trainable=True)
+
+model.compile(
+    optimizer=keras.optimizers.Adam(1e-5),  # Low learning rate
+    loss=keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=[keras.metrics.BinaryAccuracy()],
+)
+
+epochs = 1
+print("Fitting the end-to-end model")
+model.fit(train_ds, epochs=epochs, validation_data=validation_ds)
+
+print("Test dataset evaluation")
+model.evaluate(test_ds)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
