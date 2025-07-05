f82949ccc4cc93232872cdbf29d36d4509711813
atmelino <tmbscience@protonmail.com>
2025-07-04 21:38:49 -0500

    u

--- ai/practice/tfds/mnist_transfer/mnist_transfer_fit.py
+++ ai/practice/tfds/mnist_transfer/mnist_transfer_fit.py
@@ -1,3 +1,4 @@
+
 # based on https://keras.io/guides/transfer_learning/
 
 import numpy as np
@@ -15,22 +16,113 @@ DATA_PATH = "../../../../../local_data/tfds/"
 OUTPUT_PATH = BASE_PATH+"mnist_transfer/"
 os.system("mkdir -p " + OUTPUT_PATH)
 
-(train_ds, val_ds, test_ds), metadata = tfds.load('mnist',
+(train_ds, validation_ds, test_ds), metadata = tfds.load(
+                # 'mnist',
+                # 'cifar10',
+                # 'tf_flowers',
+                'cats_vs_dogs',
                 data_dir=DATA_PATH,
                 split=['train[:80%]', 'train[80%:90%]', 'train[90%:]'],
                 with_info=True,
                 as_supervised=True,
         )
 print(f"Number of training samples: {train_ds.cardinality()}")
-print(f"Number of validation samples: {val_ds.cardinality()}")
+print(f"Number of validation samples: {validation_ds.cardinality()}")
 print(f"Number of test samples: {test_ds.cardinality()}")
-
 
 base_model = keras.applications.Xception(
     weights='imagenet',  # Load weights pre-trained on ImageNet.
     input_shape=(150, 150, 3),
     include_top=False)  # Do not include the ImageNet classifier at the top.
 
+plt.figure(figsize=(10, 10))
+for i, (image, label) in enumerate(train_ds.take(9)):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(image)
+    plt.title(int(label))
+    plt.axis("off")
 
+
+resize_fn = keras.layers.Resizing(150, 150)
+
+train_ds = train_ds.map(lambda x, y: (resize_fn(x), y))
+validation_ds = validation_ds.map(lambda x, y: (resize_fn(x), y))
+test_ds = test_ds.map(lambda x, y: (resize_fn(x), y))
+
+
+augmentation_layers = [
+    layers.RandomFlip("horizontal"),
+    layers.RandomRotation(0.1),
+]
+
+
+def data_augmentation(x):
+    for layer in augmentation_layers:
+        x = layer(x)
+    return x
+
+
+train_ds = train_ds.map(lambda x, y: (data_augmentation(x), y))
+
+
+from tensorflow import data as tf_data
+
+batch_size = 64
+
+train_ds = train_ds.batch(batch_size).prefetch(tf_data.AUTOTUNE).cache()
+validation_ds = validation_ds.batch(batch_size).prefetch(tf_data.AUTOTUNE).cache()
+test_ds = test_ds.batch(batch_size).prefetch(tf_data.AUTOTUNE).cache()
+
+
+for images, labels in train_ds.take(1):
+    plt.figure(figsize=(10, 10))
+    first_image = images[0]
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        augmented_image = data_augmentation(np.expand_dims(first_image, 0))
+        plt.imshow(np.array(augmented_image[0]).astype("int32"))
+        plt.title(int(labels[0]))
+        plt.axis("off")
+
+
+base_model = keras.applications.Xception(
+    weights="imagenet",  # Load weights pre-trained on ImageNet.
+    input_shape=(150, 150, 3),
+    include_top=False,
+)  # Do not include the ImageNet classifier at the top.
+
+# Freeze the base_model
+base_model.trainable = False
+
+# Create new model on top
+inputs = keras.Input(shape=(150, 150, 3))
+
+# Pre-trained Xception weights requires that input be scaled
+# from (0, 255) to a range of (-1., +1.), the rescaling layer
+# outputs: `(inputs * scale) + offset`
+scale_layer = keras.layers.Rescaling(scale=1 / 127.5, offset=-1)
+x = scale_layer(inputs)
+
+# The base model contains batchnorm layers. We want to keep them in inference mode
+# when we unfreeze the base model for fine-tuning, so we make sure that the
+# base_model is running in inference mode here.
+x = base_model(x, training=False)
+x = keras.layers.GlobalAveragePooling2D()(x)
+x = keras.layers.Dropout(0.2)(x)  # Regularize with dropout
+outputs = keras.layers.Dense(1)(x)
+model = keras.Model(inputs, outputs)
+
+model.summary(show_trainable=True)
+
+
+model.compile(
+    optimizer=keras.optimizers.Adam(),
+    loss=keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=[keras.metrics.BinaryAccuracy()],
+)
+
+epochs = 2
+print("Fitting the top layer of the model")
+model.fit(train_ds, epochs=epochs, validation_data=validation_ds)
 
 
