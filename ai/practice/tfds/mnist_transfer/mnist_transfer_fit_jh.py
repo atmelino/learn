24876dbb3237f82df422a36b53acfa5e2418a7c1
atmelino <tmbscience@protonmail.com>
2025-07-07 20:25:25 -0500

    u

---
+++ ai/practice/tfds/mnist_transfer/mnist_transfer_fit_jh.py
@@ -0,0 +1,124 @@
+# based on https://github.com/jeffheaton/t81_558_deep_learning/blob/master/t81_558_class_09_2_keras_xfer_cv.ipynb
+
+import numpy as np
+import tensorflow_datasets as tfds
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+import logging, os
+import matplotlib.pyplot as plt
+
+logging.basicConfig(level=logging.INFO)
+# logging.basicConfig(level=logging.DEBUG)
+
+BASE_PATH = "../../../../../local_data/jheaton/"
+DATA_PATH = "../../../../../local_data/tfds/"
+OUTPUT_PATH = BASE_PATH+"mnist_transfer_fit_jh/"
+os.system("mkdir -p " + OUTPUT_PATH)
+
+(train_ds, validation_ds), metadata= tfds.load(
+    # 'mnist',
+    # 'cifar10',
+    'tf_flowers',
+    # 'cats_vs_dogs',
+    data_dir=DATA_PATH,
+    split=["train[:40%]", "train[40%:50%]"],
+    with_info=True,
+    as_supervised=True, 
+)# Include labels
+
+num_train = tf.data.experimental.cardinality(train_ds)
+num_test = tf.data.experimental.cardinality(validation_ds)
+print(f"Number of training samples: {num_train}")
+print(f"Number of validation samples: {num_test}")
+print("train_ds",train_ds)
+
+plt.figure(figsize=(10, 10))
+for i, (image, label) in enumerate(train_ds.take(3)):
+    ax = plt.subplot(3, 3, i + 1)
+    plt.imshow(image)
+    plt.title(int(label))
+    plt.axis("off")
+
+
+size = (150, 150)
+
+# train_ds = train_ds.map(lambda x, y: (tf.image.resize(x, size)/ 255.0, y))
+# validation_ds = validation_ds.map(lambda x, y: (tf.image.resize(x, size)/ 255.0, y))
+train_ds = train_ds.map(lambda x, y: (tf.image.resize(x, size), y))
+validation_ds = validation_ds.map(lambda x, y: (tf.image.resize(x, size), y))
+
+
+num_train = tf.data.experimental.cardinality(train_ds)
+num_test = tf.data.experimental.cardinality(validation_ds)
+print(f"Number of training samples: {num_train}")
+print(f"Number of validation samples: {num_test}")
+print("train_ds",train_ds)
+
+
+batch_size = 32
+train_ds = train_ds.cache().batch(batch_size).prefetch(buffer_size=10)
+validation_ds = validation_ds.cache() \
+.batch(batch_size).prefetch(buffer_size=10)
+
+
+data_augmentation = keras.Sequential(
+    [layers.RandomFlip("horizontal"), layers.RandomRotation(0.1),]
+)
+
+
+base_model = keras.applications.Xception(
+    weights="imagenet", # Load weights pre-trained on ImageNet.
+    input_shape=(150, 150, 3),
+    include_top=False,
+) # Do not include the ImageNet classifier at the top.
+
+# Freeze the base_model
+base_model.trainable = False
+
+# Create new model on top
+inputs = keras.Input(shape=(150, 150, 3))
+x = data_augmentation(inputs) # Apply random data augmentation
+
+# Pre-trained Xception weights requires that input be scaled
+# from (0, 255) to a range of (-1., +1.), the rescaling layer
+# outputs: `(inputs * scale) + offset`
+scale_layer = keras.layers.Rescaling(scale=1 / 127.5, offset=-1)
+x = scale_layer(x)
+
+# The base model contains batchnorm layers.
+# We want to keep them in inference mode
+# when we unfreeze the base model for fine-tuning,
+# so we make sure that the
+# base_model is running in inference mode here.
+x = base_model(x, training=False)
+x = keras.layers.GlobalAveragePooling2D()(x)
+x = keras.layers.Dropout(0.2)(x) # Regularize with dropout
+outputs = keras.layers.Dense(1)(x)
+model = keras.Model(inputs, outputs)
+
+model.summary()
+
+
+model.compile(
+    optimizer=keras.optimizers.Adam(),
+    loss=keras.losses.BinaryCrossentropy(from_logits=True),
+    metrics=[keras.metrics.BinaryAccuracy()],
+)
+epochs = 2
+model.fit(train_ds, epochs=epochs, validation_data=validation_ds)
+
+
+# model.compile(
+#     optimizer=tf.keras.optimizers.Adam(0.001),
+#     loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+#     metrics=[tf.keras.metrics.SparseCategoricalAccuracy()],
+# )
+
+# model.fit(
+#     train_ds,
+#     epochs=1,
+#     validation_data=validation_ds,
+# )
+
+
