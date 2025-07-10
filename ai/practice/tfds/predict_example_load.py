62f5ed045e7935855ee34e6fc4a46089d3da2b1e
atmelino <tmbscience@protonmail.com>
2025-07-09 21:00:09 -0500

    update

--- ai/practice/tfds/predict_example_load.py
+++ ai/practice/tfds/predict_example_load.py
@@ -1,9 +1,13 @@
+# Explanation of prediction output when activation is sigmoid:
+# https://forum.freecodecamp.org/t/model-predict-output/470349
+
 import tensorflow as tf
 import tensorflow_datasets as tfds
 import logging, os
 import pandas as pd
 import time
 from tensorflow.keras.models import load_model
+import numpy as np
 
 BASE_PATH = "../../../../local_data/practice/tfds/"
 DATA_PATH = "../../../../local_data/tfds/"
@@ -15,10 +19,14 @@ os.system("mkdir -p " + OUTPUT_PATH)
 (train_dataset, test_dataset), metadata = tfds.load(
     'cats_vs_dogs',
     data_dir=DATA_PATH,
-    split=['train[:80%]', 'train[80%:]'],
+    # split=['train[:80%]', 'train[80%:]'],
+    split=['train[:80%]', 'train[99%:]'],
     with_info=True,
     as_supervised=True
 )
+
+print(f"Number of test samples: {test_dataset.cardinality()}")
+
 
 # Preprocess the data
 def preprocess(image, label):
@@ -30,9 +38,36 @@ def preprocess(image, label):
 train_dataset = train_dataset.map(preprocess)
 test_dataset = test_dataset.map(preprocess)
 
-batch_size = 32
+batch_size = 64
 train_dataset = train_dataset.cache().batch(batch_size).prefetch(buffer_size=10)
 test_dataset = test_dataset.cache().batch(batch_size).prefetch(buffer_size=10)
+
+print(test_dataset)
+
+# exit()
+
+# alllabels=[]
+# alllabels=np.empty(0)
+
+# for images, labels in test_dataset:
+#     print("labels\n",labels)
+    
+#     # Convert the tensor to a NumPy array
+#     numpy_labels = labels.numpy()
+#     print(numpy_labels)    
+#     print(numpy_labels.shape)   
+
+#     numpy_labels1=numpy_labels.reshape(numpy_labels.shape[0],1)
+#     print(numpy_labels1)    
+#     print(numpy_labels1.shape)   
+
+#     alllabels = np.append(alllabels, numpy_labels)
+#     print(alllabels.shape)
+
+# print(alllabels.shape)
+# collabels = pd.DataFrame(alllabels, columns=["l"])
+# print(collabels)
+# exit()
 
 
 filename = "epochs_5.000_date_20250708-215035.h5"
@@ -48,43 +83,45 @@ model.summary()
 predictions = model.predict(test_dataset)
 print(predictions)
 
+
+
 allpred=[]
-alllabels=[]
+alllabels=np.empty(0)
 
 for images, labels in test_dataset:
-    # collabels = pd.DataFrame(labels, columns=["l"])
-    # print(collabels)
+    numpy_labels = labels.numpy()
+    # print(numpy_labels)    
+    # print(numpy_labels.shape)   
+    numpy_labels1=numpy_labels.reshape(numpy_labels.shape[0],1)
+    # print(numpy_labels1)    
+    # print(numpy_labels1.shape)   
+    alllabels = np.append(alllabels, numpy_labels)
+    print("alllabels.shape=",alllabels.shape)
+
     preds = model.predict(images)
+    # print("preds\n",preds)
     # colpred = pd.DataFrame(preds, columns=["p0"])
     # print(colpred)
     # compare = pd.concat([collabels, colpred], axis=1)
     # print(compare)
     # print("labels\n",labels)
-    # print("preds\n",preds)
     allpred.append(preds)
-    alllabels.append(labels)
 
+print(alllabels.shape)
+collabels = pd.DataFrame(alllabels, columns=["l"])
+print(collabels)
 
-collabels = pd.DataFrame(allpred, columns=["l"])
-colpred = pd.DataFrame(alllabels, columns=["p0"])
+# print(allpred.shape)
+# allpred1=allpred.reshape(allpred,shape)
+# print(allpred1.shape)
+
+colpred = pd.DataFrame( allpred, columns=["p0"])
+# print(colpred)
+
 
 compare = pd.concat([collabels, colpred], axis=1)
 compare.to_csv(OUTPUT_PATH + "pred_test.csv", index=False)    
 
 
 
-# ds = train_ds.take(20)
-# print("type(image), type(label), label,info.features['label'].names[label]=")
-# for image, label in tfds.as_numpy(ds):
-#   print(type(image), type(label), label,info.features["label"].names[label])
 
-
-
-
-# for images, labels in train_dataset:
-#     preds = model.predict(images)
-#     print(preds)
-    # Compare preds with labels
-
-# Explanation of prediction output when activation is sigmoid:
-# https://forum.freecodecamp.org/t/model-predict-output/470349
