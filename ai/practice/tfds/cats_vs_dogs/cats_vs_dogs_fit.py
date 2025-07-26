ad806331fc99fdadd8be498e139b5c077e57eccc
atmelino <tmbscience@protonmail.com>
2025-07-26 16:26:46 -0500

    u

--- ai/practice/tfds/cats_vs_dogs/cats_vs_dogs_fit.py
+++ ai/practice/tfds/cats_vs_dogs/cats_vs_dogs_fit.py
@@ -1,3 +1,4 @@
+
 # Explanation of prediction output when activation is sigmoid:
 # https://forum.freecodecamp.org/t/model-predict-output/470349
 
@@ -8,22 +9,28 @@ import pandas as pd
 import time
 from sklearn import metrics
 import numpy as np
+import matplotlib.pyplot as plt
 
 BASE_PATH = "../../../../../local_data/practice/tfds/"
 DATA_PATH = "../../../../../local_data/tfds/"
-OUTPUT_PATH = BASE_PATH+"predict_example_01/"
+OUTPUT_PATH = BASE_PATH+"cats_vs_dogs/"
 os.system("mkdir -p " + OUTPUT_PATH)
 
 
 # Load the dataset
-(train_dataset, test_dataset), metadata = tfds.load(
+(train_dataset, validation_dataset), metadata = tfds.load(
     'cats_vs_dogs',
     data_dir=DATA_PATH,
+    # split=['train[:5%]', 'train[5%:10%]'],
+    # split=['train[:20%]', 'train[20%:40%]'],
     split=['train[:80%]', 'train[80%:]'],
     # split=['train[:80%]', 'train[99%:]'],
     with_info=True,
     as_supervised=True
 )
+
+print(f"Number of train samples: {train_dataset.cardinality()}")
+print(f"Number of validation samples: {validation_dataset.cardinality()}")
 
 # Preprocess the data
 def preprocess(image, label):
@@ -33,11 +40,11 @@ def preprocess(image, label):
     return image, label
 
 train_dataset = train_dataset.map(preprocess)
-test_dataset = test_dataset.map(preprocess)
+validation_dataset = validation_dataset.map(preprocess)
 
 batch_size = 32
 train_dataset = train_dataset.cache().batch(batch_size).prefetch(buffer_size=10)
-test_dataset = test_dataset.cache().batch(batch_size).prefetch(buffer_size=10)
+validation_dataset = validation_dataset.cache().batch(batch_size).prefetch(buffer_size=10)
 
 
 # Apply data augmentation
@@ -49,7 +56,7 @@ def augment(image, label):
 train_dataset = train_dataset.map(augment)
 
 
-# Model prediction
+# Model definition
 model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(256, 256, 3)),
     tf.keras.layers.MaxPooling2D(2,2),
@@ -62,18 +69,51 @@ model = tf.keras.models.Sequential([
 
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
+
 # Train the model
-epochs=10
-model.fit(train_dataset, epochs=epochs)
+epochs=20
+history =model.fit(train_dataset, epochs=epochs,validation_data=validation_dataset)
+
+
+#-----------------------------------------------------------
+# Retrieve a list of list results on training and test data
+# sets for each training epoch
+#-----------------------------------------------------------
+acc      = history.history[     'accuracy' ]
+val_acc  = history.history[ 'val_accuracy' ]
+loss     = history.history[    'loss' ]
+val_loss = history.history['val_loss' ]
+
+epochs_plot   = range(len(acc)) # Get number of epochs
+
+#------------------------------------------------
+# Plot training and validation accuracy per epoch
+#------------------------------------------------
+plt.plot  ( epochs_plot,     acc , label="training accuracy")
+plt.plot  ( epochs_plot, val_acc, label="validation accuracy" )
+plt.title ('Training and validation accuracy')
+plt.legend(loc="upper left")
+plt.savefig(OUTPUT_PATH + "accuracy.png")
+plt.figure()
+
+
+#------------------------------------------------
+# Plot training and validation loss per epoch
+#------------------------------------------------
+plt.plot  ( epochs_plot,     loss , label="training loss")
+plt.plot  ( epochs_plot, val_loss , label="validation loss" )
+plt.legend(loc="upper left")
+plt.title ('Training and validation loss'   )
+plt.savefig(OUTPUT_PATH + "loss.png")
 
 
 # Make predictions
-predictions = model.predict(test_dataset)
+predictions = model.predict(validation_dataset)
 allpreds=predictions.flatten()
 allpnorms = np.where(allpreds > 0.5, 1, 0)
 
 alllabels=np.empty(0)
-for images, labels in test_dataset:
+for images, labels in validation_dataset:
     alllabels = np.append(alllabels, labels.numpy().flatten())
 
 score = metrics.accuracy_score(alllabels, allpnorms)
@@ -88,11 +128,13 @@ compare = pd.concat([collabels, colpreds,pnorm,diff], axis=1)
 compare.columns = ["l", "pred", "pnorm","diff"]
 print(compare)
 
-compare.to_csv(OUTPUT_PATH + "pred_test_fit.csv", index=False)    
+compare.to_csv(OUTPUT_PATH + "pred_test_fit.csv", index=False)   
+
 
 # Save model
 timestr = time.strftime("%Y%m%d-%H%M%S")
-filename = f"acc_{score:.3f}_epochs_{epochs:.3f}_date_{timestr}.h5"
+acc=val_acc[-1] # last element
+filename = f"valacc_{score:.3f}_epochs_{epochs}_date_{timestr}.h5"
 fullpath = f"{OUTPUT_PATH}{filename}"
 print("Saving model to ", filename)
 model.save(fullpath)
