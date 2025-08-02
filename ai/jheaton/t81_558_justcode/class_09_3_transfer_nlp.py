7a671ef6696404ca053ca50b357b9ae3b50aef70
atmelino <tmbscience@protonmail.com>
2025-08-02 15:39:42 -0500

    update

--- ai/jheaton/t81_558_justcode/class_09_3_transfer_nlp.py
+++ ai/jheaton/t81_558_justcode/class_09_3_transfer_nlp.py
@@ -8,8 +8,19 @@ train_data, test_data = tfds.load(name="imdb_reviews",
     batch_size=-1, as_supervised=True
 )
 
+# print(train_data)
+
+# print(f"Number of train samples: {train_data.cardinality()}")
+# print(f"Number of test samples: {test_data.cardinality()}")
+
 train_examples, train_labels = tfds.as_numpy(train_data)
 test_examples, test_labels = tfds.as_numpy(test_data)
+
+index=5
+print(train_examples[index])
+print(train_labels[index])
+
+
 
 # /Users/jheaton/tensorflow_datasets/imdb_reviews/plain_text/0.1.0
 
