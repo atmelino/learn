a6e1b6856728ea1dec213053b298e78ba5bc2831
atmelino <tmbscience@protonmail.com>
2025-08-04 21:52:25 -0500

    u

--- ai/jheaton/t81_558_justcode/class_09_3_transfer_nlp.py
+++ ai/jheaton/t81_558_justcode/class_09_3_transfer_nlp.py
@@ -1,4 +1,3 @@
-# HIDE OUTPUT
 import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_datasets as tfds
@@ -36,11 +35,54 @@ hub_layer = hub.KerasLayer(
 # print(hub_layer(train_examples[:3]))
 print(hub_layer(train_examples[:1]))
 
-
 model = tf.keras.Sequential()
 model.add(hub_layer)
 model.add(tf.keras.layers.Dense(16, activation='relu'))
 model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
 model.summary()
 
+model.compile(optimizer='adam',
+loss='binary_crossentropy',
+metrics=['accuracy'])
+init_weights = model.get_weights()
+
+x_val = train_examples[:10000]
+partial_x_train = train_examples[10000:]
+y_val = train_labels[:10000]
+partial_y_train = train_labels[10000:]
+
+history = model.fit(
+    partial_x_train,
+    partial_y_train,
+    epochs=40,
+    batch_size=512,
+    validation_data=(x_val, y_val),
+    verbose=1
+)
+
+import matplotlib.pyplot as plt
+history_dict = history.history
+
+acc = history_dict['accuracy']
+val_acc = history_dict['val_accuracy']
+loss = history_dict['loss']
+val_loss = history_dict['val_loss']
+epochs = range(1, len(acc) + 1)
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+
+plt.clf()
+# clear figure
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
 
