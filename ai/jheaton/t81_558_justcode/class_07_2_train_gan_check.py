b7b4bc48f862c0306411db3c6be2dbe5fcd8fb86
atmelino <tmbscience@protonmail.com>
2025-08-10 21:26:43 -0500

    u

--- ai/jheaton/t81_558_justcode/class_07_2_train_gan_check.py
+++ ai/jheaton/t81_558_justcode/class_07_2_train_gan_check.py
@@ -9,15 +9,21 @@ import os
 from PIL import Image
 from tqdm.notebook import tqdm
 IMAGE_PATH = '../../../../local_data/jheaton/class_07_2_train_gan/images/dogs'
+
 files = [f for f in listdir(IMAGE_PATH) if isfile(join(IMAGE_PATH, f))]
 base_size = None
-for file in tqdm(files):
+for i, file in enumerate(tqdm(files)):
+# for file in tqdm(files):
     file2 = os.path.join(IMAGE_PATH,file)
     img = Image.open(file2)
     sz = img.size
     if base_size and sz!=base_size:
-        print(f"Inconsistant size: {file2}")
+        print(f"{i} Inconsistent size: {file2}")
     elif img.mode!='RGB':
-        print(f"Inconsistant color format: {file2}")
+        print(f"Inconsistent color format: {file2}")
     else:
+        print(f"{i} Consistent size: {file2}")
         base_size = sz
+
+# To resize images all to same size:
+# convert *.jpg -resize 1024x1024! new/dog_%03d.jpg
