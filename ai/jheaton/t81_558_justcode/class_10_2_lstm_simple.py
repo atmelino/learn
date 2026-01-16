0430a510974b3111e5a422c0d4008bde74492d0e
atmelino <tmbscience@protonmail.com>
2026-01-15 20:54:13 -0600

    u

--- ai/jheaton/t81_558_justcode/class_10_2_lstm_simple.py
+++ ai/jheaton/t81_558_justcode/class_10_2_lstm_simple.py
@@ -3,6 +3,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Embedding
 from tensorflow.keras.layers import LSTM
 import numpy as np
+import pandas as pd
 
 max_features = 4  # 0,1,2,3 (total of 4)
 x = [
@@ -16,12 +17,21 @@ x = [
 x = np.array(x, dtype=np.float32)
 y = np.array([1, 2, 3, 2, 3, 1], dtype=np.int32)
 
+# print("shape of x",x.shape)
+# print(x)
+array_2d = x.reshape(6, 6)
+# array_2d = x.reshape(1, 36)
+# print(array_2d)
+
+df_x = pd.DataFrame(array_2d)
+print("input x\n",df_x)
+
 # Convert y2 to dummy variables
 y2 = np.zeros((y.shape[0], max_features), dtype=np.float32)
-print(y2)
+# print(y2)
 
 y2[np.arange(y.shape[0]), y] = 1.0
-print(y2)
+print("learning target\n",y2)
 
 print("Build model...")
 model = Sequential()
@@ -29,7 +39,7 @@ model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2, input_shape=(None, 1)))
 model.add(Dense(4, activation="sigmoid"))
 model.summary()
 
-exit()
+# exit()
 
 
 # try using different optimizers and different optimizer configs
@@ -38,6 +48,10 @@ print("Train...")
 model.fit(x, y2, epochs=200)
 
 pred = model.predict(x)
+print("prediction\n",pred)
+
+
+
 predict_classes = np.argmax(pred, axis=1)
 print("Predicted classes: {}", predict_classes)
 print("Expected classes: {}", y)
