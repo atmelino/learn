5f691799f4acba79fccef4b0c170fc8686a8107d
atmelino <tmbscience@protonmail.com>
2026-01-21 20:35:55 -0600

    u

---
+++ ai/practice/jheaton/pr_class_10_2_lstm_simple_fit.py
@@ -0,0 +1,78 @@
+from tensorflow.keras.preprocessing import sequence
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Embedding
+from tensorflow.keras.layers import LSTM
+import numpy as np
+import pandas as pd
+import logging, os
+import time
+
+BASE_PATH = "../../../../local_data/practice/jheaton/"
+OUTPUT_PATH = BASE_PATH+"pr_class_10_2_lstm_simple/"
+print("OUTPUT_PATH=",OUTPUT_PATH)
+os.system("mkdir -p " + OUTPUT_PATH)
+
+
+max_features = 4  # 0,1,2,3 (total of 4)
+x = [
+    [[0], [1], [1], [0], [0], [0]],
+    [[0], [0], [0], [2], [2], [0]],
+    [[0], [0], [0], [0], [3], [3]],
+    [[0], [2], [2], [0], [0], [0]],
+    [[0], [0], [3], [3], [0], [0]],
+    [[0], [0], [0], [0], [1], [1]],
+]
+x = np.array(x, dtype=np.float32)
+y = np.array([1, 2, 3, 2, 3, 1], dtype=np.int32)
+
+# print("shape of x",x.shape)
+# print(x)
+array_2d = x.reshape(6, 6)
+# array_2d = x.reshape(1, 36)
+# print(array_2d)
+
+df_x = pd.DataFrame(array_2d)
+print("input x\n", df_x)
+
+# Convert y2 to dummy variables
+y2 = np.zeros((y.shape[0], max_features), dtype=np.float32)
+# print(y2)
+
+y2[np.arange(y.shape[0]), y] = 1.0
+print("learning target\n", y2)
+
+print("Build model...")
+model = Sequential()
+model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2, input_shape=(None, 1)))
+model.add(Dense(4, activation="sigmoid"))
+model.summary()
+
+# exit()
+
+# try using different optimizers and different optimizer configs
+model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
+print("Train...")
+epochs=200
+history =model.fit(x, y2, epochs=epochs)
+acc      = history.history[     'accuracy' ]
+# print(acc)
+
+# Save model
+timestr = time.strftime("%Y%m%d-%H%M%S")
+filename = f"acc_{acc[-1]:.3f}_epochs_{epochs}_date_{timestr}.h5"
+filename="pr_class_10_2_lstm_simple.h5"
+
+fullpath = f"{OUTPUT_PATH}{filename}"
+print("Saving model to ", filename)
+model.save(fullpath)
+
+
+pred = model.predict(x)
+np.set_printoptions(suppress=True, precision=3)
+print("prediction\n", pred)
+predict_classes = np.argmax(pred, axis=1)
+print("Predicted classes: {}", predict_classes)
+print("Expected classes: {}", y)
+
+
+
--- ai/practice/jheaton/pr_class_10_2_lstm_simple_load.py
+++ ai/practice/jheaton/pr_class_10_2_lstm_simple_load.py
@@ -4,8 +4,29 @@ from tensorflow.keras.layers import Dense, Embedding
 from tensorflow.keras.layers import LSTM
 import numpy as np
 import pandas as pd
+import logging, os
+from tensorflow.keras.models import load_model
+
+BASE_PATH = "../../../../local_data/practice/jheaton/"
+OUTPUT_PATH = BASE_PATH+"pr_class_10_2_lstm_simple/"
+print("OUTPUT_PATH=",OUTPUT_PATH)
 
 max_features = 4  # 0,1,2,3 (total of 4)
+y = np.array([1, 2, 3, 2, 3, 1], dtype=np.int32)
+# Convert y2 to dummy variables
+y2 = np.zeros((y.shape[0], max_features), dtype=np.float32)
+# print(y2)
+y2[np.arange(y.shape[0]), y] = 1.0
+print("learning target\n", y2)
+
+
+filename="pr_class_10_2_lstm_simple.h5"
+fullpath = f"{OUTPUT_PATH}{filename}"
+model = load_model(fullpath)
+model.summary()
+
+
+
 x = [
     [[0], [1], [1], [0], [0], [0]],
     [[0], [0], [0], [2], [2], [0]],
@@ -15,36 +36,18 @@ x = [
     [[0], [0], [0], [0], [1], [1]],
 ]
 x = np.array(x, dtype=np.float32)
-y = np.array([1, 2, 3, 2, 3, 1], dtype=np.int32)
 
-# print("shape of x",x.shape)
-# print(x)
-array_2d = x.reshape(6, 6)
-# array_2d = x.reshape(1, 36)
-# print(array_2d)
+pred = model.predict(x)
+np.set_printoptions(suppress=True, precision=3)
+print("prediction\n", pred)
+predict_classes = np.argmax(pred, axis=1)
+print("Predicted classes: {}", predict_classes)
+print("Expected classes: {}", y)
 
-df_x = pd.DataFrame(array_2d)
-print("input x\n", df_x)
 
-# Convert y2 to dummy variables
-y2 = np.zeros((y.shape[0], max_features), dtype=np.float32)
-# print(y2)
+exit()
 
-y2[np.arange(y.shape[0]), y] = 1.0
-print("learning target\n", y2)
 
-print("Build model...")
-model = Sequential()
-model.add(LSTM(128, dropout=0.2, recurrent_dropout=0.2, input_shape=(None, 1)))
-model.add(Dense(4, activation="sigmoid"))
-model.summary()
-
-# exit()
-
-# try using different optimizers and different optimizer configs
-model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
-print("Train...")
-model.fit(x, y2, epochs=200)
 
 def runit(model, inp):
     inp = np.array(inp,dtype=np.float32)
@@ -55,11 +58,6 @@ def runit(model, inp):
 print( runit( model,x))
 
 
-
-exit()
-
-
-
 pred = model.predict(x)
 np.set_printoptions(suppress=True, precision=3)
 print("prediction\n", pred)
@@ -68,6 +66,11 @@ print("prediction\n", pred)
 predict_classes = np.argmax(pred, axis=1)
 print("Predicted classes: {}", predict_classes)
 print("Expected classes: {}", y)
+
+
+exit()
+
+
 
 xnew = [
     [[2], [2], [0], [0], [0], [0]],
