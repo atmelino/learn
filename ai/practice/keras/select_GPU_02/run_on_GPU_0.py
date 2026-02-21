466d557202057de30ae0af40fddc7e125e9d301b
atmelino <tmbscience@protonmail.com>
2026-02-20 20:32:58 -0600

    u

--- ai/practice/keras/select_GPU_02/run_on_GPU_0.py
+++ ai/practice/keras/select_GPU_02/run_on_GPU_0.py
@@ -4,16 +4,22 @@ import tensorflow as tf
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
 
-tf.debugging.set_log_device_placement(True)
+# tf.debugging.set_log_device_placement(True)
+tf.debugging.set_log_device_placement(False)
 
 try:
-  # Specify an invalid GPU device
+  # Specify the GPU device
+  print('Using /device:GPU:0')
   with tf.device('/device:GPU:0'):
     a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
     b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
 
-    for i in range(0,10000000):
-        print(i)
+    max=2000000
+    for i in range(0,max):
+        # print(i)
+        # print (i, end="\r")
+        print(f"{i} of {max}", end="\r")
+
         c = tf.matmul(a, b)
 except RuntimeError as e:
   print(e)
--- ai/practice/keras/select_GPU_02/run_on_GPU_1.py
+++ ai/practice/keras/select_GPU_02/run_on_GPU_1.py
@@ -1,20 +1,27 @@
 # https://www.tensorflow.org/guide/gpu#using_a_single_gpu_on_a_multi-gpu_system
 
 import tensorflow as tf
-print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices("GPU")))
 
 
-tf.debugging.set_log_device_placement(True)
+# tf.debugging.set_log_device_placement(True)
+tf.debugging.set_log_device_placement(False)
 
 try:
-  # Specify an invalid GPU device
-  with tf.device('/device:GPU:1'):
-    a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
-    b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
+    # Specify an invalid GPU device
+    print('Using /device:GPU:1')
+    with tf.device("/device:GPU:1"):
+        a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
+        b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
 
-    for i in range(0,10000000):
-        print(i)
-        c = tf.matmul(a, b)
+        max = 2000000
+        for i in range(0, max):
+            # print(i)
+            # print (i, end="\r")
+            print(f"{i} of {max}", end="\r")
+            # for i in range(0,10000000):
+            #     print(i)
+            c = tf.matmul(a, b)
 except RuntimeError as e:
-  print(e)
-
+    print(e)
