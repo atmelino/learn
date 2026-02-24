f54eb67d3194086faa04d154d682d648a0944a43
atmelino <tmbscience@protonmail.com>
2026-02-23 20:52:23 -0600

    u

--- ai/practice/jheaton/pr_class_10_3_text_generation/predict_on_GPU_0.py
+++ ai/practice/jheaton/pr_class_10_3_text_generation/predict_on_GPU_0.py
@@ -10,36 +10,14 @@ import os
 import requests
 import numpy as np
 from sklearn import metrics
+from tensorflow.keras.models import load_model
+import random
+import re
+import sys
 
-# df = pd.read_csv(
-#     "https://data.heatonresearch.com/data/t81-558/auto-mpg.csv",
-#     na_values=['NA', '?'])
-
-DATA_PATH = "../../../../../local_data/jheaton/input/"
-
-df = pd.read_csv(DATA_PATH+"auto-mpg.csv", na_values=["NA", "?"])
-print(df)
-
-cars = df["name"]
-
-# Handle missing value
-df["horsepower"] = df["horsepower"].fillna(df["horsepower"].median())
-
-# Pandas to Numpy
-x = df[
-    [
-        "cylinders",
-        "displacement",
-        "horsepower",
-        "weight",
-        "acceleration",
-        "year",
-        "origin",
-    ]
-].values
-# x = df[['cylinders']].values
-y = df["mpg"].values  # regression
-print(x)
+BASE_PATH = "../../../../../local_data/"
+DATA_PATH = BASE_PATH + "jheaton/input/"
+OUTPUT_PATH = BASE_PATH + "practice/jheaton/pr_class_10_3_text_generation/"
 
 
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices("GPU")))
@@ -52,26 +30,67 @@ try:
     print('Using /device:GPU:0')
     with tf.device("/device:GPU:0"):
 
-        # Build the neural network
-        model = Sequential()
-        model.add(Dense(25, input_dim=x.shape[1], activation="relu"))  # Hidden 1
-        model.add(Dense(10, activation="relu"))  # Hidden 2
-        model.add(Dense(1))  # Output
-        model.compile(loss="mean_squared_error", optimizer="adam")
+        # Load model
+        filename_static = "pr_class_10_3_text_generation.keras"
+        fullpath = f"{OUTPUT_PATH}{filename_static}"
+        model = load_model(fullpath)
         model.summary()
-        model.fit(x, y, verbose=2, epochs=100)
 
-        pred = model.predict(x)
-        print(f"Shape: {pred.shape}")
-        print(pred[0:10])
 
-        # Measure RMSE error.  RMSE is common for regression.
-        score = np.sqrt(metrics.mean_squared_error(pred, y))
-        print(f"Final score (RMSE): {score}")
+        def sample(preds, temperature=1.0):
+            # helper function to sample an index from a probability array
+            preds = np.asarray(preds).astype("float64")
+            preds = np.log(preds) / temperature
+            exp_preds = np.exp(preds)
+            preds = exp_preds / np.sum(exp_preds)
+            probas = np.random.multinomial(1, preds, 1)
+            return np.argmax(probas)
 
-        # Sample predictions
-        for i in range(10):
-            print(f"{i+1}. Car name: {cars[i]}, MPG: {y[i]}, " + f"predicted MPG: {pred[i]}")
+
+        def on_epoch_end():
+            # Function invoked at end of each epoch. Prints generated text.
+            print("******************************************************")
+            print("----- Generating text" )
+            start_index = random.randint(0, len(processed_text) - maxlen - 1)
+            for temperature in [0.2, 0.5, 1.0, 1.2]:
+                print("----- temperature:", temperature)
+                generated = ""
+                sentence = processed_text[start_index : start_index + maxlen]
+                generated += sentence
+                print('----- Generating with seed: "' + sentence + '"')
+                sys.stdout.write(generated)
+                for i in range(400):
+                    x_pred = np.zeros((1, maxlen, len(chars)))
+                    for t, char in enumerate(sentence):
+                        x_pred[0, t, char_indices[char]] = 1.0
+                    preds = model.predict(x_pred, verbose=0)[0]
+                    next_index = sample(preds, temperature)
+                    next_char = indices_char[next_index]
+                    generated += next_char
+                    sentence = sentence[1:] + next_char
+                    sys.stdout.write(next_char)
+                    sys.stdout.flush()
+            print()
+
+        filepath=DATA_PATH+"treasure_island.txt"
+        print(filepath)
+        raw_text = open(filepath, 'r').read()
+
+        processed_text = raw_text.lower()
+        processed_text = re.sub(r"[^\x00-\x7f]", r"", processed_text)
+        chars = sorted(list(set(processed_text)))
+
+        char_indices = dict((c, i) for i, c in enumerate(chars))
+        indices_char = dict((i, c) for i, c in enumerate(chars))
+
+        maxlen = 40
+
+
+
+
+
+        on_epoch_end()
+
 
 except RuntimeError as e:
     print(e)
