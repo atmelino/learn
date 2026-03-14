2ce33237ddc7f2b6665d56e13e438f0a47cb0880
atmelino <tmbscience@protonmail.com>
2026-03-13 20:52:19 -0500

    u

--- ai/jheaton/t81_558_justcode/class_10_5_keras_transformers.py
+++ ai/jheaton/t81_558_justcode/class_10_5_keras_transformers.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import os
+from tensorflow import keras
+from tensorflow.keras import layers
 
 BASE_PATH = "../../../../local_data/jheaton/"
 DATA_PATH = BASE_PATH + "class_10_5_keras_transformers/"
@@ -27,43 +29,61 @@ print("Ending file:")
 print(df[-10:])
 
 # Find the last zero and move one beyond
-start_id = max(df[df['obs_num'] == 0].index.tolist())+1
+start_id = max(df[df["obs_num"] == 0].index.tolist()) + 1
 print(start_id)
-df = df[start_id:] # Trim the rows that have missing observations
+df = df[start_id:]  # Trim the rows that have missing observations
 
-df['sn_value'] = df['sn_value'].astype(float)
-df_train = df[df['year']<2000]
-df_test = df[df['year']>=2000]
-spots_train = df_train['sn_value'].tolist()
-spots_test = df_test['sn_value'].tolist()
+df["sn_value"] = df["sn_value"].astype(float)
+df_train = df[df["year"] < 2000]
+df_test = df[df["year"] >= 2000]
+spots_train = df_train["sn_value"].tolist()
+spots_test = df_test["sn_value"].tolist()
 print("Training set has {} observations.".format(len(spots_train)))
 print("Test set has {} observations.".format(len(spots_test)))
 
 import numpy as np
+
+
 def to_sequences(seq_size, obs):
     x = []
     y = []
-    for i in range(len(obs)-SEQUENCE_SIZE):
-        #print(i)
-        window = obs[i:(i+SEQUENCE_SIZE)]
-        after_window = obs[i+SEQUENCE_SIZE]
+    for i in range(len(obs) - SEQUENCE_SIZE):
+        # print(i)
+        window = obs[i : (i + SEQUENCE_SIZE)]
+        after_window = obs[i + SEQUENCE_SIZE]
         window = [[x] for x in window]
-        #print("{} - {}".format(window,after_window))
+        # print("{} - {}".format(window,after_window))
         x.append(window)
         y.append(after_window)
-    return np.array(x),np.array(y)
+    return np.array(x), np.array(y)
+
 
 SEQUENCE_SIZE = 10
-x_train,y_train = to_sequences(SEQUENCE_SIZE,spots_train)
-x_test,y_test = to_sequences(SEQUENCE_SIZE,spots_test)
+x_train, y_train = to_sequences(SEQUENCE_SIZE, spots_train)
+x_test, y_test = to_sequences(SEQUENCE_SIZE, spots_test)
 print("Shape of training set: {}".format(x_train.shape))
 print("Shape of training y: {}".format(y_train.shape))
 print("Shape of test set: {}".format(x_test.shape))
 
 
 # print(x_train)
-x_tr=np.reshape(x_train,(55150,10))
+x_tr = np.reshape(x_train, (55150, 10))
 print(x_tr)
-x_te=np.reshape(x_test,(6381,10))
+x_te = np.reshape(x_test, (6381, 10))
 print(x_te)
 
+
+def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
+    # Normalization and Attention
+    x = layers.LayerNormalization(epsilon=1e-6)(inputs)
+    x = layers.MultiHeadAttention(
+        key_dim=head_size, num_heads=num_heads, dropout=dropout
+    )(x, x)
+    x = layers.Dropout(dropout)(x)
+    res = x + inputs
+    # Feed Forward Part
+    x = layers.LayerNormalization(epsilon=1e-6)(res)
+    x = layers.Conv1D(filters=ff_dim, kernel_size=1, activation="relu")(x)
+    x = layers.Dropout(dropout)(x)
+    x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
+    return x + res
