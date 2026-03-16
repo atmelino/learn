41473a3f165b57ecfffe4681f99e373e139340b1
atmelino <tmbscience@protonmail.com>
2026-03-15 20:53:52 -0500

    u

--- ai/jheaton/t81_558_justcode/class_10_5_keras_transformers.py
+++ ai/jheaton/t81_558_justcode/class_10_5_keras_transformers.py
@@ -87,3 +87,54 @@ def transformer_encoder(inputs, head_size, num_heads, ff_dim, dropout=0):
     x = layers.Dropout(dropout)(x)
     x = layers.Conv1D(filters=inputs.shape[-1], kernel_size=1)(x)
     return x + res
+
+
+def build_model(
+    input_shape,
+    head_size,
+    num_heads,
+    ff_dim,
+    num_transformer_blocks,
+    mlp_units,
+    dropout=0,
+    mlp_dropout=0,
+):
+    inputs = keras.Input(shape=input_shape)
+    x = inputs
+    for _ in range(num_transformer_blocks):
+        x = transformer_encoder(x, head_size, num_heads, ff_dim, dropout)
+
+    x = layers.GlobalAveragePooling1D(data_format="channels_first")(x)
+
+    for dim in mlp_units:
+        x = layers.Dense(dim, activation="relu")(x)
+        x = layers.Dropout(mlp_dropout)(x)
+    outputs = layers.Dense(1)(x)
+    return keras.Model(inputs, outputs)
+
+
+input_shape = x_train.shape[1:]
+model = build_model(
+    input_shape,
+    head_size=256,
+    num_heads=4,
+    ff_dim=4,
+    num_transformer_blocks=4,
+    mlp_units=[128],
+    mlp_dropout=0.4,
+    dropout=0.25,
+)
+model.compile(
+    loss="mean_squared_error", optimizer=keras.optimizers.Adam(learning_rate=1e-4)
+)
+# model.summary()
+callbacks = [keras.callbacks.EarlyStopping(patience=10, restore_best_weights=True)]
+model.fit(
+    x_train,
+    y_train,
+    validation_split=0.2,
+    epochs=200,
+    batch_size=64,
+    callbacks=callbacks,
+)
+model.evaluate(x_test, y_test, verbose=1)
