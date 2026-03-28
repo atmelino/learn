bfd44ccf4ab42e73980d00895d4f08a8dbc48cd2
atmelino <tmbscience@protonmail.com>
2026-03-27 20:28:36 -0500

    u

--- ai/practice/keras/transformers/translate_attention.py
+++ ai/practice/keras/transformers/translate_attention.py
@@ -20,52 +20,58 @@ OUTPUT_PATH = BASE_PATH + "translate_attention/"
 os.system("mkdir -p " + OUTPUT_PATH)
 
 
-class ShapeChecker():
-  def __init__(self):
-    # Keep a cache of every axis-name seen
-    self.shapes = {}
+class ShapeChecker:
+    def __init__(self):
+        # Keep a cache of every axis-name seen
+        self.shapes = {}
 
-  def __call__(self, tensor, names, broadcast=False):
-    if not tf.executing_eagerly():
-      return
+    def __call__(self, tensor, names, broadcast=False):
+        if not tf.executing_eagerly():
+            return
 
-    parsed = einops.parse_shape(tensor, names)
+        parsed = einops.parse_shape(tensor, names)
 
-    for name, new_dim in parsed.items():
-      old_dim = self.shapes.get(name, None)
+        for name, new_dim in parsed.items():
+            old_dim = self.shapes.get(name, None)
 
-      if (broadcast and new_dim == 1):
-        continue
+            if broadcast and new_dim == 1:
+                continue
 
-      if old_dim is None:
-        # If the axis name is new, add its length to the cache.
-        self.shapes[name] = new_dim
-        continue
+            if old_dim is None:
+                # If the axis name is new, add its length to the cache.
+                self.shapes[name] = new_dim
+                continue
 
-      if new_dim != old_dim:
-        raise ValueError(f"Shape mismatch for dimension: '{name}'\n"
-                         f"    found: {new_dim}\n"
-                         f"    expected: {old_dim}\n")
+            if new_dim != old_dim:
+                raise ValueError(
+                    f"Shape mismatch for dimension: '{name}'\n"
+                    f"    found: {new_dim}\n"
+                    f"    expected: {old_dim}\n"
+                )
+
 
 path_to_zip = tf.keras.utils.get_file(
-    'spa-eng.zip', origin='http://storage.googleapis.com/download.tensorflow.org/data/spa-eng.zip',
-    extract=True)
+    "spa-eng.zip",
+    # origin="http://storage.googleapis.com/download.tensorflow.org/data/spa-eng.zip",
+    origin=DATA_PATH+"spa-eng.zip",
+    extract=True,
+)
 
-path_to_file = pathlib.Path(path_to_zip).parent/'spa-eng/spa.txt'
+path_to_file = pathlib.Path(path_to_zip).parent / "spa-eng/spa.txt"
+
 
 def load_data(path):
-  text = path.read_text(encoding='utf-8')
+    text = path.read_text(encoding="utf-8")
 
-  lines = text.splitlines()
-  pairs = [line.split('\t') for line in lines]
+    lines = text.splitlines()
+    pairs = [line.split("\t") for line in lines]
 
-  context = np.array([context for target, context in pairs])
-  target = np.array([target for target, context in pairs])
+    context = np.array([context for target, context in pairs])
+    target = np.array([target for target, context in pairs])
 
-  return target, context
+    return target, context
+
 
 target_raw, context_raw = load_data(path_to_file)
 print(context_raw[-1])
 print(target_raw[-1])
-
-
