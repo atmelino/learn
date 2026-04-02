7744468b350ba1bce8d39767b67d76b1ce3051dd
atmelino <tmbscience@protonmail.com>
2026-04-01 20:29:02 -0500

    u

--- ai/practice/keras/TextVectorization/Load_text.py
+++ ai/practice/keras/TextVectorization/Load_text.py
@@ -3,29 +3,47 @@
 
 import collections
 import pathlib
-
+import os
 import matplotlib.pyplot as plt
 import tensorflow as tf
-
 from tensorflow.keras import layers
 from tensorflow.keras import losses
 from tensorflow.keras import utils
 from tensorflow.keras.layers import TextVectorization
-
 import tensorflow_datasets as tfds
 import tensorflow_text as tf_text
 
+BASE_PATH = "../../../../../local_data/practice/keras/TextVectorization/"
+DATA_PATH = BASE_PATH + "Load_text/"
+OUTPUT_PATH = BASE_PATH + "Load_text/"
+os.system("mkdir -p " + OUTPUT_PATH)
 
 
-data_url = 'https://storage.googleapis.com/download.tensorflow.org/data/stack_overflow_16k.tar.gz'
+local = False
+local = True
+if local == False:
+    print("get dataset from Internet")
+    data_url = 'https://storage.googleapis.com/download.tensorflow.org/data/stack_overflow_16k.tar.gz'
+    dataset_dir = utils.get_file(
+        origin=data_url,
+        untar=True,
+        cache_dir='stack_overflow',
+        cache_subdir='')
+    dataset_dir = pathlib.Path(dataset_dir).parent / 'stack_overflow_16k.tar.gz'
+    print(dataset_dir)
+    print(list(dataset_dir.iterdir()))
+else:
+    print("get dataset local")
+    data_url = DATA_PATH + "stack_overflow_16k.tar.gz"
+    dataset_dir = utils.get_file(
+        origin=data_url,
+        untar=True,
+        cache_dir='stack_overflow',
+        cache_subdir='')
+    dataset_dir = pathlib.Path(dataset_dir).parent / 'stack_overflow_16k.tar.gz'
+    print(dataset_dir)
+    print(list(dataset_dir.iterdir()))
 
-dataset_dir = utils.get_file(
-    origin=data_url,
-    untar=True,
-    cache_dir='stack_overflow',
-    cache_subdir='')
-
-dataset_dir = pathlib.Path(dataset_dir).parent
-
-print(list(dataset_dir.iterdir()))
-
+train_dir = dataset_dir / "train"
+print(train_dir)
+print(list(train_dir.iterdir()))
